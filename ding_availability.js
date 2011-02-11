15d35a1887887db5e7c4957b975bab43e7502bbc
Thomas Fini Hansen <xen@xen.dk>
2011-02-11 12:49:24 +0100

    Implemented holdings and added css.

--- ding_availability.js
+++ ding_availability.js
@@ -13,13 +13,31 @@ Drupal.behaviors.dingAvailabilityAttach = {
     });
 
     if (ids.length > 0) {
-      $.getJSON(settings.basePath + 'ding_availability/item/' + ids.join(','), {}, update);
+      $.getJSON(settings.basePath + 'ding_availability/' + settings.ding_availability_mode + '/' + ids.join(','), {}, update);
     }
+
     function update(data, textData) {
-      console.dir(data);
       $.each(data, function(id, item) {
         if (settings.ding_availability[id] != undefined) {
-          $('.availability-' + id).css('border', '1px solid red');
+          var text = "";
+          if (item['available']) {
+            $('.availability-' + id).addClass('available');
+            text += Drupal.t('available');
+          }
+          if (item['reservable']) {
+            $('.availability-' + id).addClass('reservable');
+            text += Drupal.t('reservable');
+          }
+          $('.availability-' + id).text(text);
+          if (item['holdings'] !== undefined) {
+            if (item['holdings'].length > 0) {
+              $('.holdings-' + id).append('<ul>');
+              var container = $('.holdings-' + id + ' ul');
+              $.each(item['holdings'], function (i, holding) {
+                container.append('<li>' + holding + '</li>');
+              });
+            }
+          }
         }
       });
     }
