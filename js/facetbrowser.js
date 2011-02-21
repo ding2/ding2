2a1f2e15a9e6df2dfd3f277bf34e396699741b31
Hasse R. Hansen <hasse@ramlev.dk>
2011-02-21 13:51:10 +0100

    Fold facet groups in and out

--- js/facetbrowser.js
+++ js/facetbrowser.js
@@ -1,7 +1,49 @@
 (function($) {
   Drupal.behaviors.facetbrowser = {
     attach: function(context, settings) {
+
+      Drupal.FoldFacetGroup();
+
     }
   };
 
+  Drupal.FoldFacetGroup = function() {
+    $(Drupal.settings.dingFacetBrowser.mainElement + ' fieldset.form-wrapper').each(function() {
+      var facetGroup = $(this);
+      if (facetGroup.find('.form-type-checkbox').size() > Drupal.settings.dingFacetBrowser.showCount) {
+        facetGroup.find('.form-type-checkbox').each(function(counter, facetElement) {
+          if (counter >= Drupal.settings.dingFacetBrowser.showCount) {
+            $(facetElement).hide();
+          }
+        });
+        facetGroup.append('<span class="expand" id="expand_more">Flere</span>');
+      }
+      else {
+      }
+    });
+
+    $(Drupal.settings.dingFacetBrowser.mainElement + ' .expand').live('click', function() {
+      var clickedKey = this;
+      var facetGroup = $(clickedKey).parent();
+
+      facetGroup.find('.form-type-checkbox:' + (clickedKey.id == 'expand_more' ? 'hidden' : 'visible')).each(function(count, facetElement) {
+        if (clickedKey.id == 'expand_more' && count < Drupal.settings.dingFacetBrowser.showCount) {
+          $(facetElement).slideDown('fast', function() {
+            if (facetGroup.find('.form-type-checkbox:visible').size() > 4 && facetGroup.find('#expand_less').size() == 0 && count % Drupal.settings.dingFacetBrowser.showCount == 0) {
+              facetGroup.find('#expand_more').after('<span class="expand" id="expand_less">Færre</span>');
+            }
+          });
+        }
+        else if (clickedKey.id == 'expand_less' && count > 4) {
+          $(facetElement).slideUp('fast', function() {
+            if (facetGroup.find('.form-type-checkbox:visible').size() == 5 && facetGroup.find('#expand_less:visible')) {
+             facetGroup.find('#expand_less').fadeOut().remove();
+            }
+
+          });
+        }
+      });
+
+    });
+  }
 })(jQuery);
