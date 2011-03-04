4b501e5b924dcec44e162d0f61cdd8dee7a6c4e6
Hasse R. Hansen <hasse@ramlev.dk>
2011-03-04 14:56:14 +0100

    Wrtap search results in a div container

--- ting-search-results.tpl.php
+++ ting-search-results.tpl.php
@@ -11,12 +11,14 @@
  */
 ?>
 <?php if ($search_results) : ?>
-  <h2><?php print t('Search results');?></h2>
-  <ol class="search-results <?php print $module; ?>-results">
-    <?php print $search_results; ?>
-  </ol>
-  <?php print $pager; ?>
-<?php else : ?>
-  <h2><?php print t('Your search yielded no results');?></h2>
-  <?php print search_help('search#noresults', drupal_help_arg()); ?>
+  <div class="search-results">
+    <h2><?php print t('Search results');?></h2>
+    <ol class="search-results <?php print $module; ?>-results">
+      <?php print $search_results; ?>
+    </ol>
+    <?php print $pager; ?>
+  <?php else : ?>
+    <h2><?php print t('Your search yielded no results');?></h2>
+    <?php print search_help('search#noresults', drupal_help_arg()); ?>
+  </div>
 <?php endif; ?>
