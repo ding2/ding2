5eef28b8ad03daf00884c86e32582eb36025481b
Thomas Fini Hansen <xen@xen.dk>
2011-04-08 15:10:46 +0200

    Removed dead code.

--- ding_popup.js
+++ ding_popup.js
@@ -4,8 +4,8 @@
 /**
  * Add a keypress handler on text and password fields that catches
  * return and submits the form by triggering the first submit
- * button. Else the browser standard handler is used, and it doesn't
- * post by AJAX.
+ * button. Otherwise the browser standard handler is used, and it
+ * doesn't post by AJAX.
  *
  * @todo possibly support more input types.
  */
@@ -14,7 +14,7 @@ Drupal.behaviors.ding_popup_form_submit = {
     $('.ui-dialog-content input[type=text]:not(.ding-popup-processed), .ui-dialog-content input[type=password]:not(.ding-popup-processed)').addClass('ding-popup-processed').each(function () {
       $(this).keypress(function (event) {
         if (event.which == 13) {
-          $($(this).parents('form').find('input[type=submit]').get(0)).trigger('mousedown');
+          $($(this.form).find('input[type=submit]').get(0)).trigger('mousedown');
           return false;
         }
       });
@@ -75,25 +75,6 @@ Drupal.ajax.prototype.commands['ding_popup'] = function (ajax, response, status)
   response['orig_ajax'] = ajax;
   Drupal.ding_popup.open(response);
   return;
-  var content_wrapped = $('<div></div>').html(response.data);
-
-  content_wrapped.dialog({
-      'close': function (event, ui) {
-        $(this).dialog('destroy').remove();
-      },
-      'modal': true,
-      'draggable': false,
-      'resizable': false,
-      'title': response.title
-  });
-
-  // Store data for cleanup.
-  Drupal.ding_popup.push({
-    'dialog': content_wrapped,
-    'resubmit': response.resubmit,
-    'extra_data': response.extra_data,
-    'orig_ajax': ajax
-  });
 };
 
 /**
@@ -102,17 +83,6 @@ Drupal.ajax.prototype.commands['ding_popup'] = function (ajax, response, status)
 Drupal.ajax.prototype.commands['ding_popup_close'] = function (ajax, response, status) {
   Drupal.ding_popup.close(response);
   return;
-  var popup = Drupal.ding_popup.pop();
-  if (popup != undefined) {
-    // Destroy dialog.
-    popup['dialog'].dialog('destroy');
-    if (popup['resubmit']) {
-      // Add in extra post vars.
-      $.extend(popup['orig_ajax'].options.data, popup['extra_data']);
-      // Call original ajax callback.
-      popup['orig_ajax'].eventResponse(popup['orig_ajax'], null);
-    }
-  }
 };
 
 
