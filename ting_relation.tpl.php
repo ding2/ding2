3cfb0cce6d7c32b16bce1f319646f0e994a7870d
Hasse R. Hansen <hasse@ramlev.dk>
2011-06-23 13:15:05 +0200

    Render all content elements

--- ting_relation.tpl.php
+++ ting_relation.tpl.php
@@ -1,8 +1,8 @@
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-   <h2><?php print($type_title); ?></h2>
-   <h3><?php print($title); ?></h3>
-   <div><?php print($content); ?></div>
+   <h2><?php print render($type_title); ?></h2>
+   <h3><?php print render($title); ?></h3>
+   <div><?php print render($content); ?></div>
    <?php if ($online_url): ?>
-   <div><?php print($online_url); ?></div>
+     <div><?php print render($online_url); ?></div>
    <?php endif; ?>
 </div>
