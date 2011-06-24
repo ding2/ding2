e19f84ea963147a03d1f0eb10f2713efad4f7a30
Mikkel Hoegh <mikkel@hoegh.org>
2011-06-24 12:56:57 +0200

    Added docstring, fixed indentation.

--- ting_relation.tpl.php
+++ ting_relation.tpl.php
@@ -1,8 +1,15 @@
+<?php
+/**
+ * @file
+ * Template to render ting_releation content.
+ */
+?>
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-   <h2><?php print render($type_title); ?></h2>
-   <h3><?php print render($title); ?></h3>
-   <div><?php print render($content); ?></div>
-   <?php if ($online_url): ?>
-     <div><?php print render($online_url); ?></div>
-   <?php endif; ?>
+  <h2><?php print render($type_title); ?></h2>
+  <h3><?php print render($title); ?></h3>
+  <div><?php print render($content); ?></div>
+  <?php if ($online_url): ?>
+    <div><?php print render($online_url); ?></div>
+  <?php endif; ?>
 </div>
+
