4094e068ef01efa70ca7b0148b4ba32b0d16f33d
Thomas Fini Hansen <xen@xen.dk>
2011-07-11 14:47:14 +0200

    Added hook_ding_entity_fields.

--- ding_entity.api.php
+++ ding_entity.api.php
@@ -52,5 +52,50 @@ function hook_ding_entity_collection_view(&$object, $view_mode) {
 }
 
 /**
+ * Tell ding_entity about ding_entity fields, so it can create the
+ * proper fields and instances.
+ *
+ * @return
+ *   An array whose keys are the names of the field, and the value is
+ *   an array with the following key value pairs:
+ *   - field: An array of field settings, as accepted by
+ *     field_create_field. field_name and type defaults to the same as
+ *     the name of the field.
+ *   - instance: Instance settings, as accepted by
+ *     field_create_instance, with the difference that entity_type and
+ *     bundle will be set automatically depending on ding_entity_type,
+ *     and ding_entity_type itself that defaults to 'ding_entity'.
+ */
+function hook_ding_entity_fields() {
+  return array(
+    'ding_availability_item' => array(
+      'field' => array(
+        'type' => 'ding_availability_item',
+        'locked' => TRUE,
+        'storage' => array(
+          'type' => 'blackhole',
+        ),
+      ),
+      'instance' => array(
+        'label' => t('Availability information'),
+      ),
+    ),
+    'ding_availability_holdings' => array(
+      'field' => array(
+        'type' => 'ding_availability_holdings',
+        'locked' => TRUE,
+        'storage' => array(
+          'type' => 'blackhole',
+        ),
+      ),
+      'instance' => array(
+        'label' => t('Holdings information'),
+      ),
+    ),
+  );
+}
+
+
+/**
  * @} End of "addtogroup hooks".
  */
