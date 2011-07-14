8439cb287614372a4332e3149899837c5506b4cd
Thomas Fini Hansen <xen@xen.dk>
2011-07-14 14:48:00 +0200

    Reworked to a field.

--- ding_availability.js
+++ ding_availability.js
@@ -8,8 +8,8 @@
 Drupal.behaviors.dingAvailabilityAttach = {
   attach: function (context, settings) {
     var ids = [];
-    $.each(settings.ding_availability, function(id, class_name) {
-        ids.push((class_name+'').split('-')[1]);
+    $.each(settings.ding_availability, function(class_name, id) {
+        ids.push(id);
     });
 
     if (ids.length > 0) {
@@ -18,23 +18,25 @@ Drupal.behaviors.dingAvailabilityAttach = {
 
     function update(data, textData) {
       $.each(data, function(id, item) {
-        var text = "";
-        if (item['available']) {
-          $('.availability-' + id).addClass('available');
-        }
-        if (item['reservable']) {
-          $('.availability-' + id).addClass('reservable');
-        }
-        $('.availability-' + id).text(item['status']);
-        if (item['holdings'] !== undefined) {
-          // TODO: Check if holdings-<id> exists.
-          if (item['holdings'].length > 0) {
-            $('.holdings-' + id).append('<h2>' + 'Holdings' + '</h2>');
-            $('.holdings-' + id).append('<ul>');
-            var container = $('.holdings-' + id + ' ul');
-            $.each(item['holdings'], function (i, holding) {
-              container.append('<li>' + holding + '</li>');
-            });
+        if (settings.ding_availability['availability-' + id] != undefined) {
+          var text = "";
+          if (item['available']) {
+            $('.availability-' + id).addClass('available');
+          }
+          if (item['reservable']) {
+            $('.availability-' + id).addClass('reservable');
+          }
+          $('.availability-' + id).text(item['status']);
+          if (item['holdings'] !== undefined) {
+            // TODO: Check if holdings-<id> exists.
+            if (item['holdings'].length > 0) {
+              $('.holdings-' + id).append('<h2>' + 'Holdings' + '</h2>');
+              $('.holdings-' + id).append('<ul>');
+              var container = $('.holdings-' + id + ' ul');
+              $.each(item['holdings'], function (i, holding) {
+                container.append('<li>' + holding + '</li>');
+              });
+            }
           }
         }
       });
