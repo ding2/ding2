bd4bb02d99568e5b15125514782845b041dee572
Thomas Fini Hansen <xen@xen.dk>
2011-08-02 13:25:23 +0200

    Fixed availability.

--- js/ding_availability.js
+++ js/ding_availability.js
@@ -5,41 +5,88 @@
 
 (function ($) {
 
+// Cache of fetched availability information.
+Drupal.DADB = {};
+
 Drupal.behaviors.dingAvailabilityAttach = {
   attach: function (context, settings) {
     var ids = [];
-    $.each(settings.ding_availability, function(class_name, id) {
-        ids.push(id);
+    $.each(settings.ding_availability, function(id, entity_ids) {
+        $.each(entity_ids, function(index, entity_id) {
+            if (Drupal.DADB[entity_id] === undefined) {
+              Drupal.DADB[entity_id] = null;
+              ids.push(entity_id);
+            }
+        });
     });
 
+    // Fetch availability.
     if (ids.length > 0) {
-      $.getJSON(settings.basePath + 'ding_availability/' + settings.ding_availability_mode + '/' + ids.join(','), {}, update);
+      $.getJSON(settings.basePath + 'ding_availability/' + (settings.ding_availability_mode ? settings.ding_availability_mode : 'items') + '/' + ids.join(','), {}, update);
     }
 
     function update(data, textData) {
       $.each(data, function(id, item) {
-        if (settings.ding_availability['availability-' + id] != undefined) {
-          var text = "";
-          if (item['available']) {
-            $('.availability-' + id).addClass('available');
+        // Update cache.
+        Drupal.DADB[id] = item;
+      });
+
+      $.each(settings.ding_availability, function(id, entity_ids) {
+          if (id.match(/^availability-/)) {
+            // Update availability indicators.
+            updateAvailability(id, entity_ids);
           }
-          if (item['reservable']) {
-            $('.availability-' + id).addClass('reservable');
+          else {
+            // Update holding information.
+            updateHoldings(id, entity_ids);
           }
-          $('.availability-' + id).text(item['status']);
-          if (item['holdings'] !== undefined) {
-            // TODO: Check if holdings-<id> exists.
-            if (item['holdings'].length > 0) {
-              $('.holdings-' + id).append('<h2>' + Drupal.t('Holdings') + '</h2>');
-              $('.holdings-' + id).append('<ul>');
-              var container = $('.holdings-' + id + ' ul');
-              $.each(item['holdings'], function (i, holding) {
-                container.append('<li>' + holding + '</li>');
-              });
-            }
-          }
+        });
+    }
+
+    function updateAvailability(id, entity_ids) {
+      var available = false;
+      var reservable = false;
+      $.each(entity_ids, function(index, entity_id) {
+          if (Drupal.DADB[entity_id] !== undefined) {
+          available = available || Drupal.DADB[entity_id]['available'];
+          reservable = reservable || Drupal.DADB[entity_id]['reservable'];
         }
       });
+
+      if (available) {
+        $('#' + id).addClass('available');
+      }
+      if (reservable) {
+        $('#' + id).addClass('reservable');
+      }
+
+      if (available && reservable) {
+        $('#' + id).attr('title', Drupal.t('available'));
+      }
+      else if (!available && reservable) {
+        $('#' + id).attr('title', Drupal.t('on loan'));
+      }
+      else if (available && !reservable) {
+        $('#' + id).attr('title', Drupal.t('not reservable'));
+      }
+      else if (!available && !reservable) {
+        $('#' + id).attr('title', Drupal.t('unavailable'));
+      }
+    }
+
+    function updateHoldings(id, entity_ids) {
+      var entity_id = entity_ids.pop();
+      if (Drupal.DADB[entity_id] !== undefined && Drupal.DADB[entity_id]['holdings'] !== undefined) {
+        var holdings = Drupal.DADB[entity_id]['holdings'];
+        if (holdings.length > 0) {
+          $('#' + id).append('<h2>' + Drupal.t('Holdings') + '</h2>');
+          $('#' + id).append('<ul>');
+          var container = $('#' + id + ' ul');
+          $.each(holdings, function (i, holding) {
+              container.append('<li>' + holding + '</li>');
+            });
+        }
+      }
     }
   }
 }
