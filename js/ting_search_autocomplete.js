984f5fad97c3aafae8703c2220d3037e9193a907
Hasse R. Hansen <hasse@ramlev.dk>
2011-08-11 23:01:14 +0200

    When searching show a spinner image.

--- js/ting_search_autocomplete.js
+++ js/ting_search_autocomplete.js
@@ -1,6 +1,7 @@
 (function ($) {
   Drupal.behaviors.tingSearchAutocomplete = {
     attach: function(context) {
+
       $('#edit-search-block-form--2').autocomplete({
         minLength: 3,
         source: function(request, response) {
@@ -9,16 +10,16 @@
           }, response);
         },
         search: function(event, ui) {
-          // Use this when starting a search,
-          // for ex showing a spinner gif.
-          //$('.spinner').show();
+          // When a search is beginning, show the spinner
+          $('#edit-search-block-form--2').addClass('spinner');
+
         },
         open: function(event, ui) {
-          // When a search is done, use this,
-          // to ex. hide the spinner.
-          //$('.spinner').hide();
+          // When a search is done, use this, to hide the spinner.
+          $('#edit-search-block-form--2').removeClass('spinner');
         },
         select: function(event, ui) {
+          // Add the chosen value to the searchbox and submit.
           if (ui.item) {
             $('#edit-search-block-form--2').val(ui.item.value);
             $('#search-block-form').submit();
