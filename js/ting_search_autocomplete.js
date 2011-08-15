0976fae18a6c391b0376eb1f07680a0ea752bd3e
Hasse R. Hansen <hasse@ramlev.dk>
2011-08-15 16:28:19 +0200

    Fixed a few bugs, need javascript to validate through jslint

--- js/ting_search_autocomplete.js
+++ js/ting_search_autocomplete.js
@@ -1,20 +1,19 @@
-(function ($) {
+(function($) {
   Drupal.behaviors.tingSearchAutocomplete = {
     attach: function(context) {
 
       $('#edit-search-block-form--2').autocomplete({
         minLength: 3,
         source: function(request, response) {
-          $.getJSON('/ting/autocomplete',{
+          $.getJSON('/ting/autocomplete', {
             query: request.term
           }, response);
         },
-        search: function(event, ui) {
+        search: function(event, ui) {
           // When a search is beginning, show the spinner
           $('#edit-search-block-form--2').addClass('spinner');
-
         },
-        open: function(event, ui) {
+        open: function(event, ui) {
           // When a search is done, use this, to hide the spinner.
           $('#edit-search-block-form--2').removeClass('spinner');
         },
@@ -28,4 +27,5 @@
       });
     }
   };
-}(jQuery));
+} (jQuery));
+
