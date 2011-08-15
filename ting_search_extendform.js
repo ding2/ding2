8d7cdd2583b96ab6b646869916911ae7a3e6ee09
Hasse R. Hansen <hasse@ramlev.dk>
2011-08-15 16:48:59 +0200

    Adding missing semicolon

--- ting_search_extendform.js
+++ ting_search_extendform.js
@@ -1,7 +1,6 @@
+(function($) {
 
-(function($){
-
-  $(document).ready(function(){
+  $(document).ready(function() {
     Drupal.setSelectedLabel();
   });
 
@@ -10,9 +9,9 @@
 
   Drupal.behaviors.showExtendedForm = {
     // 'Show advanced search' link onClick function
-    attach: function (context, settings) {
-      $('#extend-form-show', context).click(function () {
-        if ( ! $.TingExtendedForm.showExtended ) {
+    attach: function(context, settings) {
+      $('#extend-form-show', context).click(function() {
+        if (!$.TingExtendedForm.showExtended) {
           $("#search-extend-form").removeClass('extend-form-show');
           $("#search-extend-form").addClass('extend-form-show');
           $.TingExtendedForm.showExtended = true;
@@ -27,8 +26,8 @@
 
   Drupal.behaviors.hideExtendedForm = {
     // 'Hide Advanced search' link onClick function
-    attach: function (context, settings) {
-      $('#extend-form-hide', context).click(function () {
+    attach: function(context, settings) {
+      $('#extend-form-hide', context).click(function() {
         $("#search-extend-form").removeClass('extend-form-show');
         return false;
       });
@@ -37,9 +36,9 @@
 
   Drupal.behaviors.readyExtendedForm = {
     // If there's no 'Show advanced search' link, set display: block.
-    attach: function (context, settings) {
-      $('#extend-form', context).ready(function () {
-        if ( !$('#extend-form-show') ) {
+    attach: function(context, settings) {
+      $('#extend-form', context).ready(function() {
+        if (!$('#extend-form-show')) {
           $("#search-extend-form").addClass('extend-form-show');
         }
       });
@@ -47,13 +46,13 @@
   };
 
   Drupal.behaviors.toggleSize = {
-    attach: function (context, settings) {
-      $('.form-item-size').find('input').change(function () {
+    attach: function(context, settings) {
+      $('.form-item-size').find('input').change(function() {
         Drupal.setSelectedLabel();
         var action = document.location.href.split('?');
         action = action[0] + '?sort=' + $('#edit-sort').val() + '&size=' + $("input[name='size']:checked").val();
         var fragment = document.location.href.split('#');
-        if ( fragment[1] ) {
+        if (fragment[1]) {
           action = action + '#' + fragment[1];
         }
         document.location.href = action;
@@ -62,12 +61,12 @@
   };
 
   Drupal.behaviors.toggleSort = {
-    attach: function (context, settings) {
-      $('#edit-sort').change(function () {
+    attach: function(context, settings) {
+      $('#edit-sort').change(function() {
         var action = document.location.href.split('?');
         action = action[0] + '?sort=' + $('#edit-sort').val() + '&size=' + $("input[name='size']:checked").val();
         var fragment = document.location.href.split('#');
-        if ( fragment[1] ) {
+        if (fragment[1]) {
           action = action + '#' + fragment[1];
         }
         document.location.href = action;
@@ -75,50 +74,49 @@
     }
   };
 
-
   Drupal.behaviors.setExtendedFormCreator = {
-    attach: function (context, settings) {
+    attach: function(context, settings) {
       $('#edit-extendform-creator').change(function() {
-          $('#extend_search_creator').val( $('#edit-extendform-creator').val() );
+        $('#extend_search_creator').val($('#edit-extendform-creator').val());
       });
     }
   };
 
   Drupal.behaviors.setExtendedFormTitle = {
-    attach: function (context, settings) {
+    attach: function(context, settings) {
       $('#edit-extendform-title').change(function() {
-          $('#extend_search_title').val( $('#edit-extendform-title').val() );
+        $('#extend_search_title').val($('#edit-extendform-title').val());
       });
     }
   };
 
   Drupal.behaviors.setExtendedFormSubject = {
-    attach: function (context, settings) {
+    attach: function(context, settings) {
       $('#edit-extendform-subject').change(function() {
-          $('#extend_search_subject').val( $('#edit-extendform-subject').val() );
+        $('#extend_search_subject').val($('#edit-extendform-subject').val());
       });
     }
   };
 
   Drupal.behaviors.setExtendedFormSubmit = {
-    attach: function (context, settings) {
+    attach: function(context, settings) {
       $('#search-extend-form').keypress(function(e) {
-          c = e.which ? e.which : e.keyCode;
-          if ( c == 13 ) {
-              $('#extend_search_creator').val( $('#edit-extendform-creator').val() );
-              $('#extend_search_title').val( $('#edit-extendform-title').val() );
-              $('#extend_search_subject').val( $('#edit-extendform-subject').val() );
-              $('#search-block-form').submit();
-              return false;
-          }
+        c = e.which ? e.which: e.keyCode;
+        if (c == 13) {
+          $('#extend_search_creator').val($('#edit-extendform-creator').val());
+          $('#extend_search_title').val($('#edit-extendform-title').val());
+          $('#extend_search_subject').val($('#edit-extendform-subject').val());
+          $('#search-block-form').submit();
+          return false;
+        }
       });
     }
   };
 
-  Drupal.setSelectedLabel = function () {
+  Drupal.setSelectedLabel = function() {
     $('.form-item-size').find('label').removeClass('labelSelected');
     $('input[name=size]').filter(':checked').parent().find('label').addClass('labelSelected');
-  }
+  };
 
-}(jQuery));
+} (jQuery));
 
