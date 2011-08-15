eaf3aa57159da96832173b34d7b512fe07bcddd4
Hasse R. Hansen <hasse@ramlev.dk>
2011-08-15 16:32:12 +0200

    Adding missing ; after behaviour

--- js/ding_availability.js
+++ js/ding_availability.js
@@ -3,35 +3,35 @@
  * JavaScript behaviours for fetching and displaying availability.
  */
 
-(function ($) {
+(function($) {
 
-// Cache of fetched availability information.
-Drupal.DADB = {};
+  // Cache of fetched availability information.
+  Drupal.DADB = {};
 
-Drupal.behaviors.dingAvailabilityAttach = {
-  attach: function (context, settings) {
-    var ids = [];
-    $.each(settings.ding_availability, function(id, entity_ids) {
+  Drupal.behaviors.dingAvailabilityAttach = {
+    attach: function(context, settings) {
+      var ids = [];
+      $.each(settings.ding_availability, function(id, entity_ids) {
         $.each(entity_ids, function(index, entity_id) {
-            if (Drupal.DADB[entity_id] === undefined) {
-              Drupal.DADB[entity_id] = null;
-              ids.push(entity_id);
-            }
+          if (Drupal.DADB[entity_id] === undefined) {
+            Drupal.DADB[entity_id] = null;
+            ids.push(entity_id);
+          }
         });
-    });
-
-    // Fetch availability.
-    if (ids.length > 0) {
-      $.getJSON(settings.basePath + 'ding_availability/' + (settings.ding_availability_mode ? settings.ding_availability_mode : 'items') + '/' + ids.join(','), {}, update);
-    }
-
-    function update(data, textData) {
-      $.each(data, function(id, item) {
-        // Update cache.
-        Drupal.DADB[id] = item;
       });
 
-      $.each(settings.ding_availability, function(id, entity_ids) {
+      // Fetch availability.
+      if (ids.length > 0) {
+        $.getJSON(settings.basePath + 'ding_availability/' + (settings.ding_availability_mode ? settings.ding_availability_mode: 'items') + '/' + ids.join(','), {}, update);
+      }
+
+      function update(data, textData) {
+        $.each(data, function(id, item) {
+          // Update cache.
+          Drupal.DADB[id] = item;
+        });
+
+        $.each(settings.ding_availability, function(id, entity_ids) {
           if (id.match(/^availability-/)) {
             // Update availability indicators.
             updateAvailability(id, entity_ids);
@@ -41,54 +41,55 @@ Drupal.behaviors.dingAvailabilityAttach = {
             updateHoldings(id, entity_ids);
           }
         });
-    }
+      }
 
-    function updateAvailability(id, entity_ids) {
-      var available = false;
-      var reservable = false;
-      $.each(entity_ids, function(index, entity_id) {
+      function updateAvailability(id, entity_ids) {
+        var available = false;
+        var reservable = false;
+        $.each(entity_ids, function(index, entity_id) {
           if (Drupal.DADB[entity_id] !== undefined) {
-          available = available || Drupal.DADB[entity_id]['available'];
-          reservable = reservable || Drupal.DADB[entity_id]['reservable'];
+            available = available || Drupal.DADB[entity_id]['available'];
+            reservable = reservable || Drupal.DADB[entity_id]['reservable'];
+          }
+        });
+
+        if (available) {
+          $('#' + id).addClass('available');
         }
-      });
+        if (reservable) {
+          $('#' + id).addClass('reservable');
+        }
 
-      if (available) {
-        $('#' + id).addClass('available');
-      }
-      if (reservable) {
-        $('#' + id).addClass('reservable');
+        if (available && reservable) {
+          $('#' + id).attr('title', Drupal.t('available'));
+        }
+        else if (!available && reservable) {
+          $('#' + id).attr('title', Drupal.t('on loan'));
+        }
+        else if (available && ! reservable) {
+          $('#' + id).attr('title', Drupal.t('not reservable'));
+        }
+        else if (!available && ! reservable) {
+          $('#' + id).attr('title', Drupal.t('unavailable'));
+        }
       }
 
-      if (available && reservable) {
-        $('#' + id).attr('title', Drupal.t('available'));
-      }
-      else if (!available && reservable) {
-        $('#' + id).attr('title', Drupal.t('on loan'));
-      }
-      else if (available && !reservable) {
-        $('#' + id).attr('title', Drupal.t('not reservable'));
-      }
-      else if (!available && !reservable) {
-        $('#' + id).attr('title', Drupal.t('unavailable'));
-      }
-    }
-
-    function updateHoldings(id, entity_ids) {
-      var entity_id = entity_ids.pop();
-      if (Drupal.DADB[entity_id] !== undefined && Drupal.DADB[entity_id]['holdings'] !== undefined) {
-        var holdings = Drupal.DADB[entity_id]['holdings'];
-        if (holdings.length > 0) {
-          $('#' + id).append('<h2>' + Drupal.t('Holdings') + '</h2>');
-          $('#' + id).append('<ul>');
-          var container = $('#' + id + ' ul');
-          $.each(holdings, function (i, holding) {
+      function updateHoldings(id, entity_ids) {
+        var entity_id = entity_ids.pop();
+        if (Drupal.DADB[entity_id] !== undefined && Drupal.DADB[entity_id]['holdings'] !== undefined) {
+          var holdings = Drupal.DADB[entity_id]['holdings'];
+          if (holdings.length > 0) {
+            $('#' + id).append('<h2>' + Drupal.t('Holdings') + '</h2>');
+            $('#' + id).append('<ul>');
+            var container = $('#' + id + ' ul');
+            $.each(holdings, function(i, holding) {
               container.append('<li>' + holding + '</li>');
             });
+          }
         }
       }
     }
-  }
-}
+  };
 
 })(jQuery);
+
