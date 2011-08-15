3889ef6043b668a492810312c43c951c526d89d4
Hasse R. Hansen <hasse@ramlev.dk>
2011-08-15 16:40:02 +0200

    Adding missing semicolon

--- js/ding_toggle_format.js
+++ js/ding_toggle_format.js
@@ -1,10 +1,9 @@
-
-(function($){
+(function($) {
 
   Drupal.behaviors.toggleFormat = {
-    attach: function (context, settings) {
-      $('#ding-toggle-format', context).click(function () {
-        var toFormat = ( $.cookie("ding_toggle_format") == 'short' ) ? 'long' : 'short';
+    attach: function(context, settings) {
+      $('#ding-toggle-format', context).click(function() {
+        var toFormat = ($.cookie("ding_toggle_format") == 'short') ? 'long': 'short';
         Drupal.setFormat(toFormat);
         return false;
       });
@@ -12,22 +11,25 @@
   };
 
   Drupal.behaviors.readyFormat = {
-    attach: function (context, settings) {
-      $('#ding-toggle-format', context).ready(function () {
-        var format = ( $.cookie("ding_toggle_format") ) ? $.cookie("ding_toggle_format") : 'short';
+    attach: function(context, settings) {
+      $('#ding-toggle-format', context).ready(function() {
+        var format = ($.cookie("ding_toggle_format")) ? $.cookie("ding_toggle_format") : 'short';
         Drupal.setFormat(format);
       });
     }
   };
 
-  Drupal.setFormat = function (format) {
+  Drupal.setFormat = function(format) {
     $("#ding-toggle-format").removeClass('ding-toggle-format-long');
     $("#ding-toggle-format").removeClass('ding-toggle-format-short');
     $("#ding-toggle-format").addClass('ding-toggle-format-' + format);
     $("li.search-result").removeClass('ding-format-long');
     $("li.search-result").removeClass('ding-format-short');
     $("li.search-result").addClass('ding-format-' + format);
-    $.cookie("ding_toggle_format", format, { expires: 30 });
-  }
+    $.cookie("ding_toggle_format", format, {
+      expires: 30
+    });
+  };
 
-}(jQuery));
+} (jQuery));
+
