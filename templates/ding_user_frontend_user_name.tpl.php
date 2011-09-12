204d0f8743f394a3ad05d5d6ddfbb7d7665c5624
Mikkel Hoegh <mikkel@hoegh.org>
2011-09-12 20:31:45 +0200

    Refactored blocks to use templates for rendering.
    Lots of small code style tweaks.

---
+++ templates/ding_user_frontend_user_name.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file
+ * Template to render the user name block.
+ */
+?>
+<div id="ding-user-icon"><div class="content"></div></div>
+
+<div id="ding-user-name"><div class="content"><?php print $name; ?></div></div>
+
---
+++ templates/ding_user_frontend_user_status.tpl.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file
+ * Template to render the user status block.
+ */
+?>
+<div id="ding-user-loan-amount"><div class="content">
+  <span class="label"><?php print l(t('Loans:'), 'user/' . $user->uid . '/status'); ?></span>
+  <span class="amount"><?php print $loan_count; ?></span>
+</div></div>
+
+<div id="ding-user-reservation-amount"><div class="content">
+  <span class="label"><?php print l(t('Reservations:'),'user/' . $user->uid . '/status/reservations'); ?></span>
+  <span class="amount"><?php print $reservation_count; ?></span>
+</div></div>
+
+<div id="ding-user-debt-amount"><div class="content">
+  <span class="label"><?php l(t('Debts:'), 'user/' . $user->uid . '/status/debts'); ?></span>
+  <span class="amount"><?php print $debt_count; ?></span>
+</div></div>
+
