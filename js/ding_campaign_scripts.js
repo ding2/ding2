4105d8b443504c703dc8bc5d5389016b9864c4c1
AnatolyMuntean <am@inleadmedia.com>
2011-09-22 11:21:35 -0400

    case 12284 - Rule editor for campaigns. Page rules display.

--- js/ding_campaign_scripts.js
+++ js/ding_campaign_scripts.js
@@ -1,26 +1,13 @@
 (function ($) {
   campaign_content_click = function(type) {
-    $('#edit-field-camp-text-plain, #edit-field-camp-image, #edit-field-camp-text-full').hide();
+    $('#edit-field-camp-image, #edit-field-camp-text-full').hide();
 
     switch(type) {
-      case 'plain':
-        $('#edit-field-camp-text-plain').show();
-        break;
       case 'image':
         $('#edit-field-camp-image').show();
         break;
       case 'full':
         $('#edit-field-camp-text-full').show();
-        break;
-    }
-  }
-
-  campaign_type_click = function(type) {
-    $('#edit-field-camp-keywords').hide();
-
-    switch(type) {
-      case 'search':
-        $('#edit-field-camp-keywords').show();
         break;
     }
   }
@@ -34,11 +21,13 @@
         campaign_content_click(type);
       });
 
-      campaign_type_click($('.node-campaign-form #edit-field-camp-type input[type=radio]:checked').attr('value'));
-
-      $('.node-campaign-form #edit-field-camp-type input[type=radio]').click(function() {
-        var type = $(this).attr('value');
-        campaign_type_click(type);
+      $('.ding-campaign-rule select').change(function() {
+        if ($(this).selected().attr('value') == 'rule_generic') {
+          $(this).parent().parent().parent().find('.rule-value').hide();
+        }
+        else {
+          $(this).parent().parent().parent().find('.rule-value').show();
+        }
       });
     }
   }
