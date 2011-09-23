0afd99f22445a673c7d40612597ee1eace4080c2
Martin Cording <mc@inleadmedia.com>
2011-09-23 09:03:43 +0200

    Added the themes according to user story #260.

---
+++ templates/ding_campaign-orange.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * Campaign template for orange theme.
+ */
+?>
+<div class="ding-campaign-item ding-campaign-item-orange">
+<?php print ($content); ?>
+</div>
--- templates/ding_campaign-red.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @file
- * Campaign template for red theme.
- */
-?>
-<div class="ding-campaign-item ding-campaign-item-red">
-<?php print ($content); ?>
-</div>
