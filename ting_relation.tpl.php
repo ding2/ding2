85e3e87094ed0775000962d82cec81531b6b7fd0
pjohans <pjohans@gmail.com>
2011-09-28 11:31:05 +0200

    switch on relation-types. buttons for links etc.

--- ting_relation.tpl.php
+++ ting_relation.tpl.php
@@ -7,9 +7,16 @@
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <h2><?php print render($type_title); ?></h2>
   <h3><?php print render($title); ?></h3>
+ 
   <div><?php print render($content); ?></div>
+
   <?php if ($online_url): ?>
-    <div><?php print render($online_url); ?></div>
+<div class="field-type-ting-relation">
+<div class ="field-items">
+    <?php print render($online_url); ?>
+</div>
+</div>
   <?php endif; ?>
+
 </div>
 
