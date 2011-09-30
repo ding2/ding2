418865c1410904de421795728d8bb35da1e37bb9
pjohans <pjohans@gmail.com>
2011-09-30 12:48:33 +0200

    logo

--- ting_infomedia_article_view.tpl.php
+++ ting_infomedia_article_view.tpl.php
@@ -1,5 +1,5 @@
 <hr style="margin-bottom:1em"/>
 <div class="infomedia_image">
-  <img src="<?php echo drupal_get_path('module','ting_infomedia') . '/ting_infomedia_logo.gif'; ?>"/>
+  <img src="<?php echo base_path().'/'.drupal_get_path('module','ting_infomedia') . '/ting_infomedia_logo.jpg'; ?>"/>
 </div>
 <?php echo $variables['element']['#markup']; ?>
