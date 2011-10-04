9376d01e1f6984faf306cabcacca5c8cc3ab5bb7
pjohans <pjohans@gmail.com>
2011-10-04 20:55:02 +0200

    Template for advanced search and clear field button

---
+++ ting-search-extended-search.tpl.php
@@ -0,0 +1,17 @@
+<?php
+// $Id: search-results.tpl.php,v 1.7 2010/08/18 18:40:50 dries Exp $
+
+/**
+ * @file
+ * Default theme implementation for displaying ting search results.
+ *
+ * We need a custom template because we need a special pager.
+ *
+ * @see template_preprocess_ting_search_results()
+ */
+?>
+
+<div id="extend-form-div">
+<a id="extend-form-show" href="#"><?php echo t('Advanced search') ?></a>
+<a id="extend-form-clear" href="#"><?php echo t('Clear fields')?></a>
+</div>
