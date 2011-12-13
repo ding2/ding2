7a747a69876d3cea213eed0fe5793eb7759450fb
Jorgen G. Nielsen <jgn@dbc.dk>
2011-12-13 15:53:47 +0100

    added infomedia content type, and set up panel page for articles

--- ting_infomedia_article_view.tpl.php
+++ ting_infomedia_article_view.tpl.php
@@ -1,5 +1,6 @@
-<hr style="margin-bottom:1em"/>
-<div class="infomedia_image">
-  <?php print theme('image', array('path' => drupal_get_path('module', 'ting_infomedia') . '/ting_infomedia_logo.jpg')); ?>
+<div  class="infomedia-article">
+  <div class="infomedia-image">
+    <?php print theme('image', array('path' => drupal_get_path('module', 'ting_infomedia') . '/ting_infomedia_logo.jpg')); ?>
+  </div>
+  <?php echo $variables['element']['#markup']; ?>
 </div>
-<?php echo $variables['element']['#markup']; ?>
