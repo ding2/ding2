7f9e0e1d769822d3be721098dcd678ef974cb230
pjohans <pjohans@gmail.com>
2012-01-05 08:07:16 +0100

    periodicals foldable by year

---
+++ ding_periodical.js
@@ -0,0 +1,13 @@
+(function ($) {
+$(document).ready(function(){
+    $('.field-name-ding-periodical-issues li').children('.item-list').hide();
+});
+
+  Drupal.behaviors.dingPeriodicalIssueToggle = {
+    attach: function (context, settings) {
+        $('.field-name-ding-periodical-issues .ding-periodical-fold').click(function(){
+	    $(this).parent('.ding-periodical-foldable').children('.item-list').toggle();
+        });
+    }
+  }    
+}(jQuery));
