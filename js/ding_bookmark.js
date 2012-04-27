5346c0d8aacedcb6019fc3874064f2d7e133073d
Jorgen G. Nielsen <jgn@dbc.dk>
2012-04-27 14:11:53 +0200

    use jQuery to trigger mousedown event.

--- js/ding_bookmark.js
+++ js/ding_bookmark.js
@@ -1,9 +1,9 @@
 (function($) {
 
-  Drupal.behaviors.ie9bookmarkSubmit = {
+  Drupal.behaviors.bookmarkSubmit = {
       attach:function(context, settings) {
           $('.ding-bookmark-reservation-button', context).click(function() {
-              $('#ding-reservation-reserve-form-' + this.id).submit();
+              $('#ding-reservation-reserve-form-' + this.id + ' input[type=submit]').mousedown();
               return false;
           });
       }
