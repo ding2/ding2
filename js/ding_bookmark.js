07281bd7cb1d6dbaac87d4f7fc569b788ce650e5
Jorgen G. Nielsen <jgn@dbc.dk>
2012-05-08 11:29:42 +0200

    add javascript to reload page after reservation pop-up closes

--- js/ding_bookmark.js
+++ js/ding_bookmark.js
@@ -9,4 +9,24 @@
       }
   };
 
-} (jQuery));
+  // reload page after closing reservation pop-up
+  Drupal.behaviors.reloadBookmarkOnPopupClose = {
+    attach: function(context) {
+      // Capture link clicks.
+      $('body.page-user-bookmarks a.ui-dialog-titlebar-close').each( function(context) {
+        // Unbind existing click behaviors.
+        $(this).unbind('click');
+        // Handle clicks.
+        $(this).click( function(e) {
+          // Remove the default click handler.
+          e.preventDefault();
+          location.reload();
+          return false;
+        });
+      });
+    }
+  };
+
+
+})(jQuery);
+
