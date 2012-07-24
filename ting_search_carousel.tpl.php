31915bd92c77639451b4ea9c2487369af0c9e1ea
Rasmus Frey <rasmus@artz.dk>
2012-07-24 13:35:47 +0200

    Updated settings page with frontend settings making it possible to choose where tabs for search carousel is placed

--- ting_search_carousel.tpl.php
+++ ting_search_carousel.tpl.php
@@ -1,7 +1,12 @@
 <?php
 /**
  * @file
- *
+ * 
+ * 
+ * * Available variables:
+ * - $tab_position: String with settings info, values: top,bottom,left,right.
+ * - $searches: Array with each tab search.
+ * 
  */
 $path = drupal_get_path('module', 'ting_search_carousel');
 
@@ -9,9 +14,9 @@ drupal_add_library('system', 'ui.widget');
 drupal_add_js($path . '/js/jquery.rs.carousel.js');
 drupal_add_js($path . '/js/ting_search_carousel.js');
 drupal_add_css($path . '/css/ting_search_carousel.css');
-
 ?>
 <div class="ting-search-carousel">
+  <?php if ($tab_position != 'bottom') : ?>
   <ul class="search-controller">
     <?php foreach ($searches as $i => $search) : ?>
       <li class="<?php echo ($i == 0) ? 'active' : ''; ?>">
@@ -19,19 +24,28 @@ drupal_add_css($path . '/css/ting_search_carousel.css');
       </li>
     <?php endforeach; ?>
   </ul>
+  <?php endif; ?>
   <div class="ting-search-results">
       <div class="subtitle">
       </div>
       <div class="ting-rs-carousel">
         <div class="rs-carousel-mask">
           <ul class="rs-carousel-runner">
-<li style="list-style:none;padding-top:5em;">
-  <img src="<?php echo $path;?>/images/ajax-loader.gif" />
-</li>
+            <li style="list-style:none;padding-top:5em;">
+              <img src="<?php echo $path;?>/images/ajax-loader.gif" />
+            </li>
           </ul>
         </div>
       </div>
       <div class="clearfix"></div>
   </div>
-
+  <?php if ($tab_position == 'bottom') : ?>
+  <ul class="search-controller">
+    <?php foreach ($searches as $i => $search) : ?>
+      <li class="<?php echo ($i == 0) ? 'active' : ''; ?>">
+        <a href="#"><?php echo $search['title'] ?></a>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+  <?php endif; ?>
 </div>
