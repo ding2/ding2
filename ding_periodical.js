1fe90161231d30247704a44e548953a97a748799
Alexei Goja <pliuwik@gmail.com>
2012-08-13 14:04:14 +0300

    Style fix and popup view for periodical view

--- ding_periodical.js
+++ ding_periodical.js
@@ -5,9 +5,11 @@ $(document).ready(function(){
 
   Drupal.behaviors.dingPeriodicalIssueToggle = {
     attach: function (context, settings) {
-        $('.field-name-ding-periodical-issues .ding-periodical-fold').click(function(){
-	    $(this).parent('.ding-periodical-foldable').children('.item-list').toggle();
-        });
+      $('.field-name-ding-periodical-issues .ding-periodical-fold').click(function(){
+	    $(this).parent('.ding-periodical-foldable').children('.item-list').dialog({
+	      title: Drupal.t('Reserve')
+	    });
+      });
     }
   }    
 }(jQuery));
