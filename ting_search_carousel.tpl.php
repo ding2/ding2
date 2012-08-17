32c74a5a06d36ac0e9cb434d24dce3d3b8b9a961
Brian Jensen <mail@magnify.dk>
2012-08-17 13:17:06 +0200

    Fixed styling, reponsive, IE 8

--- ting_search_carousel.tpl.php
+++ ting_search_carousel.tpl.php
@@ -22,26 +22,30 @@ drupal_add_css($path . '/css/ting_search_carousel.css');
 
 ?>
 
-<div class="rs-carousel<?php echo (variable_get('ting_search_carousel_description_toggle', 0)) ? ' rs-carousel-wide' : ' rs-carousel-compact'; ?>">
-  <div class="rs-carousel-inner">
-    <div class="ajax-loader"></div>
-<?php if (variable_get('ting_search_carousel_description_toggle', 0)) { ?>
-    <div class="rs-carousel-title"></div>
-<?php } ?>
-    <ul class="rs-carousel-runner">
-    </ul>
-  </div>
-  
-  <!-- Only print tabs if there is more than 1 -->
-  <?php if (count($searches) > 1) { ?>
-  <div class="rs-carousel-tabs">
-    <ul>
-      <?php foreach ($searches as $i => $search) : ?>
-        <li class="<?php echo ($i == 0) ? 'active' : ''; ?>">
-          <a href="#"><?php echo $search['title'] ?></a>
-        </li>
-      <?php endforeach; ?>
-    </ul>
-  </div>
+<!-- The wrapper div is important because rs-carousel replaces
+     <div class="rs-carousel" so we can't add more classes -->
+<div class="rs-carousel-wrapper<?php echo (variable_get('ting_search_carousel_description_toggle', 0)) ? ' rs-carousel-wide' : ' rs-carousel-compact'; ?>">
+  <div class="rs-carousel">
+    <div class="rs-carousel-inner">
+      <div class="ajax-loader"></div>
+  <?php if (variable_get('ting_search_carousel_description_toggle', 0)) { ?>
+      <div class="rs-carousel-title"></div>
   <?php } ?>
+      <ul class="rs-carousel-runner">
+      </ul>
+    </div>
+
+    <!-- Only print tabs if there is more than 1 -->
+    <?php if (count($searches) > 1) { ?>
+    <div class="rs-carousel-tabs">
+      <ul>
+        <?php foreach ($searches as $i => $search) : ?>
+          <li class="<?php echo ($i == 0) ? 'active' : ''; ?>">
+            <a href="#"><?php echo $search['title'] ?></a>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    </div>
+    <?php } ?>
+  </div>
 </div>
