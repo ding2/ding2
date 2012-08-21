258b7939a8dddab449c61ef85710ca9f64a0d95d
AnatolyMuntean <am@inlead.dk>
2012-08-21 15:45:54 +0400

    case 925 - Trail spaces.

--- lib/addi-client/AdditionalInformationService.php
+++ lib/addi-client/AdditionalInformationService.php
@@ -15,18 +15,18 @@ class AdditionalInformationService {
   
   public function getByIsbn($isbn) {
     $isbn = str_replace('-', '', $isbn);
-    
+
     $identifiers = $this->collectIdentifiers('isbn', $isbn);
     $response = $this->sendRequest($identifiers);
     return $this->extractAdditionalInformation('isbn', $response);
   }
-  
+
   public function getByFaustNumber($faustNumber) {
     $identifiers = $this->collectIdentifiers('faust', $faustNumber);
     $response = $this->sendRequest($identifiers);
     return $this->extractAdditionalInformation('faust', $response);
   }
-  
+
   protected function collectIdentifiers($idName, $ids) {
     if (!is_array($ids)) {
       $ids = array($ids);
@@ -39,52 +39,52 @@ class AdditionalInformationService {
 
     return $identifiers;
   }
-  
+
   protected function sendRequest($identifiers) {
     $ids = array();
     foreach ($identifiers as $i) {
       $ids = array_merge($ids, array_values($i));
     }
-    
+
     $authInfo = array('authenticationUser' => $this->username,
                       'authenticationGroup' => $this->group,
                       'authenticationPassword' => $this->password);
     $client = new SoapClient($this->wsdlUrl);
-    
+
     $startTime = explode(' ', microtime()); 
     $response = $client->moreInfo(array(
                           'authentication' => $authInfo,
                           'identifier' => $identifiers));
-    
+
     $stopTime = explode(' ', microtime());
     $time = floatval(($stopTime[1]+$stopTime[0]) - ($startTime[1]+$startTime[0]));
-    
+
     //Drupal specific code - consider moving this elsewhere
     if (variable_get('addi_enable_logging', false)) {
       watchdog('addi', 'Completed request (' . round($time, 3) . 's): Ids: %ids', array('%ids' => implode(', ', $ids)), WATCHDOG_DEBUG, 'http://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
     }
-    
+
     if ($response->requestStatus->statusEnum != 'ok') {
       throw new AdditionalInformationServiceException($response->requestStatus->statusEnum . ': ' . $response->requestStatus->errorText);
     }
-        
+
     if (!is_array($response->identifierInformation)) {
       $response->identifierInformation = array($response->identifierInformation); 
     }
-    
+
     return $response;
   }
-  
+
   protected function extractAdditionalInformation($idName, $response) {
     $additionalInformations = array();
-    
+
     foreach ($response->identifierInformation as $info) {
       $thumbnailUrl = $detailUrl = NULL;
       if (isset($info->identifierKnown) && $info->identifierKnown && $info->coverImage) {
         if (!is_array($info->coverImage)) {
           $info->coverImage = array($info->coverImage);
         }
-        
+
         foreach ($info->coverImage as $image) {
           switch ($image->imageSize) {
             case 'thumbnail':
@@ -97,7 +97,7 @@ class AdditionalInformationService {
               // Do nothing other image sizes may appear but ignore them for now
           }
         }     
-  
+
         $additionalInfo = new AdditionalInformation($thumbnailUrl, $detailUrl);
         $additionalInformations[$info->identifier->$idName] = $additionalInfo;
       }
