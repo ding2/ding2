e0044bc20030166cd39a0817da299789cbe1872e
AnatolyMuntean <am@inlead.dk>
2012-08-24 13:35:54 +0400

    case 920 - Fixes after CR.

--- ding_periodical.js
+++ ding_periodical.js
@@ -1,12 +1,12 @@
 (function ($) {
   $(document).ready(function(){
-      $('.field-name-ding-periodical-issues li').children('.item-list').hide();
-      $('.field-name-ding-periodical-issues li').children('.item-list form').hide();
+    $('.field-name-ding-periodical-issues li').children('.item-list').hide();
+    $('.field-name-ding-periodical-issues li').children('.item-list form').hide();
   });
 
   Drupal.behaviors.dingPeriodicalIssueToggle = {
     attach: function (context, settings) {
-      $('.field-name-ding-periodical-issues .ding-periodical-fold').toggle(function(){
+      $('.field-name-ding-periodical-issues .ding-periodical-fold').toggle(function() {
         $(this).next().show();
         $('.page-ting-object .panel-pane.pane-ting-object-ding-availability-holdings').addClass('holdings-collapsed');
         $('.page-ting-object .grid-3 .panel-pane.pane-ting-object-ding-entity-buttons').addClass('holdings-collapsed');
@@ -18,7 +18,7 @@
         $(this).next().toggleClass('expanded-periodicals');
         $(this).parent().toggleClass('expanded-periodicals-parent');
       });
-      $('.ding-periodical-foldable .item-list .ding-periodical-foldable li').click(function(){
+      $('.ding-periodical-foldable .item-list .ding-periodical-foldable li').click(function() {
         $(this).find('form').clone().appendTo($(this));
         $(this).find('form:first').dialog({
           title: Drupal.t('Reserve')
