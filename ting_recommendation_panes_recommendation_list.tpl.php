dcac7c67c688528b21108dc793d4242dd8a4920c
Rune Schjellerup Philosof <rune.git@philosof.dk>
2012-09-04 14:05:47 +0200

    Retrieve Ting_Entities with ding_entity_load instead of the low level ting_get_object.

--- ting_recommendation_panes_recommendation_list.tpl.php
+++ ting_recommendation_panes_recommendation_list.tpl.php
@@ -1,12 +1,10 @@
-<?php if ($objects && (sizeof($objects) > 0)) :?>
-<div class="ding-box-wide">
-  <h3><?php print t("Other did borrow:"); ?></h3>
-  <ul class="ting-recommendation">
-	  <?php foreach ($objects as $i => $object) : ?>
-	    <li class="<?php echo (($i % 2) == 0) ? 'odd' : 'even' ?> <?php echo ((is_int(($i-1) / 4)) || (is_int($i / 4))) ? 'either' : 'or' ; ?>">
-	      <?php echo theme('ting_recommendation_panes_recommendation_list_entry', $object); ?>      
-	    </li>    
-	  <?php endforeach; ?>
-  </ul>
-</div>  
+<?php if ($models && (sizeof($models) > 0)) :?>
+<h3><?php print t("Others borrowed:"); ?></h3>
+<ul class="list simple">
+  <?php foreach ($models as $i => $model) : ?>
+    <li class="<?php echo (($i % 2) == 0) ? 'odd' : 'even' ?>">
+      <?php echo theme('ting_recommendation_panes_recommendation_list_entry', array("model" => $model)); ?>
+    </li>
+  <?php endforeach; ?>
+</ul>
 <?php endif; ?>
--- ting_recommendation_panes_recommendation_list_entry.tpl.php
+++ ting_recommendation_panes_recommendation_list_entry.tpl.php
@@ -1,6 +1,11 @@
-<a href="<?php echo $object->url ?>" title="<?php print check_plain($object->title) ?><?php if (!empty($object->creators_string)) { print ': ' . check_plain($object->creators_string); } ?>">
-  <span class="title"><?php print check_plain($object->title); ?></span>
-  <?php if (!empty($object->creators_string)) { ?>
-    <span class="creator"><?php print check_plain($object->creators_string); ?></span>
+<?php
+$url = url($model['url']);
+$creators_string = implode(", ", $model['creators']);
+$title = $model['title'];
+?>
+<a href="<?php echo $url ?>" title="<?php print check_plain($title) ?><?php if (!empty($creators_string)) { print ': ' . check_plain($creators_string); } ?>">
+  <span class="title"><?php print check_plain($title); ?></span>
+  <?php if (!empty($creators_string)) { ?>
+    <span class="creator"><?php print check_plain($creators_string); ?></span>
   <?php } ?>
 </a>
