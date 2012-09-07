13439899ecb6b042834f543c17daa5102ca609f8
Jesper Kristensen <cableman@linuxdev.dk>
2012-09-07 18:32:52 +0200

    Restructed the coding style

--- templates/ting_search_carousel.tpl.php
+++ templates/ting_search_carousel.tpl.php
@@ -10,30 +10,29 @@
  */
 ?>
 
-<!-- The wrapper div is important because rs-carousel replaces
-     <div class="rs-carousel" so we can't add more classes -->
-<div class="rs-carousel-wrapper<?php echo $toggle_description ? ' rs-carousel-wide' : ' rs-carousel-compact'; ?>">
+<!-- The wrapper div is important because rs-carousel replaces it -->
+<div class="rs-carousel-wrapper <?php echo $toggle_description; ?>">
   <div class="rs-carousel">
     <div class="rs-carousel-inner">
       <div class="ajax-loader"></div>
-      <?php if ($toggle_description) { ?>
+      <?php if ($toggle_description): ?>
         <div class="rs-carousel-title"></div>
-      <?php } ?>
+      <?php endif; ?>
       <ul class="rs-carousel-runner">
       </ul>
     </div>
 
     <!-- Only print tabs if there is more than 1 -->
-    <?php if (count($searches) > 1) { ?>
+    <?php if (count($searches) > 1): ?>
     <div class="rs-carousel-tabs">
       <ul>
-        <?php foreach ($searches as $i => $search) : ?>
+        <?php foreach ($searches as $i => $search): ?>
           <li class="<?php echo ($i == 0) ? 'active' : ''; ?>">
             <a href="#"><?php echo $search['title'] ?></a>
           </li>
         <?php endforeach; ?>
       </ul>
     </div>
-    <?php } ?>
+    <?php endif; ?>
   </div>
 </div>
