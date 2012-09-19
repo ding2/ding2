9137522e966af30353509b6f8819c894797a2277
Kristian Kaa <kaakristian@gmail.com>
2012-09-19 11:36:13 +0200

    Added a inner wrapper for the search field

--- templates/panels-pane--search--form.tpl.php
+++ templates/panels-pane--search--form.tpl.php
@@ -18,12 +18,14 @@
  */
 ?>
 <div class="search-wrapper <?php print $classes; ?>" <?php print $id; ?>>
-<?php if ($admin_links): ?>
-  <?php print $admin_links; ?>
-<?php endif; ?>
+  <div class="search-inner-wrapper">
+    <?php if ($admin_links): ?>
+      <?php print $admin_links; ?>
+    <?php endif; ?>
 
-<?php if ($title): ?>
-  <h2><?php print $title; ?></h2>
-<?php endif; ?> 
-<?php print render($content); ?>
+    <?php if ($title): ?>
+      <h2><?php print $title; ?></h2>
+    <?php endif; ?> 
+    <?php print render($content); ?>
+  </div>
 </div>
