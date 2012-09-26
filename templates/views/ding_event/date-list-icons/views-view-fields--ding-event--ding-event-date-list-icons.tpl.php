556e03d45e152dc6343aac5b0a470664c4e7489f
wiredloose <ea@vejlebib.dk>
2012-09-26 02:43:39 -0500

    added and adjusted template files for event views

---
+++ templates/views/ding_event/date-list-icons/views-view-fields--ding-event--ding-event-date-list-icons.tpl.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @file views-view-fields.tpl.php
+ * Default simple view template to all the fields as a row.
+ *
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ *   - $field->inline: Whether or not the field should be inline.
+ *   - $field->inline_html: either div or span based on the above flag.
+ *   - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
+ *   - $field->wrapper_suffix: The closing tag for the wrapper.
+ *   - $field->separator: an optional separator that may appear before a field.
+ *   - $field->label: The wrap label text to use.
+ *   - $field->label_html: The full HTML of the label to use including
+ *     configured element type.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php foreach ($fields as $id => $field): ?>
+  <?php if($id == 'field_ding_event_library'): ?>
+    <?php print '<div class="content">'; ?>
+  <?php endif; ?>
+  
+  <?php if (!empty($field->separator)): ?>
+    <?php print $field->separator; ?>
+  <?php endif; ?>
+  
+  <?php if($id != 'field_ding_event_library' || ($id == 'field_ding_event_library' && empty($fields['field_ding_event_location']->content)) ): ?>
+    <?php print $field->wrapper_prefix; ?>
+    <?php if($id == 'field_ding_event_library' || $id == 'field_ding_event_location'): ?>
+      <?php print '<i class="icon-home"></i> '; ?>
+    <?php endif; ?>
+    <?php if($id == 'field_ding_event_date_1'): ?>
+      <?php print '<i class="icon-time"></i> '; ?>
+    <?php endif; ?>
+    <?php if($id == 'field_ding_event_price'): ?>
+      <?php print '<i class="icon-credit-card"></i> '; ?>
+    <?php endif; ?>
+    <?php print $field->label_html; ?>
+    <?php print $field->content; ?>
+    <?php print $field->wrapper_suffix; ?>
+  <?php endif; ?>
+  
+  <?php if($id == 'field_ding_event_price'): ?>
+    <?php print "</div>"; ?>
+  <?php endif; ?>
+<?php endforeach; ?>
