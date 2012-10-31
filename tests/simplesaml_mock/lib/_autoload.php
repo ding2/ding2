e23274dadd48b50197a4b14d08f12a16172812b0
Michael Carlsson <mca@dbc.dk>
2012-10-31 16:09:45 +0100

    WAYF capabilities moved to provider.

--- tests/simplesaml_mock/lib/_autoload.php
+++ tests/simplesaml_mock/lib/_autoload.php
@@ -6,35 +6,55 @@
  */
 
 class SimpleSAML_Auth_Simple {
-  private $attributes;
+  private static $attributes = array();
 
   /**
    * Constructor for mock object.
    */
   public function __construct($sp) {
+    $attributes = variable_get('simplesaml_mock');
+
+    if (!$attributes) {
+      self::$attributes = $attributes;
+    }
   }
 
   public function isAuthenticated() {
-    return isset($this->attributes);
+    $this->updateAttributes();
+    return !empty(self::$attributes);
   }
 
   public function getAttributes() {
-    return $this->attributes;
+    $this->updateAttributes();
+    return self::$attributes;
   }
 
   public function requireAuth() {
+    $this->updateAttributes();
 
+    if (empty(self::$attributes)) {
+      drupal_set_message(t('Not WAYF authenticated.'));
+      drupal_goto('<front>');
+    }
   }
 
   public function logout() {
-    unset($this->attributes);
+    variable_del('simplesaml_mock');
+    self::$attributes = array();
+  }
+
+  private function updateAttributes() {
+    if (empty(self::$attributes)) {
+      self::$attributes = variable_get('simplesaml_mock', array());
+    }
   }
 
   /**
    * Helper method for testing.
    */
-  public function setAttributes($attribute_list) {
-    $this->attributes = $attribute_list;
+  public static function setAttributes($attribute_list) {
+    self::$attributes = $attribute_list;
+    variable_set('simplesaml_mock', $attribute_list);
   }
 
 }
