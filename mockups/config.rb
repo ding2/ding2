fa35f17322319fb3fef9434c468afadf4412ace9
Brian Jensen <mail@magnify.dk>
2012-12-11 09:54:11 +0100

    Fixed import of sass files

--- mockups/config.rb
+++ mockups/config.rb
@@ -12,7 +12,7 @@ output_style = :expanded
 environment = :development
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
