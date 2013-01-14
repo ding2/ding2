232ceef015f4604ebaa56a27ddcfde164ef44b18
Rasmus Frey <rasmus@artz.dk>
2013-01-14 11:07:32 +0100

    Fixed typo

--- plugins/layouts/single_row/25_50_25/25-50-25.tpl.php
+++ plugins/layouts/single_row/25_50_25/25-50-25.tpl.php
@@ -8,14 +8,35 @@
  *   panel of the layout.
  * - $css_id: unique id if present.
  */
+
+/**
+ * Add a css class based on wich sidebars have content
+ */
+
+// No sidebars
+if (empty($content['left_sidebar']) && empty($content['right_sidebar'])) {
+  $add_class = 'no-sidebars';
+}
+// Only tertiary content (right sidebar)
+else if (empty($content['left_sidebar']) && !empty($content['right_sidebar'])) {
+  $add_class = 'right-sidebar';
+}
+// Only secondary content (left sidebar)
+else if (!empty($content['left_sidebar']) && empty($content['right_sidebar'])) {
+  $add_class = 'left-sidebar';
+}
+// Both secondary and tertiary content (left and right sidebars)
+else if (!empty($content['left_sidebar']) && !empty($content['right_sidebar'])) {
+  $add_class = 'sidebars';
+}
 ?>
-<div <?php if (!empty($css_id)) { print " id=\"$css_id\""; } ?> class="panel-content-wrapper">
+<div <?php if (!empty($css_id)) { print " id=\"$css_id\""; } ?> class="<?php echo $add_class; ?> panel-content-wrapper">
   <div class="primary-content">
     <div class="grid-inner"><?php print $content['main_content']; ?></div>
   </div>
   <?php if (!empty($content['left_sidebar'])): ?>
     <aside class="secondary-content">
-      <div class="grid-inner"><?php print $content['left_content']; ?></div>
+      <div class="grid-inner"><?php print $content['left_sidebar']; ?></div>
     </aside>
   <?php endif ?>
   <?php if (!empty($content['right_sidebar'])): ?>
