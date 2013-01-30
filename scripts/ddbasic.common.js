f01f72bc3f68218dbac09156f1836fcfc4457f81
Jesper Kristensen <cableman@linuxdev.dk>
2013-01-30 13:58:16 +0100

    Basic clean-up of common js file

--- scripts/ddbasic.common.js
+++ scripts/ddbasic.common.js
@@ -1,77 +1,91 @@
 (function($) {
   $(document).ready(function () {
 
-    // Fixed elements and scroll
+    // Get calculations for elements position on the page and their sizes.
     var menu = $('.navigation-wrapper');
+    var pos = menu.offset();
+    var menu_height = menu.height();
+    var menu_pos_relative = pos.top - menu_height;
 
-		var pos = menu.offset();
-
-    // Take the Drupal toolbar into account
-    var bodyPad = parseInt($('body').css('paddingTop'), 10);
-    if (bodyPad) {
-      pos.top = pos.top - bodyPad;
+    // Take the Drupal toolbar into account and calculate padding.
+    var body_paddding = parseInt($('body').css('paddingTop'), 10);
+    if (body_paddding) {
+      pos.top = pos.top - body_paddding;
     }
 
-    if ($(window).scrollTop() > pos.top-$(menu).height()) {
-      $(menu).addClass('fixed');
+    if ($(window).scrollTop() > (menu_pos_relative)) {
+      menu.addClass('fixed');
 
-      // Only when Drupal toolbar is present
-      if ($(menu).hasClass('fixed') && bodyPad) {
-        $(menu).css('top', bodyPad);
+      // Only when Drupal toolbar is present.
+      if (menu.hasClass('fixed') && body_paddding) {
+        menu.css('top', body_paddding);
       }
 
+      /**
+       * @todo: what does this do !
+       */
       $('.js-fixed-element').each(function() {
         $(this).show();
       });
-
     }
 
+//    console.log(pos);
+//    console.log(menu_height);
 
-		$(window).scroll(function(){
+    $(window).scroll(function(){
+//      console.log($(window).scrollTop());
+//      console.log(menu_pos_relative);
+      if ($(window).scrollTop() > menu_pos_relative) {
+        menu.addClass('fixed');
 
-			if ($(window).scrollTop() > pos.top-$(menu).height()) {
-				$(menu).addClass('fixed');
-        
+        /**
+         * @todo: what does this do !
+         */
         $('.js-fixed-element').each(function() {
           $(this).show();
         });
-
-			} else if($(menu).scrollTop() <= pos.top && $(menu).hasClass('fixed')) {
-        $(menu).removeClass('fixed');
+      }
+      else if (menu.scrollTop() <= pos.top && menu.hasClass('fixed')) {
+        menu.removeClass('fixed');
         $('.js-fixed-element').each(function() {
           $(this).hide();
         });
-
-			}
-
-      // Only when Drupal toolbar is present
-      if ($(menu).hasClass('fixed') && bodyPad) {
-        $(menu).css('top', bodyPad);
-      } else {
-        $(menu).css('top', '');
       }
 
+      // Only when Drupal toolbar is present.
+      if (menu.hasClass('fixed') && body_paddding) {
+        menu.css('top', body_paddding);
+      }
+      else {
+        menu.css('top', '');
+      }
     });
 
 
-    // Toggle element
+    // Toggle element.
     $(".js-show-element").click(function() {
 
-      var $element = $("." + $(this).attr("rel"));
-      var offset = $(this).offset();
+      var self = $(this);
 
-      if ($(this).hasClass("active")) {
-        $(this).removeClass("active");
-      } else {
-        $(this).addClass("active");
+      var $element = $("." + self.attr("rel"));
+      var offset = self.offset();
+
+      /**
+       * @todo is this needed
+       */
+      if (self.hasClass("active")) {
+        self.removeClass("active");
+      }
+      else {
+        self.addClass("active");
       }
 
       $element.toggle();
-
-      $element.css({"top": offset.top + 72, "left": offset.left - 275});
-
+      $element.css({
+        "top": offset.top + 72,
+        "left": offset.left - 275
+      });
     });
-
 
     // Hide element
     $(".js-hide-element").click(function() {
