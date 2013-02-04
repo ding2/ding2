ecad085f75db27fc448c80ce1fae04c0e4e812cb
Brian Jensen <mail@magnify.dk>
2013-02-04 13:56:25 +0100

    Added equal heights script instead (testet in all browsers) and moved function to window load instead (see code comment)

--- scripts/ddbasic.common.js
+++ scripts/ddbasic.common.js
@@ -80,17 +80,17 @@
       ddbasic_login_state = !ddbasic_login_state;
     }
     
-    
-    // Equal heights function
-    $.fn.setAllToMaxHeight = function(){
-      return this.height( Math.max.apply(this, $.map( this , function(e){ return $(e).height() }) ) );
-    }
+  });
+
+  // Add equal heights on $(window).load() instead of $(document).ready()
+  // See: http://www.cssnewbie.com/equalheights-jquery-plugin/#comment-13286
+  $(window).load(function () {
     
     // Set equal heights on front page content
-    $('.main-wrapper .grid-inner').setAllToMaxHeight();
-    
+    $('.main-wrapper .grid-inner').equalHeights();
+
     // Set equal heights on front page attachments
-    $('.attachments-wrapper .grid-inner').setAllToMaxHeight();
-    
+    $('.attachments-wrapper .grid-inner > div').equalHeights();
   });
+
 })(jQuery);
