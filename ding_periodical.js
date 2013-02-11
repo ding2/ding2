553eaae37345a1d61705b9affd663f81af92f2b6
wiredloose <ea@vejlebib.dk>
2013-02-11 15:29:38 +0100

    adding classes to indicate whether foldable periodical volumes are open or closed

--- ding_periodical.js
+++ ding_periodical.js
@@ -1,13 +1,15 @@
 (function ($) {
 $(document).ready(function(){
     $('.field-name-ding-periodical-issues li').children('.item-list').hide();
+    $('.ding-periodical-fold').addClass('expand expand-more')
 });
 
   Drupal.behaviors.dingPeriodicalIssueToggle = {
     attach: function (context, settings) {
         $('.field-name-ding-periodical-issues .ding-periodical-fold').click(function(){
-	    $(this).parent('.ding-periodical-foldable').children('.item-list').toggle();
+          $(this).parent('.ding-periodical-foldable').children('.item-list').toggle();
+          $(this).toggleClass('expand-more').toggleClass('expand-less');
         });
     }
-  }    
+  }
 }(jQuery));
