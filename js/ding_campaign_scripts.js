f1822acc09cce29ec38eea04976084ba460f7380
Alex Semeniuc <as@inlead.dk>
2013-03-26 14:04:30 +0200

    case 2416 - Fixed campaign rule settings.

--- js/ding_campaign_scripts.js
+++ js/ding_campaign_scripts.js
@@ -1,61 +1,45 @@
 (function ($) {
-  campaign_content_click = function(type) {
-    $('#edit-field-camp-text-plain, #edit-field-camp-image, #edit-field-camp-text-full, #edit-field-camp-link').hide();
-
-    switch(type) {
-      case 'plain':
-        $('#edit-field-camp-text-plain, #edit-field-camp-link').show();
-        break;
-      case 'image':
-        $('#edit-field-camp-image, #edit-field-camp-link').show();
-        break;
-      case 'full':
-        $('#edit-field-camp-text-full').show();
-        break;
-    }
-  }
 
   Drupal.behaviors.ding_campaing_init = {
-    bindAutocomplete: function(obj, type) {
-      $(obj).find('input.autocomplete')
+      bindAutocomplete: function(obj, type) {
+        // Add autocomplete bevavior to 'rule value' input.
+        $(obj).find('input.autocomplete')
         .val(Drupal.settings.ding_campaing_init.autocompleteUrl + type)
         .removeClass('autocomplete-processed')
-      .end()
+        .end()
         .find('input.form-text')
         .addClass('form-autocomplete');
-      Drupal.attachBehaviors($(obj));
-    },
-    attach: function(context) {
-      campaign_content_click($('.node-ding_campaign-form #edit-field-camp-settings input[type=radio]:checked').attr('value'));
+        Drupal.attachBehaviors($(obj));
+      },
 
-      $('.node-ding_campaign-form #edit-field-camp-settings input[type=radio]').click(function() {
-        var type = $(this).attr('value');
-        campaign_content_click(type);
-      });
-
-      $('.ding-campaign-rule select').change(function() {
-        if ($(this).selected().attr('value') == 'rule_generic') {
-          $(this).parent().parent().parent().find('.rule-value').hide();
-        }
-        else {
-          $(this).parent().parent().parent().find('.rule-value').show();
-          var select_value = $(this).selected().attr('value');
-          if (select_value == 'rule_path' || select_value == 'rule_term' || select_value == 'rule_generic') {
-            $(this).parent().parent().parent().find('input.form-text').unbind().removeClass('form-autocomplete');
-          } else {
-            Drupal.behaviors.ding_campaing_init.bindAutocomplete(
-              $(this).parent().parent().parent(), 
-              select_value);
+      attach: function (context, settings) {
+        // OnChange event for 'rule type' dropdown.
+        $('.ding-campaign-rule select', context).once('ding_campaign_init').change(function () {
+          var $context = $(this).parent().parent().parent();
+          var value = $(this).selected().val();
+          if (value == 'rule_generic') {
+            // Generic does not need a 'rule value'.
+            $('.rule-value', $context).hide();
           }
-        }
-        $(this).parent().parent().parent().find('input.form-text').val('');
-      });
+          else {
+            // Add/remove autocomplete for 'rule value'.
+            $('.rule-value', $context).show();
 
-      $('#ding-campaign-rules .ding-campaign-rule').each(function() {
-        if ($(this).find('.rule-type select').selected().attr('value') == 'rule_generic') {
-          $(this).find('.rule-value').hide();
-        }
-      });
-    }
-  }
+            // Remove autocomplete.
+            // Needed to prevent duplicating autocomplete behavior.
+            var $obj = $('input.form-text', $context);
+            $obj.unbind().removeClass('form-autocomplete');
+            $('#'+$obj.attr('id')+'-autocomplete-aria-live', $context).remove();
+
+            if (value == 'rule_page' || value == 'rule_taxonomy' || value == 'rule_library') {
+              // Add autocomplete.
+              Drupal.behaviors.ding_campaing_init.bindAutocomplete($context, value);
+            }
+          }
+          // Crear rule value on rule type change.
+          $('input.form-text', $context).val('');
+        });
+      }
+  };
+
 })(jQuery);
