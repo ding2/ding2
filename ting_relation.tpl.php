3ea32844e012034b500d0389753b14eab6a12f14
Alex Semeniuc <as@inlead.dk>
2013-03-27 18:07:18 +0200

    Resolved merge conflict in tpl file

--- ting_relation.tpl.php
+++ ting_relation.tpl.php
@@ -1,36 +1,25 @@
 <?php
 /**
- * @file
- * Template to render ting_releation content.
- */
+* @file
+* Template to render ting_releation content.
+*/
 
-if (!empty($content)) {
-  foreach( $content as $ns => $relations ){
-    if (!empty($relations) && $ns != 'dbcaddi:hasOnlineAccess') { ?>
+if (empty($content)) {
+  return;
+}
+
+foreach( $content as $ns => $relations ){
+  if (!empty($relations) && $ns != 'dbcaddi:hasOnlineAccess') { ?>
     <a name="<?php echo $ns; ?>"></a>
     <div class="<?php print $classes . ' ting-relation-'.drupal_html_class($ns) . ' clearfix'; ?>">
-      <h2><?php print $relations[0]['type']?></h2>
-      <?php foreach($relations as $key=>$relation) { ?>
+    <h2>
+      <?php
+      $relation = reset($relations);
+      print $relation['type'];
+      ?>
+    </h2>
+    <?php foreach($relations as $key=>$relation) { ?>
       <div class="meta">
-        <?php
-          if (isset($relation['title'])) {
-            print '<h3>' . $relation['title'] . '</h3>';
-          }
-          if (isset($relation['year'])) {
-            print '<div>' . $relation['year'] . '</div>';
-          }
-          if (isset($relation['creator'])) {
-            print '<div>' . $relation['creator'] . '</div>';
-          }
-          if (isset($relation['byline'])) {
-            print '<div>' . $relation['byline'] . '</div>';
-          }
-          if (isset($relation['isPartOf'])) {
-            print $relation['isPartOf'];
-          }
-        ?>
-      </div>
-
       <?php
         if (isset($relation['title'])) {
           print '<h3>' . $relation['title'] . '</h3>';
@@ -44,25 +33,36 @@ if (!empty($content)) {
         if (isset($relation['byline'])) {
           print '<div>' . $relation['byline'] . '</div>';
         }
-
-        if( isset($relation['text']) ) {
-          print '<div>' . $relation['text'] . '</div>';
+        if (isset($relation['isPartOf'])) {
+          print $relation['isPartOf'];
         }
+      ?>
+      </div>
 
-        if (isset($relation['online_url'])) {
-          print '<div class="field-type-ting-relation">';
-          print '<div class="field-items rounded-corners">';
-          print render($relation['online_url']);
-          print '</div></div>';
-        }
-
-        if (isset($relation['docbook_link'])) {
-          print render($relation['docbook_link']);
-        }
-
-        print '<div class="clearfix"></div>';
+      <?php
+      if (isset($relation['abstract'])) {
+        print '<div>' . $relation['abstract'] . '</div>';
       }
-      print '</div>';
+
+      if( isset($relation['text']) ) {
+        print '<div>' . $relation['text'] . '</div>';
+      }
+
+      if (!empty($relation['online_url'])) {
+        print '<div class="field-type-ting-relation">';
+        print '<div class="field-items rounded-corners">';
+        $online_url = $relation['online_url'];
+        print render($online_url);
+        print '</div></div>';
+      }
+
+      if (!empty($relation['docbook_link'])) {
+        $docbok_link = $relation['docbook_link'];
+        print render($docbok_link);
+      }
+
+      print '<div class="clearfix"></div>';
     }
-  } 
+    print '</div>';
+  }
 }
