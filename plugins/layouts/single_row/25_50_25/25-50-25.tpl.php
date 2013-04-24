49f48a01b93d4076812b4989ea930385b663565a
Rasmus Frey <rasmus@artz.dk>
2013-04-24 14:17:54 +0200

    Updated default layout with attachments option

--- plugins/layouts/single_row/25_50_25/25-50-25.tpl.php
+++ plugins/layouts/single_row/25_50_25/25-50-25.tpl.php
@@ -30,7 +30,7 @@ else if (!empty($content['left_sidebar']) && !empty($content['right_sidebar'])) 
   $add_class = 'sidebars';
 }
 ?>
-<div <?php if (!empty($css_id)) { print " id=\"$css_id\""; } ?> class="<?php echo $add_class; ?> panel-content-wrapper">
+<div <?php if (!empty($css_id)) { print " id=\"$css_id\""; } ?> class="<?php echo $add_class; ?> ding-default-layout panel-content-wrapper">
   <div class="primary-content">
     <div class="grid-inner"><?php print $content['main_content']; ?></div>
   </div>
@@ -43,5 +43,55 @@ else if (!empty($content['left_sidebar']) && !empty($content['right_sidebar'])) 
     <aside class="tertiary-content">
       <div class="grid-inner"><?php print $content['right_sidebar']; ?></div>
     </aside>
+  <?php endif ?>
+
+  <?php if (!empty($content['attachment_4_1']) || !empty($content['attachment_4_2']) || !empty($content['attachment_4_3']) || !empty($content['attachment_4_4'])): ?>
+    <div class="attachments-wrapper attachments-4-4">
+      <div class="attachment-first">
+        <div class="grid-inner"><?php print $content['attachment_4_1']; ?></div>
+      </div>
+      <div class="attachment-second">
+        <div class="grid-inner"><?php print $content['attachment_4_2']; ?></div>
+      </div>
+      <div class="attachment-third">
+        <div class="grid-inner"><?php print $content['attachment_4_3']; ?></div>
+      </div>
+      <div class="attachment-fourth">
+        <div class="grid-inner"><?php print $content['attachment_4_4']; ?></div>
+      </div>
+    </div>
+  <?php endif ?>
+
+  <?php if (!empty($content['attachment_3_1']) || !empty($content['attachment_3_2']) || !empty($content['attachment_3_3'])): ?>
+    <div class="attachments-wrapper attachments-3-3">
+      <div class="attachment-first">
+        <div class="grid-inner"><?php print $content['attachment_3_1']; ?></div>
+      </div>
+      <div class="attachment-second">
+        <div class="grid-inner"><?php print $content['attachment_3_2']; ?></div>
+      </div>
+      <div class="attachment-third">
+        <div class="grid-inner"><?php print $content['attachment_3_3']; ?></div>
+      </div>
+    </div>
+  <?php endif ?>
+
+  <?php if (!empty($content['attachment_2_1']) || !empty($content['attachment_2_2'])): ?>
+    <div class="attachments-wrapper attachments-2-2">
+      <div class="attachment-first">
+        <div class="grid-inner"><?php print $content['attachment_2_1']; ?></div>
+      </div>
+      <div class="attachment-second">
+        <div class="grid-inner"><?php print $content['attachment_2_2']; ?></div>
+      </div>
+    </div>
+  <?php endif ?>
+
+  <?php if (!empty($content['attachment_1_1'])): ?>
+    <div class="attachments-wrapper attachments-1-1">
+      <div class="attachment-first">
+        <div class="grid-inner"><?php print $content['attachment_1_1']; ?></div>
+      </div>
+    </div>
   <?php endif ?>
 </div>
