7728d0835634e4e10cf11a1969f7cb44abb05886
Rasmus Frey <rasmus@artz.dk>
2013-04-24 09:16:59 +0200

    Change frontpage layout to use primary, secondary, tertiary content wrappers

--- plugins/layouts/frontpage/ding_layout_1/ding-layout-1.tpl.php
+++ plugins/layouts/frontpage/ding_layout_1/ding-layout-1.tpl.php
@@ -8,55 +8,26 @@
  *   panel of the layout.
  * - $css_id: unique id if present.
  */
-
-/**
- * Add a css class based on wich regions have content
- */
-$add_class = '';
-
-if (empty($content['top_secondary'])) {
-  $add_class.= 'no-top-secondary ';
-}
-
-if (empty($content['main_left_right'])) {
-  $add_class.= 'no-main-left-right ';
-}
-
-if (empty($content['main_right_right'])) {
-  $add_class.= 'no-main-right-right ';
-}
 ?>
-<div <?php if (!empty($css_id)) { print " id=\"$css_id\""; } ?> class="<?php echo $add_class; ?> panel-content-wrapper">
+<div <?php if (!empty($css_id)) { print " id=\"$css_id\""; } ?> class="ding-layout-1 panel-content-wrapper">
 
-  <div class="top-wrapper">
-    <div class="top-content">
-      <div class="grid-inner"><?php print $content['top']; ?></div>
+  <?php if (!empty($content['primary'])): ?>
+    <div class="primary-content">
+      <div class="grid-inner"><?php print $content['primary']; ?></div>
     </div>
+  <?php endif ?>
 
-    <?php if (!empty($content['top_secondary'])): ?>
-      <div class="top-secondary">
-        <div class="grid-inner"><?php print $content['top_secondary']; ?></div>
-      </div>
-    <?php endif ?>
-  </div>
-
-  <div class="main-wrapper">
-    <div class="main-content">
-      <div class="grid-inner"><?php print $content['main_content']; ?></div>
+  <?php if (!empty($content['secondary'])): ?>
+    <div class="secondary-content">
+      <div class="grid-inner"><?php print $content['secondary']; ?></div>
     </div>
+  <?php endif ?>
 
-    <?php if (!empty($content['main_left_right'])): ?>
-      <div class="main-left-right">
-        <div class="grid-inner"><?php print $content['main_left_right']; ?></div>
-      </div>
-    <?php endif ?>
-
-    <?php if (!empty($content['main_right_right'])): ?>
-      <div class="main-right-right">
-        <div class="grid-inner"><?php print $content['main_right_right']; ?></div>
-      </div>
-    <?php endif ?>
-  </div>
+  <?php if (!empty($content['tertiary'])): ?>
+    <div class="tertiary-content">
+      <div class="grid-inner"><?php print $content['tertiary']; ?></div>
+    </div>
+  <?php endif ?>
 
   <?php if (!empty($content['attachment_4_1']) || !empty($content['attachment_4_2']) || !empty($content['attachment_4_3']) || !empty($content['attachment_4_4'])): ?>
     <div class="attachments-wrapper attachments-4-4">
