beb49d0f36401cfb23ace9cb7eb5e1a4a084ba25
Brian Jensen <mail@magnify.dk>
2013-04-25 15:12:50 +0200

    Added temporary markup, styling of topbar menu

--- templates/pane-header.tpl.php
+++ templates/pane-header.tpl.php
@@ -1,27 +1,33 @@
-<div class="topbar-inner">
-  <?php if ($logo || $site_name || $site_slogan): ?>
-    <?php if ($logo): ?>
-      <div class="logo">
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        </a>
-      </div>
-    <?php endif; ?>
-    <?php if ($site_name || $site_slogan): ?>
-      <div class="site-name-wrapper">
-        <?php if ($site_name): ?>
-          <h1 class="site-name<?php if ($site_slogan) print '-with-slogan' ?>">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
-              <?php print $site_name; ?>
-            </a>
-          </h1>
-        <?php endif; ?>
-        <?php if ($site_slogan): ?>
-          <div class="site-slogan">
-            <?php print $site_slogan; ?>
-          </div>
-        <?php endif; ?>
-      </div>
-    <?php endif; ?>
+<?php if ($logo || $site_name || $site_slogan): ?>
+  <?php if ($logo): ?>
+    <div class="logo">
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
+        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+      </a>
+    </div>
   <?php endif; ?>
-</div>
+  <?php if ($site_name || $site_slogan): ?>
+    <div class="site-name-wrapper">
+      <?php if ($site_name): ?>
+        <h1 class="site-name<?php if ($site_slogan) print '-with-slogan' ?>">
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
+            <?php print $site_name; ?>
+          </a>
+        </h1>
+      <?php endif; ?>
+      <?php if ($site_slogan): ?>
+        <div class="site-slogan">
+          <?php print $site_slogan; ?>
+        </div>
+      <?php endif; ?>
+    </div>
+  <?php endif; ?>
+<?php endif; ?>
+
+<!-- Temporary until my local version works again -->
+
+<ul class="topbar-menu">
+  <li><a href="/search" class="topbar-menu-item-active"><span><?php print t('Search'); ?></span></a></li>
+  <li><a href="/user" class="topbar-menu-item"><span><?php print t('Log in / create user'); ?></span></a></li>
+  <li><a href="#" class="topbar-menu-item"><span><?php print t('Menu'); ?></span></a></li>
+</ul>
