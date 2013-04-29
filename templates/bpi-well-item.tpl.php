0079eb6a973e8d50336d5911c90bd0364fa0f465
Anatoly Muntean <am@inlead.dk>
2013-04-29 12:19:45 +0300

    case 2674 - Pass URL arguments for filtering. Docblocks.

--- templates/bpi-well-item.tpl.php
+++ templates/bpi-well-item.tpl.php
@@ -3,6 +3,7 @@
  * @file
  * Single item template.
  */
+
 ?>
 <div class="bpi-item bpi-single-well-item">
   <h3 class="item-title"><?php echo $item['title']; ?></h3>
@@ -12,21 +13,21 @@
     <div class="details-left">
       <p class="item-details-author">
         <span class="item-details-author-label details-label"><?php echo t('Author'); ?>: </span>
-        <span class="item-details-author-value details-value"><?php echo $item['author']; ?></span>
+        <span class="item-details-author-value details-value"><?php echo l($item['author'], 'admin/bpi', array('query' => _bpi_build_query('author', $item['author']))); ?></span>
       </p>
       <p class="item-details-agency">
         <span class="item-details-agency-label details-label"><?php echo t('Agency'); ?>: </span>
-        <span class="item-details-agency-value details-value"><?php echo $item['agency']; ?></span>
+        <span class="item-details-agency-value details-value"><?php echo l($item['agency'], 'admin/bpi', array('query' => _bpi_build_query('agency', $item['agency']))); ?></span>
       </p>
     </div>
     <div class="details-right">
       <p class="item-details-category">
         <span class="item-details-category-label details-label"><?php echo t('Category'); ?>: </span>
-        <span class="item-details-category-value details-value"><?php echo $item['category']; ?></span>
+        <span class="item-details-category-value details-value"><?php echo l($item['category'], 'admin/bpi', array('query' => _bpi_build_query('category', $item['category']))); ?></span>
       </p>
       <p class="item-details-audience">
         <span class="item-details-audience-label details-label"><?php echo t('Audience'); ?>: </span>
-        <span class="item-details-audience-value details-value"><?php echo $item['audience']; ?></span>
+        <span class="item-details-audience-value details-value"><?php echo l($item['audience'], 'admin/bpi', array('query' => _bpi_build_query('audience', $item['audience']))); ?></span>
       </p>
     </div>
   </div>
