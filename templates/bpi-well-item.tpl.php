f1aa7481bc4de1e50313947ae9fbbe6b176cbf00
Anatoly Muntean <am@inlead.dk>
2013-04-29 13:20:38 +0300

    case 2674 - Filters markup and logic.  Translatabled mapping.

--- templates/bpi-well-item.tpl.php
+++ templates/bpi-well-item.tpl.php
@@ -12,21 +12,21 @@
   <div class="item-filters item-details">
     <div class="details-left">
       <p class="item-details-author">
-        <span class="item-details-author-label details-label"><?php echo t('Author'); ?>: </span>
+        <span class="item-details-author-label details-label"><?php echo bpi_label_mapper('author'); ?>: </span>
         <span class="item-details-author-value details-value"><?php echo l($item['author'], 'admin/bpi', array('query' => _bpi_build_query('author', $item['author']))); ?></span>
       </p>
       <p class="item-details-agency">
-        <span class="item-details-agency-label details-label"><?php echo t('Agency'); ?>: </span>
+        <span class="item-details-agency-label details-label"><?php echo bpi_label_mapper('agency'); ?>: </span>
         <span class="item-details-agency-value details-value"><?php echo l($item['agency'], 'admin/bpi', array('query' => _bpi_build_query('agency', $item['agency']))); ?></span>
       </p>
     </div>
     <div class="details-right">
       <p class="item-details-category">
-        <span class="item-details-category-label details-label"><?php echo t('Category'); ?>: </span>
+        <span class="item-details-category-label details-label"><?php echo bpi_label_mapper('category'); ?>: </span>
         <span class="item-details-category-value details-value"><?php echo l($item['category'], 'admin/bpi', array('query' => _bpi_build_query('category', $item['category']))); ?></span>
       </p>
       <p class="item-details-audience">
-        <span class="item-details-audience-label details-label"><?php echo t('Audience'); ?>: </span>
+        <span class="item-details-audience-label details-label"><?php echo bpi_label_mapper('audience'); ?>: </span>
         <span class="item-details-audience-value details-value"><?php echo l($item['audience'], 'admin/bpi', array('query' => _bpi_build_query('audience', $item['audience']))); ?></span>
       </p>
     </div>
