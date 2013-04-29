cfd62383b54b08495a95d29cb35fcc25afa7cfa4
Anatoly Muntean <am@inlead.dk>
2013-04-29 13:47:38 +0300

    case 2674 - Remove logic for filters.

--- templates/bpi-filter-item.tpl.php
+++ templates/bpi-filter-item.tpl.php
@@ -8,6 +8,6 @@
   <p class="bpi-filter-item">
     <span class="bpi-filter-label"><?php echo bpi_label_mapper($filter['name']); ?>: </span>
     <span class="bpi-filter-value"><?php echo l($filter['value'], 'admin/bpi', array('query' => array(SEARCH_PHRASE_KEY => $filter['search_value']))); ?></span>
-    <span class="bpi-filter-remove"><?php echo l('<img src=/' . drupal_get_path('module', 'bpi') . '/images/cross.png' . ' alt="" width="16" height="16" />', $filter['search_stripped_filter'], array('html' => TRUE, 'attributes' => array('class' => array('bpi-filter-remove-cross')))); ?></span>
+    <span class="bpi-filter-remove"><?php echo l('<img src=/' . drupal_get_path('module', 'bpi') . '/images/cross.png' . ' alt="" width="16" height="16" />', 'admin/bpi', array('query' => $filter['search_stripped_filter'], 'html' => TRUE, 'attributes' => array('class' => array('bpi-filter-remove-cross')))); ?></span>
   </p>
 </div>
