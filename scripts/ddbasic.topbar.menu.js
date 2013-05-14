57fc2dcfab5655029817275153cf5f212a4025e8
Rasmus Frey <rasmus@artz.dk>
2013-05-14 13:58:34 +0200

    Added check for touch device to avoid body margin-top being added

--- scripts/ddbasic.topbar.menu.js
+++ scripts/ddbasic.topbar.menu.js
@@ -5,6 +5,14 @@
 
 (function($) {
 
+  /**
+   * Private: Check is the device have support for touch events.
+   */
+  function _is_touch_device() {
+    // First part work in most browser the last in IE 10.
+    return !!('ontouchstart' in window) || !!('onmsgesturechange' in window);
+  }
+
   /*
    * Toggle topbar menu items.
    */
@@ -146,12 +154,16 @@
       if (top > header_pos_relative && !header_fixed) {
         header.addClass('js-fixed');
         header_fixed = true;
-        $('.content-wrapper').css('margin-top', header.height() + 20);
+        if (!_is_touch_device()) {
+          $('.content-wrapper').css('margin-top', header.height() + 20);
+        }
       }
       else if (top < header_pos_relative && header_fixed) {
         header.removeClass('js-fixed');
         header_fixed = false;
-        $('.content-wrapper').css('margin-top', 20);
+        if (!_is_touch_device()) {
+          $('.content-wrapper').css('margin-top', 20);
+        }
       }
     });
 
