e8085902bd31c2c705fbfe11f87d40669e1a3921
Brian Jensen <mail@magnify.dk>
2013-06-04 14:40:50 +0200

    Removed unused equal heights script

--- scripts/ddbasic.common.js
+++ scripts/ddbasic.common.js
@@ -42,27 +42,15 @@
 
   // When ready start the magic
   $(document).ready(function () {
-
     // Toggle opening hours
     toggle_opening_hours();
 
-    // Toggle for footer menu
+    // Toggle footer menu
     $('.footer .pane-title').on('click', function() {
       var element = $(this).parent();
       $('.menu', element).toggle();
       $(this).toggleClass('js-toggled');
     });
-  });
-
-  // Add equal heights on $(window).load() instead of $(document).ready()
-  // See: http://www.cssnewbie.com/equalheights-jquery-plugin/#comment-13286
-  $(window).load(function () {
-
-    // Set equal heights on front page content
-    $('.main-wrapper .grid-inner').equalHeights();
-
-    // Set equal heights on front page attachments
-    $('.attachments-wrapper .grid-inner > div').equalHeights();
   });
 
 })(jQuery);
