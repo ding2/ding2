82ca3994a4142e8b3af74f56d0053459335e3e1b
Jesper Kristensen <cableman@linuxdev.dk>
2013-07-10 17:46:23 +0200

    Changed the module to use render arrays

--- templates/ding_adhl_frontend_recommendation_list.tpl.php
+++ templates/ding_adhl_frontend_recommendation_list.tpl.php
@@ -1,18 +1,20 @@
 <?php
 /**
  * @file
- * TODO: Having odd/even implemented here seems like a very one-off solution.
- * Could our base theme either provide this as an option for theme('item_list')
- * or provide the necessary styling using nth-child selectors?
+ * Default template for the recommandation item list.
+ *
+ * Available variables:
+ * - $items: Render array with recommandated Ting Entities.
+ * - $type: The list type.
+ *
+ * Other variables:
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
  */
 ?>
-<?php if (!empty($recommendations)) :?>
-<h3><?php print t("Others borrowed:"); ?></h3>
-<ul>
-  <?php foreach ($recommendations as $i => $recommendation) : ?>
-    <li class="<?php echo (($i % 2) == 0) ? 'odd' : 'even' ?>">
-      <?php echo theme('ding_adhl_frontend_recommendation_list_entry', array("recommendation" => $recommendation)); ?>
-    </li>
+<h3><?php print t("Others borrowed"); ?></h3>
+<<?php print $type; ?>>
+  <?php foreach ($items as $item) : ?>
+    <?php print drupal_render($item); ?>
   <?php endforeach; ?>
-</ul>
-<?php endif; ?>
+</<?php print $type; ?>>
