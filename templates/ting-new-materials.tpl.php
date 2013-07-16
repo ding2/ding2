dc3f7d3bf4208802e2a48602525e1ea74c1e8141
Jesper Kristensen <cableman@linuxdev.dk>
2013-07-16 11:38:37 +0200

    Added some comments and fixed render arrays

--- templates/ting-new-materials.tpl.php
+++ templates/ting-new-materials.tpl.php
@@ -16,7 +16,7 @@
   </div>
 <?php endif; ?>
 <?php if ($results) : ?>
-  <?php print $results; ?>
+  <?php print drupal_render($results); ?>
 <?php else : ?>
   <div class="no-results-this-period">
     <?php print t('There were no new materials in this period'); ?>
