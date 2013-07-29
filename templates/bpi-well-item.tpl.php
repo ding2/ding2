f67662cbe788e43668213ae75e0c9af83f0cae57
Bunulu Andrei <bunulu@gmail.com>
2013-07-29 11:44:49 +0000

    case-3104 - Bpi theme changes

--- templates/bpi-well-item.tpl.php
+++ templates/bpi-well-item.tpl.php
@@ -12,63 +12,58 @@ drupal_add_library('system', 'ui.dialog');
   <table>
     <thead>
       <tr>
-        <th>
-          <?php echo $item['title']; ?>
+        <th width="30%">
+          Title
         </th>
         <th>
-          <?php echo $item['date']; ?>
+          Date
         </th>
-        <th colspan="2">
-          <?php echo $item['teaser']; ?>
+        <th>
+          Details
+        </th>
+        <th>
+          Photos
+        </th>
+        <th>
+          Actions
         </th>
       </tr>
     </thead>
     <tbody>
       <tr>
         <td>
-          <?php echo bpi_label_mapper('author'); ?>:
+          <?php echo $item['title']; ?>
+          <?php echo $item['teaser']; ?>
         </td>
         <td>
-          <?php echo l($item['author'], 'admin/bpi', array('query' => _bpi_build_query('author', $item['author']))); ?>
+          <?php echo $item['date']; ?>
         </td>
-        <td>
+        <td width="20%">
           <?php echo bpi_label_mapper('category'); ?>:
-        </td>
-        <td>
-          <?php echo l($item['category'], 'admin/bpi', array('query' => _bpi_build_query('category', $item['category']))); ?>
-        </td>
-      </tr>
-      <tr>
-        <td>
+          <?php echo l($item['category'], 'admin/bpi', array('query' => _bpi_build_query('category', $item['category']))); ?> <br>
+          <?php echo bpi_label_mapper('author'); ?>:
+          <?php echo l($item['author'], 'admin/bpi', array('query' => _bpi_build_query('author', $item['author']))); ?> <br>
           <?php echo bpi_label_mapper('agency'); ?>:
-        </td>
-        <td>
-          <?php echo l($item['agency'], 'admin/bpi', array('query' => _bpi_build_query('agency', $item['agency_id']))); ?>
-        </td>
-        <td>
+          <?php echo l($item['agency'], 'admin/bpi', array('query' => _bpi_build_query('agency', $item['agency_id']))); ?> <br>
           <?php echo bpi_label_mapper('audience'); ?>:
+          <?php echo l($item['audience'], 'admin/bpi', array('query' => _bpi_build_query('audience', $item['audience']))); ?>
+        </td>
+        <td align="center">
+          <?php
+            $variables = array();
+            $photos_str = (count($item['assets'])>0) ? t('Photos available for content') : t('No photos available for content');
+            $variables['path'] = drupal_get_path('module', 'bpi') . '/images/' . ((count($item['assets'])>0) ? 'photos' : 'no_photos') . '.png' ;
+            $variables['alt'] = $photos_str;
+            $variables['title'] = $photos_str;
+            $variables['attributes'] = array();
+            echo theme_image($variables);
+          ?>
         </td>
         <td>
-          <?php echo l($item['audience'], 'admin/bpi', array('query' => _bpi_build_query('audience', $item['audience']))); ?>
+          <?php echo l(t('Preview'), 'admin/bpi/preview/nojs/' . $item['bpi_id'], array('attributes' => array('class' => 'use-ajax'))); ?>
+          <?php echo l(t('Syndicate'), 'admin/bpi/syndicate/' . $item['bpi_id']); ?>
         </td>
       </tr>
     </tbody>
   </table>
-  <div class="item-indicate item-indicate-images">
-    <?php
-      $variables = array();
-      $photos_str = (count($item['assets'])>0) ? t('Photos available for content') : t('No photos available for content');
-      $variables['path'] = drupal_get_path('module', 'bpi') . '/images/' . ((count($item['assets'])>0) ? 'photos' : 'no_photos') . '.png' ;
-      $variables['alt'] = $photos_str;
-      $variables['title'] = $photos_str;
-      $variables['attributes'] = array();
-      echo theme_image($variables);
-    ?>
-  </div>
-  <p class="item-action item-action-preview">
-    <?php echo l(t('Preview'), 'admin/bpi/preview/nojs/' . $item['bpi_id'], array('attributes' => array('class' => 'use-ajax'))); ?>
-  </p>
-  <p class="item-action item-action-syndicate">
-    <?php echo l(t('Syndicate'), 'admin/bpi/syndicate/' . $item['bpi_id']); ?>
-  </p>
 </div>
