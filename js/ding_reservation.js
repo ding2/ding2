ad8974068006f727a010acd789184ea54b1ad5bd
Jesper Kristensen <cableman@linuxdev.dk>
2013-09-02 15:56:06 +0200

    Added checkbox count to buttons on the reservation list

--- js/ding_reservation.js
+++ js/ding_reservation.js
@@ -1,9 +1,9 @@
 /**
- * Javascript that adds the extra select all checkbox option to the titles on
- * the user reservation page.
+ * Handle reservation checkboxes select all and select count on buttons.
  */
 (function ($) {
   $(document).ready(function($) {
+    // Handle select all checkboxes.
     $('.select-all-checkbox').click(function() {
       var checkboxes = $('input[type=checkbox]', $(this).closest('form'));
       if ($(this).prop('checked')) {
@@ -12,6 +12,29 @@
       else {
         checkboxes.prop('checked', false);
       }
+      checkboxes.change();
     });
+
+    // Handle checkbox button count.
+    $('.material_item input[type=checkbox]').change(function() {
+      var form = $(this).closest('form');
+      var buttons = $('input[type=submit]', form);
+      var count = $('.material_item input[type=checkbox]:checked', form).length;
+      update_buttons(buttons, count);
+
+      // Handle all checkbox checked state, so if no all selected the checkbox
+      // is not checked.
+      if ($('.material_item input[type=checkbox]', form).length != count) {
+        $('.select-all-checkbox', form).prop('checked', false);
+      }
+    })
+
+    // Update count string on the buttons.
+    function update_buttons(buttons, count) {
+     buttons.each(function(index) {
+       var btn = $(buttons[index]);
+       btn.val(btn.val().replace(/\(\d+\)/, '(' + count + ')'));
+     });
+    }
   });
 })(jQuery);
