14c3838183114912d0200a02019a0cf1624c5467
Jesper Kristensen <cableman@linuxdev.dk>
2013-09-04 10:53:14 +0200

    Ensured that check all checkbox is checked when all reservation are selected

--- js/ding_reservation.js
+++ js/ding_reservation.js
@@ -22,10 +22,13 @@
       var count = $('.material-item input[type=checkbox]:checked', form).length;
       update_buttons(buttons, count);
 
-      // Handle all checkbox checked state, so if no all selected the checkbox
-      // is not checked.
+      // Handle all checkbox checked state, so if not are all selected the
+      // checkbox is not checked.
       if ($('.material-item input[type=checkbox]', form).length != count) {
         $('.select-all input[type=checkbox]', form).prop('checked', false);
+      }
+      else {
+        $('.select-all input[type=checkbox]', form).prop('checked', true);
       }
     })
 
