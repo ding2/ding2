d84e477415cce9691180af2c3c6363eb8c8aac30
Jesper Kristensen <cableman@linuxdev.dk>
2013-10-20 12:02:32 +0200

    Added missing javascript code delete when cleaning up the code to use material list

--- js/ding_bookmark.js
+++ js/ding_bookmark.js
@@ -1,5 +1,25 @@
 (function($) {
   "use strict";
+
+  // Reload page after closing reservation pop-up.
+  Drupal.behaviors.reloadBookmarkOnPopupClose = {
+    attach: function(context) {
+      // Capture link clicks.
+      $('body.page-user-bookmarks a.ui-dialog-titlebar-close').each( function(context) {
+        // Unbind existing click behaviors.
+        $(this).unbind('click');
+        // Handle clicks.
+        $(this).click( function(e) {
+          // Remove the default click handler.
+          e.preventDefault();
+          location.reload();
+          return false;
+        });
+      });
+    }
+  };
+
+  // Handle the material list select all behaviour and checkboxes.
   $('document').ready(function() {
     // Ensure that all checkboxes are not checked (if user reloads the page
     // etc.).
