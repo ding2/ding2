4f4f04763cdfd7449bbc4626f084d6176567060b
Jesper Kristensen <cableman@linuxdev.dk>
2013-11-05 11:08:55 +0100

    Change records per page to use submit function

--- js/ting_search_per_page.js
+++ js/ting_search_per_page.js
@@ -4,21 +4,7 @@
   $(document).ready(function() {
     $('.pane-search-per-page select').change(function() {
       // Reloads the page when new size is selected in the drop-down.
-      window.location = updateQueryStringParameter(window.location.pathname, 'size', $(this).val());
+      $('#ting-search-per-page-form').trigger("submit");
     });
   });
-
-  /**
-   * Updates the query string parameter.
-   */
-  function updateQueryStringParameter(uri, key, value) {
-    var re = new RegExp("([?|&])" + key + "=.*?(&|$)", "i");
-    var separator = uri.indexOf('?') !== -1 ? "&" : "?";
-    if (uri.match(re)) {
-      return uri.replace(re, '$1' + key + "=" + value + '$2');
-    }
-    else {
-      return uri + separator + key + "=" + value;
-    }
-  }
 }(jQuery));
