8aa2c538523224e996257a7dd0635f58136a2aa0
Jesper Kristensen <cableman@linuxdev.dk>
2013-11-06 15:28:28 +0100

    Added the new overlay for search

--- js/ting_search.js
+++ js/ting_search.js
@@ -2,17 +2,18 @@
   "use strict";
 
   $(document).ready(function() {
+    var $overlay = $('<div class="search-overlay--wrapper"><div class="search-overlay--inner"><i class="icon-spinner icon-spin search-overlay--icon"></i><p class="search-overlay--text">' + Drupal.t('Searching please wait...') + '</p></div></div>');
     // Add spinner to search field when searching the data well.
     $('input[name="search_block_form"]').keydown(function(event) {
       // When enter is hit in the search form.
       if (event.which == 13) {
-        ting_search_toggle_spinner(true);
+        $('body').prepend($overlay);
       }
     });
 
     // Hook into the search button as well.
     $('#search-block-form input[type="submit"]').click(function() {
-      ting_search_toggle_spinner(true);
+      $('body').prepend($overlay);
 
       // Continue normal click event.
       $(this).trigger('click');
