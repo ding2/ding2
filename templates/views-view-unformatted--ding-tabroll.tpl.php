ff4791947835334b42ead9cc08c491c8135ee8ac
Jesper Kristensen <cableman@linuxdev.dk>
2013-11-27 14:16:17 +0100

    Updated coding style in tpl's

--- templates/views-view-unformatted--ding-tabroll.tpl.php
+++ templates/views-view-unformatted--ding-tabroll.tpl.php
@@ -12,10 +12,11 @@
     <ul class="ui-tabs-nav">
       <?php
         for ($i = 0; $i < min(5, count($view->result)); $i++) {
-          if($i == 0) {
-            print '<li class="ui-tabs-nav-item  ui-tabs-selected"><a href="#fragment-'.$i.' ">' .$view->result[$i]->node_title .'</a></li>';
-          } else {
-            print '<li class="ui-tabs-nav-item count-'.$i.'"><a href="#fragment-'.$i.' ">' .$view->result[$i]->node_title .'</a></li>';
+          if ($i == 0) {
+            print '<li class="ui-tabs-nav-item  ui-tabs-selected"><a href="#fragment-' . $i . ' ">' . $view->result[$i]->node_title . '</a></li>';
+          }
+          else {
+            print '<li class="ui-tabs-nav-item count-' . $i . '"><a href="#fragment-' . $i . ' ">' . $view->result[$i]->node_title . '</a></li>';
           }
         }
       ?>
