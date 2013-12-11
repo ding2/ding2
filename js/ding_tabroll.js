7888505fcaed596460f46b9f138b381b455d8358
Jesper Kristensen <cableman@linuxdev.dk>
2013-12-11 15:58:22 +0100

    Cleaned up the JS and tpl files

--- js/ding_tabroll.js
+++ js/ding_tabroll.js
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * Enables the tabs to change automatically and add handler to capture click
+ * events on the tabs.
+ */
 (function ($) {
 
   $(document).ready(function($) {
@@ -5,13 +10,28 @@
 
     // Check if the tabs lib is loaded before trying to call it.
     if ($.fn.tabs) {
-      tabroll.tabs({fx: {opacity: "toggle"}}).tabs("rotate", 5000, false);
+      tabroll.tabs().tabs("rotate", 5000, false);
+
+      // Stop tabs rotate when mouse is over the tab roll.
+      tabroll.mouseover(function(){
+        tabroll.tabs('rotate', 0, false);
+      });
+
+      // Start tabs rotate when mouse is out.
+      tabroll.mouseout(function(){
+        tabroll.tabs().tabs("rotate", 5000, false);
+      });
     }
 
     // Add click event to select tabs options.
     $('.tabroll-tabs-item', tabroll).click(function(e) {
       e.preventDefault();
       tabroll.tabs("select", $(this).index());
+    });
+
+    // Hook into click events in the responsive mobile selector.
+    $('.ding-tabroll-select-tabs').live('change', function() {
+      tabroll.tabs("select", $(this).prop('selectedIndex'));
     });
   });
 
