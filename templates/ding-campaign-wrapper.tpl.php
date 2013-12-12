94fdc1be3f24d9538ee8dead4eda696fdcc1ecf2
Jesper Kristensen <cableman@linuxdev.dk>
2013-12-12 20:27:50 +0100

    Fixed classes in tpl files

--- templates/ding-campaign-wrapper.tpl.php
+++ templates/ding-campaign-wrapper.tpl.php
@@ -4,6 +4,6 @@
  * Default template for image campaigns.
  */
 ?>
-<div class="ding-campaing-wrapper">
+<div class="ding-campaign-wrapper">
   <?php print drupal_render($content); ?>
 </div>
--- templates/ding-campaign.tpl.php
+++ templates/ding-campaign.tpl.php
@@ -8,11 +8,12 @@
  * Variables:
  *  - $type: The type of campaign (image, plain, text).
  *  - $link: Link to internal or external source.
- *  - $image: The image from the campaign.
+ *  - $image: The image from the campaign, if link present the image is wrapped
+ *            in the link.
  *  - $text: Campaign text.
  */
 ?>
-<div class="ding-campaign--content">
+<div class="ding-campaign ding-campaign--content">
   <?php if (isset($link)) : ?>
     <?php print drupal_render($link); ?>
   <?php endif; ?>
