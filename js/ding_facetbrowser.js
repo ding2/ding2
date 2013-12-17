5f8c33047d7896b686928d7dde67e067ffbc9b25
Thomas Johansen <thsj@aarhus.dk>
2013-12-17 15:10:12 +0100

    Fixed number_of_terms issue to show less facets

--- js/ding_facetbrowser.js
+++ js/ding_facetbrowser.js
@@ -106,9 +106,10 @@ function ding_facetbrowser_fold_facet_group() {
     facetGroup.find('.form-type-checkbox.unselected-checkbox:' + (clickedKey.id == 'expand_more' ? 'hidden': 'visible')).each(function(count, facetElement) {
       if (clickedKey.id == 'expand_more' && count < Drupal.settings.ding_facetbrowser.number_of_terms) {
         $(facetElement).slideDown('fast', function() {
-          if (facetGroup.find('.form-type-checkbox.unselected-checkbox:visible').size() >= Drupal.settings.dingFacetBrowser.number_of_terms &&
+            console.log(Drupal.settings.ding_facetbrowser.number_of_terms);
+          if (facetGroup.find('.form-type-checkbox.unselected-checkbox:visible').size() >= Drupal.settings.ding_facetbrowser.number_of_terms &&
               facetGroup.find('#expand_less').size() === 0 &&
-              count % Drupal.settings.dingFacetBrowser.number_of_terms === 0) {
+              count % Drupal.settings.ding_facetbrowser.number_of_terms === 0) {
             facetGroup.append('<a href="javascript:void;" class="expand expand-less" id="expand_less">' + Drupal.t('Show less') + '</a>');
           }
         });
