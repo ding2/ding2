690097a6c4a8b6098dc821f4d742b987fc31314a
holt83 <simonholt83@gmail.com>
2014-01-29 06:17:20 -0800

    Change in terminology

--- ting_search_context.js
+++ ting_search_context.js
@@ -4,12 +4,16 @@
 
 		attach: function(context, settings) {
 			$("#edit-field-ting-search-context").once(function() {
-				$(".field_ting_search_context_reset_field").click(function(e) {
+				// Hide rating select box on the last field item.
+				$("#field-ting-search-context-values tr:last-child td:nth-child(2) div:nth-child(2)").hide();
+
+				// CLick handler for the reset button.
+				$(".field_ting_search_context_remove").click(function(e) {
 					e.preventDefault();
-					var weightFormItem = $(this).prev();
-					var categoryFormItem = weightFormItem.prev();
-					weightFormItem.find("select").val(0);
-					categoryFormItem.find("select").val(0);
+					var ratingFormItem = $(this).prev();
+					var nameFormItem = ratingFormItem.prev();
+					ratingFormItem.hide().find("select").val(1);
+					nameFormItem.find("select").val(0);
 				});
 			});
 		}
