0c90cd72e7e93ab164e0b8cc1744d252a69028b4
holt83 <simonholt83@gmail.com>
2014-01-29 06:51:27 -0800

    Updates to admin ui

--- ting_search_context.js
+++ ting_search_context.js
@@ -6,13 +6,17 @@
 			$("#edit-field-ting-search-context").once(function() {
 				// Hide rating select box on the last field item.
 				$("#field-ting-search-context-values tr:last-child td:nth-child(2) div:nth-child(2)").hide();
-
+				// Change hanlder on context name select box.
+				$(".field_ting_search_context_name").change(function(e) {
+					var ratingFormItem = $(this).parent().next();
+					$(this).val() == 0 ? ratingFormItem.hide() : ratingFormItem.show();
+				});
 				// CLick handler for the reset button.
 				$(".field_ting_search_context_remove").click(function(e) {
 					e.preventDefault();
 					var ratingFormItem = $(this).prev();
 					var nameFormItem = ratingFormItem.prev();
-					ratingFormItem.hide().find("select").val(1);
+					ratingFormItem.hide();
 					nameFormItem.find("select").val(0);
 				});
 			});
