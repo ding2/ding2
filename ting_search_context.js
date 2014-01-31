78a80b503d3e17e440d5e1662c5501f38819f886
holt83 <simonholt83@gmail.com>
2014-01-31 01:09:53 -0800

    Added context to jquery calls

--- ting_search_context.js
+++ ting_search_context.js
@@ -4,14 +4,14 @@
 
 		attach: function(context, settings) {
 			// Hide rating select box on the last field item.
-			$("#edit-field-ting-search-context tr:last-child td:nth-child(2) div:nth-child(2)").hide();
+			$("#edit-field-ting-search-context tr:last-child td:nth-child(2) div:nth-child(2)", context).hide();
 			// Change hanlder on context name select box.
-			$(".field_ting_search_context_name").change(function(e) {
+			$(".field_ting_search_context_name", context).change(function(e) {
 				var ratingFormItem = $(this).parent().next();
 				$(this).val() == 0 ? ratingFormItem.hide() : ratingFormItem.show();
 			});			
 			// CLick handler for the reset button.
-			$(".field_ting_search_context_remove").click(function(e) {
+			$(".field_ting_search_context_remove", context).click(function(e) {
 				e.preventDefault();
 				var ratingFormItem = $(this).prev();
 				var nameFormItem = ratingFormItem.prev();
