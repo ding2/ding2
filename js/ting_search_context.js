b515a4ec874116e2d6d9d89a26abf271ed272d0a
Arni <al@randersbib.dk>
2014-02-05 10:32:10 +0100

    Initial development

---
+++ js/ting_search_context.js
@@ -0,0 +1,29 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function($) {
+
+  Drupal.behaviors.tingSearchContextGetNodes = {		
+    attach: function(context, settings) {
+      $.ajax({
+        type: "POST",
+        url: Drupal.settings.basePath + "ting/searchcontext/ajax",
+        dataType: "json",
+        data: {
+          "search_context" : settings.ting_search_context
+        },
+        success: function (data) {
+          if (data != null) {
+            $("#ting-search-context").html(data);
+          }
+        }
+      });				
+      
+    }
+  }
+
+})(jQuery);
+
+
