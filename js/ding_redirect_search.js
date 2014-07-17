c8dbc50fabd51405e84fe69b71f1d4dff570e6a4
Anders Bryrup <andersbryrup@gmail.com>
2014-07-17 11:33:24 +0200

    Use the same dialog to popup when doing a search and being redirected.

--- js/ding_redirect_search.js
+++ js/ding_redirect_search.js
@@ -7,8 +7,13 @@
    */
   Drupal.behaviors.ding_redirect_search_form_submit = {
     attach: function (context, settings) {
-      $('#search-block-form input[type="submit"]', context).click(function(event) {
+      $('#search-block-form input[type="submit"]', context).click(function(e) {
         var choosen_search = $('#search-block-form input[name=ding_redirect_redirect_radios]:checked').val();
+
+        // Get settings for showing a dialog.
+        var delay        = settings.ding_redirect.delay || -1,
+            dialog_title = settings.ding_redirect.dialog_title,
+            dialog_text  = settings.ding_redirect.dialog_text;
 
         if (settings.ding_redirect[choosen_search] !== '') {
           // Redirect the user to the new URL from settings. Add the query to
@@ -17,8 +22,30 @@
               query = $('#search-block-form input[type="text"]').val();
           url = url.replace('%QUERY%', query);
 
-          window.location = url;
-          event.preventDefault();
+          if (delay > 0) {
+            // Pop a dialog when there is a delay on redirect.
+            var canceled = false;
+            var $dialog = $('<div class="os2web-redirect-modal">' + dialog_text + '</div>').dialog({
+              'modal': true,
+              'title': dialog_title,
+              'closeText' : Drupal.t('close'),
+              'close': function(event, ui) {
+                canceled = true;
+                $dialog.dialog('destroy').remove();
+              }
+            });
+            setTimeout(function(){
+              if (!canceled) {
+                $dialog.dialog('destroy').remove();
+                window.location = url;
+              }
+            }, delay);
+          } else {
+            // No dialog and delay used, redirect user.
+            window.location = url;
+            e.preventDefault();
+            return false;
+          }
         }
       });
     }
