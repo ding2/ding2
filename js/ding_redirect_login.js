036a6eee1dd1efdaef9c94b5fa234d470522320f
Anders Bryrup <andersbryrup@gmail.com>
2014-07-17 10:40:32 +0200

    Add option to set delay and use of a dialog with content on redirect login.

--- js/ding_redirect_login.js
+++ js/ding_redirect_login.js
@@ -7,15 +7,42 @@
    * user to it. This file are only added, if an URL are provided.
    */
   $(document).ready(function(){
-    // To remove the quick flicker of the login form from the theme, we remove
-    // it here. Alternative is to make the theme overrideable.
 
-    // The URL the user should get redirected to.
-    var login_url = Drupal.settings.ding_redirect.login_url;
+    // Get all settings from Drupal.
+    var login_url    = Drupal.settings.ding_redirect.login_url,
+        delay        = Drupal.settings.ding_redirect.delay,
+        dialog_title = Drupal.settings.ding_redirect.dialog_title,
+        dialog_text  = Drupal.settings.ding_redirect.dialog_text;
+
     $('.topbar-link-user').on('click touchstart', function(e) {
+      // To remove the quick flicker of the login form from the theme, we remove
+      // it here. Alternative is to make the theme overrideable.
       $('.js-topbar-user').remove();
-      window.location = login_url;
+
+      if (delay > 0) {
+        // Pop a dialog when there is a delay on redirect.
+        var canceled = false;
+        var $dialog = $('<div class="os2web-redirect-modal">' + dialog_text + '</div>').dialog({
+          'modal': true,
+          'title': dialog_title,
+          'closeText' : Drupal.t('close'),
+          'close': function(event, ui) {
+            canceled = true;
+            $dialog.dialog('destroy').remove();
+          }
+        });
+        setTimeout(function(){
+          if (!canceled) {
+            $dialog.dialog('destroy').remove();
+            window.location = login_url;
+          }
+        }, delay);
+      } else {
+        // No dialog and delay used, redirect user.
+        window.location = login_url;
+      }
       e.preventDefault();
+      return false;
     });
   });
 
