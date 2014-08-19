f488ffeb82df6c1d1b0b03196772b8feb49ff7af
Bunulu Andrei <bunulu@gmail.com>
2014-08-19 16:17:20 +0300

    DDBTEST 18 - Test behaviuor corrections

--- DDBTEST 18 - Search box.php
+++ DDBTEST 18 - Search box.php
@@ -7,21 +7,31 @@ class SearchResult extends PHPUnit_Extensions_SeleniumTestCase
     $this->setBrowserUrl("http://ding2tal.easyting.dk/");
   }
 
-  public function testSearchBox()
+  public function testSearchbox()
   {
     $this->open("/");
     $this->type("id=edit-search-block-form--2", "Dune");
     $this->click("id=edit-submit");
     $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("id=edit-search-block-form--2"));
+    $this->assertTrue($this->isElementPresent("link=Dune"));
+    $this->assertTrue($this->isElementPresent("link=Gregers Tycho"));
     $this->click("link=Gregers Tycho");
     $this->waitForPageToLoad("30000");
     $this->assertTrue($this->isElementPresent("id=edit-search-block-form--2"));
-    $this->type("id=edit-search-block-form--2", "Star wars");
+    $this->assertTrue($this->isElementPresent("link=Dune"));
+    $this->assertTrue($this->isElementPresent("link=Gregers Tycho"));
+    $this->type("id=edit-search-block-form--2", "star wars");
     $this->click("id=edit-submit--3");
     $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("id=edit-search-block-form--2"));
+    $this->assertTrue($this->isElementPresent("link=Star Wars : The Essential Guide to the Force"));
+    $this->assertTrue($this->isElementPresent("link=Star Wars (Random House Paperback)"));
     $this->click("link=Star Wars (Random House Paperback)");
     $this->waitForPageToLoad("30000");
     $this->assertTrue($this->isElementPresent("id=edit-search-block-form--2"));
+    $this->assertTrue($this->isElementPresent("link=Star Wars : The Essential Guide to the Force"));
+    $this->assertTrue($this->isElementPresent("link=Star Wars (Random House Paperback)"));
   }
 }
 ?>
