3d6cd9c6ac8ef7c3af97705b520d30a6ac39fd38
Bunulu Andrei <bunulu@gmail.com>
2014-08-19 17:03:31 +0300

    DDBTEST 15 - Test auth corrections.

--- DDBTEST 15 - Series.php
+++ DDBTEST 15 - Series.php
@@ -10,10 +10,27 @@ class SearchResult extends PHPUnit_Extensions_SeleniumTestCase
   public function testSeries()
   {
     $this->open("/");
+    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
+    $this->type("id=edit-name", "1111110022");
+    $this->type("id=edit-pass", "5555");
+    $this->click("id=edit-submit--2");
+    $this->waitForPageToLoad("30000");
     $this->type("id=edit-search-block-form--2", "islandica eggert olafsson");
     $this->click("id=edit-submit");
     $this->waitForPageToLoad("30000");
-    $this->assertTrue($this->isElementPresent("link=Eggert Olafsson"));
+    $this->assertTrue($this->isElementPresent("link=Eggert Ólafsson"));
+    $this->assertTrue($this->isElementPresent("link=Islandica"));
+    $this->click("link=Islandica");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("id=edit-search-block-form--2"));
+    $this->assertTrue($this->isElementPresent("link=Eggert Olafsson : A biographical sketch"));
+    $this->assertTrue($this->isElementPresent("link=Islandica"));
+    $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
+    $this->waitForPageToLoad("30000");
+    $this->type("id=edit-search-block-form--2", "islandica eggert olafsson");
+    $this->click("id=edit-submit");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("link=Eggert Ólafsson"));
     $this->assertTrue($this->isElementPresent("link=Islandica"));
     $this->click("link=Islandica");
     $this->waitForPageToLoad("30000");
