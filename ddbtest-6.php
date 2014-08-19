99faa3325399b160e3c45c3c4eee29e10860b3d2
MM <mm@inlead.dk>
2014-08-19 15:38:49 +0300

    DDBTEST 6 - Merge anonymous and loggedin tests in one class, change browser to firefox, check for eror block not text.

--- ddbtest-6.php
+++ ddbtest-6.php
@@ -1,15 +1,28 @@
 <?php
-class SubmitLoggedIn extends PHPUnit_Extensions_SeleniumTestCase
+class OpenScan extends PHPUnit_Extensions_SeleniumTestCase
 {
   protected function setUp()
   {
-    $this->setBrowser("*chrome");
+    $this->setBrowser("*firefox");
     $this->setBrowserUrl("http://ding2tal.easyting.dk/");
+  }
+
+  public function testSubmitAnon()
+  {
+    $this->open("/en");
+    $this->type("id=edit-search-block-form--2", "harry potter");
+    $this->click("id=edit-submit");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("css=li.list-item.search-result"));
+    $this->type("id=edit-search-block-form--2", "");
+    $this->click("id=edit-submit--3");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("css=div.messages.error"));
   }
 
   public function testSubmitLoggedIn()
   {
-    $this->open("/");
+    $this->open("/en");
     $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
     $this->type("id=edit-name", "1111110022");
     $this->type("id=edit-pass", "5555");
