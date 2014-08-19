65003333ed01cb2fc64015958f4734a644e54768
Bunulu Andrei <bunulu@gmail.com>
2014-08-19 14:49:59 +0300

    DDBTEST 15 - Search result availability test

---
+++ DDBTEST 13 - Search result availability.php
@@ -0,0 +1,32 @@
+﻿<?php
+class SearchResult extends PHPUnit_Extensions_SeleniumTestCase
+{
+  protected function setUp()
+  {
+    $this->setBrowser("*firefox");
+    $this->setBrowserUrl("http://ding2tal.easyting.dk/");
+  }
+
+  public function testAvailabilityMarkers()
+  {
+    $this->open("/");
+    $this->type("id=edit-search-block-form--2", "dune");
+    $this->click("id=edit-submit");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("css=p.js-pending"));
+    $this->assertTrue($this->isElementPresent("id=availability-870970-basis28017499-bog"));
+    $this->click("link=next ›");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("css=p.js-online"));
+    $this->assertTrue($this->isElementPresent("link=Lydbog (net)"));
+    $this->click("link=next ›");
+    $this->waitForPageToLoad("30000");
+    $this->click("link=next ›");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("link=Klit"));
+    $this->assertTrue($this->isElementPresent("link=Frank Herbert"));
+    $this->assertTrue($this->isElementPresent("css=p.js-pending"));
+    $this->assertTrue($this->isElementPresent("id=availability-870970-basis28443471-bog"));
+  }
+}
+?>
