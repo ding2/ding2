a45897b8c1257f2578936fbb301b28718b31cc94
Bunulu Andrei <bunulu@gmail.com>
2014-08-19 16:13:50 +0300

    DDBTEST 15 - Test behaviuor corrections

--- DDBTEST 15 - Series.php
+++ DDBTEST 15 - Series.php
@@ -13,9 +13,13 @@ class SearchResult extends PHPUnit_Extensions_SeleniumTestCase
     $this->type("id=edit-search-block-form--2", "islandica eggert olafsson");
     $this->click("id=edit-submit");
     $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("link=Eggert Olafsson"));
+    $this->assertTrue($this->isElementPresent("link=Islandica"));
     $this->click("link=Islandica");
     $this->waitForPageToLoad("30000");
     $this->assertTrue($this->isElementPresent("id=edit-search-block-form--2"));
+    $this->assertTrue($this->isElementPresent("link=Eggert Olafsson : A biographical sketch"));
+    $this->assertTrue($this->isElementPresent("link=Islandica"));
   }
 }
 ?>
