df1805cc7dffd96eb98eea49003fa9b03fe87698
Bunulu Andrei <bunulu@gmail.com>
2014-08-19 16:57:50 +0300

    DDBTEST 13 - Test auth corrections.

--- DDBTEST 13 - Search result availability.php
+++ DDBTEST 13 - Search result availability.php
@@ -10,13 +10,20 @@ class SearchResult extends PHPUnit_Extensions_SeleniumTestCase
   public function testAvailabilityMarkers()
   {
     $this->open("/");
-    $this->type("id=edit-search-block-form--2", "dune");
+    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
+    $this->type("id=edit-name", "1111110022");
+    $this->type("id=edit-pass", "5555");
+    $this->click("id=edit-submit--2");
+    $this->waitForPageToLoad("30000");
+    $this->type("id=edit-search-block-form--2", "Dune");
     $this->click("id=edit-submit");
     $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("link=Dúné"));
     $this->assertTrue($this->isElementPresent("css=p.js-pending"));
     $this->assertTrue($this->isElementPresent("id=availability-870970-basis28017499-bog"));
     $this->click("link=next ›");
     $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("xpath=(//a[contains(text(),'Dune')])[2]"));
     $this->assertTrue($this->isElementPresent("css=p.js-online"));
     $this->assertTrue($this->isElementPresent("link=Lydbog (net)"));
     $this->click("link=next ›");
@@ -24,7 +31,26 @@ class SearchResult extends PHPUnit_Extensions_SeleniumTestCase
     $this->click("link=next ›");
     $this->waitForPageToLoad("30000");
     $this->assertTrue($this->isElementPresent("link=Klit"));
-    $this->assertTrue($this->isElementPresent("link=Frank Herbert"));
+    $this->assertTrue($this->isElementPresent("css=p.js-pending"));
+    $this->assertTrue($this->isElementPresent("id=availability-870970-basis28443471-bog"));
+    $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
+    $this->waitForPageToLoad("30000");
+    $this->type("id=edit-search-block-form--2", "Dune");
+    $this->click("id=edit-submit");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("link=Dúné"));
+    $this->assertTrue($this->isElementPresent("css=p.js-pending"));
+    $this->assertTrue($this->isElementPresent("id=availability-870970-basis28017499-bog"));
+    $this->click("link=next ›");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("xpath=(//a[contains(text(),'Dune')])[2]"));
+    $this->assertTrue($this->isElementPresent("css=p.js-online"));
+    $this->assertTrue($this->isElementPresent("link=Lydbog (net)"));
+    $this->click("link=next ›");
+    $this->waitForPageToLoad("30000");
+    $this->click("link=next ›");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("link=Klit"));
     $this->assertTrue($this->isElementPresent("css=p.js-pending"));
     $this->assertTrue($this->isElementPresent("id=availability-870970-basis28443471-bog"));
   }
