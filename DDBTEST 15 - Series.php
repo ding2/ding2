09ffba74d2dbd34d09f5575dbc173da45e34e4b3
amdk8 <amd_k8@mail.ru>
2014-08-20 13:57:07 +0300

    DDBTEST 15 - Separate methods for anon and logged users. Check search input for actual facet search.

--- DDBTEST 15 - Series.php
+++ DDBTEST 15 - Series.php
@@ -7,7 +7,28 @@ class SearchResult extends PHPUnit_Extensions_SeleniumTestCase
     $this->setBrowserUrl("http://ding2tal.easyting.dk/");
   }
 
-  public function testSeries()
+  public function testSeriesAnonymous()
+  {
+    $this->open("/");
+    $this->waitForPageToLoad("30000");
+    $this->type("id=edit-search-block-form--2", "islandica eggert olafsson");
+    $this->click("id=edit-submit");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("link=Eggert Ólafsson"));
+    $this->assertTrue($this->isElementPresent("link=Islandica"));
+    $this->click("link=Islandica");
+    $this->waitForPageToLoad("30000");
+    try {
+        $this->assertEquals("phrase.titleSeries=\"islandica\"", $this->getValue("id=edit-search-block-form--2"));
+    } catch (PHPUnit_Framework_AssertionFailedError $e) {
+        array_push($this->verificationErrors, $e->toString());
+    }
+    $this->assertTrue($this->isElementPresent("id=edit-search-block-form--2"));
+    $this->assertTrue($this->isElementPresent("link=Eggert Olafsson : A biographical sketch"));
+    $this->assertTrue($this->isElementPresent("link=Islandica"));
+  }
+
+  public function testSeriesUser()
   {
     $this->open("/");
     $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
@@ -22,21 +43,15 @@ class SearchResult extends PHPUnit_Extensions_SeleniumTestCase
     $this->assertTrue($this->isElementPresent("link=Islandica"));
     $this->click("link=Islandica");
     $this->waitForPageToLoad("30000");
+    try {
+        $this->assertEquals("phrase.titleSeries=\"islandica\"", $this->getValue("id=edit-search-block-form--2"));
+    } catch (PHPUnit_Framework_AssertionFailedError $e) {
+        array_push($this->verificationErrors, $e->toString());
+    }
     $this->assertTrue($this->isElementPresent("id=edit-search-block-form--2"));
     $this->assertTrue($this->isElementPresent("link=Eggert Olafsson : A biographical sketch"));
     $this->assertTrue($this->isElementPresent("link=Islandica"));
     $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
-    $this->waitForPageToLoad("30000");
-    $this->type("id=edit-search-block-form--2", "islandica eggert olafsson");
-    $this->click("id=edit-submit");
-    $this->waitForPageToLoad("30000");
-    $this->assertTrue($this->isElementPresent("link=Eggert Ólafsson"));
-    $this->assertTrue($this->isElementPresent("link=Islandica"));
-    $this->click("link=Islandica");
-    $this->waitForPageToLoad("30000");
-    $this->assertTrue($this->isElementPresent("id=edit-search-block-form--2"));
-    $this->assertTrue($this->isElementPresent("link=Eggert Olafsson : A biographical sketch"));
-    $this->assertTrue($this->isElementPresent("link=Islandica"));
   }
 }
 ?>
