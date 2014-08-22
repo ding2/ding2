5208d55ce2b81a446f55e2335947e904bcacfa5a
Alexander Voykov <av@inlead.dk>
2014-08-22 14:24:43 +0300

    DDBTEST 24 - Fixes scenario.

--- ddbtest-24.php
+++ ddbtest-24.php
@@ -24,19 +24,19 @@ class collectionView  extends PHPUnit_Extensions_SeleniumTestCase
     $this->type("//form[@id='user-login']/div/div[2]/input", "5555");
     $this->mouseDownAt("//form[@id='user-login']/div/div[3]/input");
     sleep(2);
-    $this->assertEquals("Added to bookmarks", $this->getText("id=ui-id-1"));
+    $this->verifyText("css=div.ding-bookmark-message", "Added to bookmarks");
     $this->mouseDownAt("//body/div[4]");
     $this->click("id=bookmark-870970-basis:05306809");
     $this->assertEquals("This item is in bookmarks already.", $this->getText("css=div.ding-bookmark-message"));
     $this->mouseDownAt("//body/div[4]");
     $this->click("id=reservation-870970-basis:05306809");
     sleep(2);
-    $this->assertEquals("css=div.messages.status > ul > li", "\"Klit\" reserved and will be available for pickup at Hjørring.");
+    $this->verifyText("css=div.messages", "\"Klit\" reserved and will be available for pickup at Hjørring.");
     $this->mouseDownAt("//div[5]/div/button");
     $this->click("id=reservation-870970-basis:05306809");
     sleep(2);
     $this->assertEquals("Error message \"You have already reserved \"Klit\".", $this->getText("css=div.messages.error"));
-    $this->mouseDownAt("//div[5]/div/button");
+    $this->mouseDownAt("//div[6]/div/button");
   }
 
   public function testcollectionViewAnonFromStart()
@@ -57,18 +57,19 @@ class collectionView  extends PHPUnit_Extensions_SeleniumTestCase
     $this->assertContains('klit', $this->getText("css=div.ting-object"), '', true);
     $this->click("id=bookmark-870970-basis:05306809");
     sleep(2);
-    $this->assertEquals("Added to bookmarks", $this->getText("id=ui-id-1"));
+    $this->verifyText("css=div.ding-bookmark-message", "Added to bookmarks");
     $this->mouseDownAt("//body/div[4]");
     $this->click("id=bookmark-870970-basis:05306809");
     sleep(2);
     $this->assertEquals("This item is in bookmarks already.", $this->getText("css=div.ding-bookmark-message"));
     $this->mouseDownAt("//body/div[4]");
     $this->click("id=reservation-870970-basis:05306809");
-    $this->assertEquals("css=div.messages.status > ul > li", "\"Klit\" reserved and will be available for pickup at Hjørring.");
+    sleep(2);
+    $this->verifyText("css=div.messages", "\"Klit\" reserved and will be available for pickup at Hjørring.");
     $this->mouseDownAt("//div[4]/div/button");
     $this->click("id=reservation-870970-basis:05306809");
     $this->assertEquals("Error message \"You have already reserved \"Klit\".", $this->getText("css=div.messages.error"));
-    $this->click("//div[4]/div/button");
+    $this->mouseDownAt("//div[5]/div/button");
   }
 }
 ?>
