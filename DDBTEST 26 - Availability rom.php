cf97cd6a07cbc219cff283cec1d1a746e78e0029
amdk8 <amd_k8@mail.ru>
2014-08-22 13:39:49 +0300

    DDBTEST 26 - Separate methods for anonymous and logged in tests.

--- DDBTEST 26 - Availability rom.php
+++ DDBTEST 26 - Availability rom.php
@@ -7,18 +7,12 @@ class ItemPage extends PHPUnit_Extensions_SeleniumTestCase
     $this->setBrowserUrl("http://ding2tal.easyting.dk/");
   }
 
-  public function testAvailabilityRom()
+  public function testAvailabilityAnonymous()
   {
     $this->open("/");
-    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->type("id=edit-name", "1111110022");
-    $this->type("id=edit-pass", "5555");
-    $this->click("id=edit-submit--2");
-    $this->waitForPageToLoad("30000");
     $this->type("id=edit-search-block-form--2", "Rom : i Vilhelm Bergsøes");
     $this->click("id=edit-submit");
     $this->waitForPageToLoad("30000");
-    $this->assertEquals("", $this->getText("id=edit-search-block-form--2"));
     $this->assertTrue($this->isElementPresent("css=span.search-result--heading-type"));
     $this->assertTrue($this->isElementPresent("link=Rom : i Vilhelm Bergsøes fodspor fra Piazza del Popolo"));
     $this->click("link=Rom : i Vilhelm Bergsøes fodspor fra Piazza del Popolo");
@@ -33,12 +27,19 @@ class ItemPage extends PHPUnit_Extensions_SeleniumTestCase
     $this->assertTrue($this->isElementPresent("id=availability-870970-basis50676927"));
     $this->assertTrue($this->isElementPresent("css=h2.pane-title"));
     $this->assertTrue($this->isElementPresent("link=Bog (1)"));
-    $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
+  }
+
+  public function testAvailabilityLoggedIn()
+  {
+    $this->open("/");
+    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
+    $this->type("id=edit-name", "1111110022");
+    $this->type("id=edit-pass", "5555");
+    $this->click("id=edit-submit--2");
     $this->waitForPageToLoad("30000");
     $this->type("id=edit-search-block-form--2", "Rom : i Vilhelm Bergsøes");
     $this->click("id=edit-submit");
     $this->waitForPageToLoad("30000");
-    $this->assertEquals("", $this->getText("id=edit-search-block-form--2"));
     $this->assertTrue($this->isElementPresent("css=span.search-result--heading-type"));
     $this->assertTrue($this->isElementPresent("link=Rom : i Vilhelm Bergsøes fodspor fra Piazza del Popolo"));
     $this->click("link=Rom : i Vilhelm Bergsøes fodspor fra Piazza del Popolo");
