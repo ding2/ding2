4afef8c1c5ae1843cfe118f1478d5e128f352273
amdk8 <amd_k8@mail.ru>
2014-08-22 17:53:38 +0300

    OpenScan test suite. Removed old test files.

--- OpenScanTestSuite.php
+++ OpenScanTestSuite.php
@@ -7,7 +7,7 @@ class OpenScan extends PHPUnit_Extensions_SeleniumTestCase
     $this->setBrowserUrl("http://ding2tal.easyting.dk/");
   }
 
-  public function testAutosuggestionAnon()
+  public function testAutosuggestionAnonymous()
   {
     $this->open("/");
     $this->type("id=edit-search-block-form--2", "star wa");
@@ -91,6 +91,93 @@ class OpenScan extends PHPUnit_Extensions_SeleniumTestCase
     } catch (PHPUnit_Framework_AssertionFailedError $e) {
         array_push($this->verificationErrors, $e->toString());
     }
+    $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
+    $this->waitForPageToLoad("30000");
+  }
+
+  public function testSpecialCharactersAnonymous()
+  {
+    $this->open("/");
+    $this->type("id=edit-search-block-form--2", "Afskrivning på maskiner");
+    $this->click("id=edit-submit");
+    $this->waitForPageToLoad("30000");
+    $this->assertEquals("Afskrivning på maskiner", $this->getValue("id=edit-search-block-form--2"));
+    $this->assertEquals("Afskrivning på maskiner", $this->getText("link=Afskrivning på maskiner"));
+    $this->click("css=img[alt=\"Home\"]");
+    $this->waitForPageToLoad("30000");
+    $this->type("id=edit-search-block-form--2", "gæ");
+    $this->fireEvent("id=edit-search-block-form--2", "keyup");
+    for ($second = 0; ; $second++) {
+        if ($second >= 60) $this->fail("timeout");
+        try {
+            if ($this->isElementPresent("//*[@id=\"autocomplete\"]/ul/li")) break;
+        } catch (Exception $e) {}
+        sleep(1);
+    }
+
+    $this->assertEquals("gæk, gæk, gæk", $this->getText("//*[@id=\"autocomplete\"]/ul/li[2]/div"));
+  }
+
+  public function testSpecialCharactersLoggedIn()
+  {
+    $this->open("/");
+    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
+    $this->type("id=edit-name", "1111110022");
+    $this->type("id=edit-pass", "5555");
+    $this->click("id=edit-submit--2");
+    $this->waitForPageToLoad("30000");
+    $this->type("id=edit-search-block-form--2", "Afskrivning på maskiner");
+    $this->click("id=edit-submit");
+    $this->waitForPageToLoad("30000");
+    $this->assertEquals("Afskrivning på maskiner", $this->getValue("id=edit-search-block-form--2"));
+    $this->assertEquals("Afskrivning på maskiner", $this->getText("link=Afskrivning på maskiner"));
+    $this->click("css=img[alt=\"Home\"]");
+    $this->waitForPageToLoad("30000");
+    $this->type("id=edit-search-block-form--2", "gæ");
+    $this->fireEvent("id=edit-search-block-form--2", "keyup");
+    for ($second = 0; ; $second++) {
+        if ($second >= 60) $this->fail("timeout");
+        try {
+            if ($this->isElementPresent("//*[@id=\"autocomplete\"]/ul/li")) break;
+        } catch (Exception $e) {}
+        sleep(1);
+    }
+
+    $this->assertEquals("gæk, gæk, gæk", $this->getText("//*[@id=\"autocomplete\"]/ul/li[2]/div"));
+    $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
+    $this->waitForPageToLoad("30000");
+  }
+
+  public function testSubmitAnonymous()
+  {
+    $this->open("/en");
+    $this->type("id=edit-search-block-form--2", "harry potter");
+    $this->click("id=edit-submit");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("css=li.list-item.search-result"));
+    $this->type("id=edit-search-block-form--2", "");
+    $this->click("id=edit-submit--3");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("css=div.messages.error"));
+  }
+
+  public function testSubmitLoggedIn()
+  {
+    $this->open("/en");
+    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
+    $this->type("id=edit-name", "1111110022");
+    $this->type("id=edit-pass", "5555");
+    $this->click("id=edit-submit--2");
+    $this->waitForPageToLoad("30000");
+    $this->assertEquals("My Account", $this->getText("//div[@id='page']/header/section/div/ul/li[3]/a/span"));
+    $this->type("id=edit-search-block-form--2", "inferno dan brown");
+    $this->click("id=edit-submit");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("link=Inferno"));
+    $this->type("id=edit-search-block-form--2", "");
+    $this->click("id=edit-submit--3");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("css=div.messages.error"));
     $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
     $this->waitForPageToLoad("30000");
   }
