b80d15c93a9a2a44d26fa19f784a45de5eb2350e
Bunulu Andrei <bunulu@gmail.com>
2014-08-22 10:24:54 +0300

    DDBTEST 5 - Special characters test fix.

--- DDBTEST 5 - Special Characters.php
+++ DDBTEST 5 - Special Characters.php
@@ -10,45 +10,54 @@ class OpenScan extends PHPUnit_Extensions_SeleniumTestCase
   public function testSpecialCharacters()
   {
     $this->open("/");
+    $this->type("id=edit-search-block-form--2", "Afskrivning på maskiner");
+    $this->click("id=edit-submit");
+    $this->waitForPageToLoad("30000");
+    $this->assertEquals("Afskrivning på maskiner", $this->getValue("id=edit-search-block-form--2"));
+    $this->assertEquals("Afskrivning på maskiner", $this->getText("link=Afskrivning på maskiner"));
+    $this->click("css=img[alt=\"Home\"]");
+    $this->waitForPageToLoad("30000");
+    $this->type("id=edit-search-block-form--2", "da \"plejer\" døde");
+    $this->fireEvent("id=edit-search-block-form--2", "keyup");
+    for ($second = 0; ; $second++) {
+        if ($second >= 60) $this->fail("timeout");
+        try {
+            if ($this->isElementPresent("//*[@id=\"autocomplete\"]/ul/li")) break;
+        } catch (Exception $e) {}
+        sleep(1);
+    }
+
+    $this->assertEquals("da \"plejer\" døde", $this->getText("//*[@id=\"autocomplete\"]/ul/li[9]/div"));
+  }
+
+  public function testSpecialCharactersAuth()
+  {
+    $this->open("/");
     $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
     $this->type("id=edit-name", "1111110022");
     $this->type("id=edit-pass", "5555");
     $this->click("id=edit-submit--2");
     $this->waitForPageToLoad("30000");
-    $this->type("id=edit-search-block-form--2", "gæ");
-    $this->typeKeys("id=edit-search-block-form--2", "gæ");
+    $this->type("id=edit-search-block-form--2", "Afskrivning på maskiner");
+    $this->click("id=edit-submit");
+    $this->waitForPageToLoad("30000");
+    $this->assertEquals("Afskrivning på maskiner", $this->getValue("id=edit-search-block-form--2"));
+    $this->assertEquals("Afskrivning på maskiner", $this->getText("link=Afskrivning på maskiner"));
+    $this->click("css=img[alt=\"Home\"]");
+    $this->waitForPageToLoad("30000");
+    $this->type("id=edit-search-block-form--2", "da \"plejer\" døde");
+    $this->fireEvent("id=edit-search-block-form--2", "keyup");
     for ($second = 0; ; $second++) {
         if ($second >= 60) $this->fail("timeout");
         try {
-            if ($this->isTextPresent("gæk, gæk, gæk")) break;
+            if ($this->isElementPresent("//*[@id=\"autocomplete\"]/ul/li")) break;
         } catch (Exception $e) {}
         sleep(1);
     }
 
-    $this->mouseOver("//div[@id='autocomplete']/ul/li[2]/div");
-    $this->click("css=li.selected > div");
-    $this->click("id=edit-submit");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("", $this->getText("id=edit-search-block-form--2"));
-    $this->assertTrue($this->isElementPresent("link=Gæk, gæk -"));
+    $this->assertEquals("da \"plejer\" døde", $this->getText("//*[@id=\"autocomplete\"]/ul/li[9]/div"));
     $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
     $this->waitForPageToLoad("30000");
-    $this->type("id=edit-search-block-form--2", "gæ");
-    $this->typeKeys("id=edit-search-block-form--2", "gæ");
-    for ($second = 0; ; $second++) {
-        if ($second >= 60) $this->fail("timeout");
-        try {
-            if ($this->isTextPresent("gæk, gæk, gæk")) break;
-        } catch (Exception $e) {}
-        sleep(1);
-    }
-
-    $this->mouseOver("//div[@id='autocomplete']/ul/li[2]/div");
-    $this->click("css=li.selected > div");
-    $this->click("id=edit-submit");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("", $this->getText("id=edit-search-block-form--2"));
-    $this->assertTrue($this->isElementPresent("link=Gæk, gæk -"));
   }
 }
 ?>
