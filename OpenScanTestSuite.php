b69d7b373025b98dcddc6da10ae26e33cdd28132
amdk8 <amd_k8@mail.ru>
2014-08-25 12:28:00 +0300

    Separate config file. Tests corrections.

--- OpenScanTestSuite.php
+++ OpenScanTestSuite.php
@@ -1,15 +1,16 @@
 <?php
-class OpenScan extends PHPUnit_Extensions_SeleniumTestCase
-{
-  protected function setUp()
-  {
-    $this->setBrowser("*firefox");
-    $this->setBrowserUrl("http://ding2tal.easyting.dk/");
+
+require_once(dirname(__FILE__) . '/config.inc');
+
+class OpenScan extends PHPUnit_Extensions_SeleniumTestCase {
+
+  protected function setUp() {
+    $this->setBrowser(TARGET_BROWSER);
+    $this->setBrowserUrl(TARGET_URL);
   }
 
-  public function testAutosuggestionAnonymous()
-  {
-    $this->open("/");
+  public function testAutosuggestionAnonymous() {
+    $this->open("/en");
     $this->type("id=edit-search-block-form--2", "star wa");
     $this->fireEvent("id=edit-search-block-form--2", "keyup");
     $this->assertTrue($this->isElementPresent("id=autocomplete"));
@@ -17,9 +18,10 @@ class OpenScan extends PHPUnit_Extensions_SeleniumTestCase
     $this->assertEquals("star wars", $this->getText("//*[@id=\"autocomplete\"]/ul/li[1]"));
     $this->mouseDown("//div[@id='autocomplete']/ul/li/div");
     try {
-        $this->assertEquals("star wars", $this->getValue("id=edit-search-block-form--2"));
-    } catch (PHPUnit_Framework_AssertionFailedError $e) {
-        array_push($this->verificationErrors, $e->toString());
+      $this->assertEquals("star wars", $this->getValue("id=edit-search-block-form--2"));
+    }
+    catch (PHPUnit_Framework_AssertionFailedError $e) {
+      array_push($this->verificationErrors, $e->toString());
     }
     $this->type("id=edit-search-block-form--2", "");
     $this->type("id=edit-search-block-form--2", "star wa");
@@ -31,9 +33,10 @@ class OpenScan extends PHPUnit_Extensions_SeleniumTestCase
     $this->click("id=edit-submit");
     $this->waitForPageToLoad("30000");
     try {
-        $this->assertEquals("star wars", $this->getValue("id=edit-search-block-form--2"));
-    } catch (PHPUnit_Framework_AssertionFailedError $e) {
-        array_push($this->verificationErrors, $e->toString());
+      $this->assertEquals("star wars", $this->getValue("id=edit-search-block-form--2"));
+    }
+    catch (PHPUnit_Framework_AssertionFailedError $e) {
+      array_push($this->verificationErrors, $e->toString());
     }
     $this->type("id=edit-search-block-form--2", "");
     $this->type("id=edit-search-block-form--2", "star wa");
@@ -41,18 +44,18 @@ class OpenScan extends PHPUnit_Extensions_SeleniumTestCase
     $this->assertTrue($this->isElementPresent("id=autocomplete"));
     $this->click("css=#ding-facetbrowser-form > div > #facet-type > #expand_more");
     try {
-        $this->assertEquals("star wa", $this->getValue("id=edit-search-block-form--2"));
-    } catch (PHPUnit_Framework_AssertionFailedError $e) {
-        array_push($this->verificationErrors, $e->toString());
+      $this->assertEquals("star wa", $this->getValue("id=edit-search-block-form--2"));
+    }
+    catch (PHPUnit_Framework_AssertionFailedError $e) {
+      array_push($this->verificationErrors, $e->toString());
     }
   }
 
-  public function testAutosuggestionLoggedIn()
-  {
-    $this->open("/");
+  public function testAutosuggestionLoggedIn() {
+    $this->open("/en");
     $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->type("id=edit-name", "1111110022");
-    $this->type("id=edit-pass", "5555");
+    $this->type("id=edit-name", TARGET_URL_USER);
+    $this->type("id=edit-pass", TARGET_URL_USER_PASS);
     $this->click("id=edit-submit--2");
     $this->waitForPageToLoad("30000");
     $this->assertEquals("My Account", $this->getText("//div[@id='page']/header/section/div/ul/li[3]/a/span"));
@@ -63,9 +66,10 @@ class OpenScan extends PHPUnit_Extensions_SeleniumTestCase
     $this->assertEquals("star wars", $this->getText("//*[@id=\"autocomplete\"]/ul/li[1]"));
     $this->mouseDown("//div[@id='autocomplete']/ul/li/div");
     try {
-        $this->assertEquals("star wars", $this->getValue("id=edit-search-block-form--2"));
-    } catch (PHPUnit_Framework_AssertionFailedError $e) {
-        array_push($this->verificationErrors, $e->toString());
+      $this->assertEquals("star wars", $this->getValue("id=edit-search-block-form--2"));
+    }
+    catch (PHPUnit_Framework_AssertionFailedError $e) {
+      array_push($this->verificationErrors, $e->toString());
     }
     $this->type("id=edit-search-block-form--2", "");
     $this->type("id=edit-search-block-form--2", "star wa");
@@ -77,9 +81,10 @@ class OpenScan extends PHPUnit_Extensions_SeleniumTestCase
     $this->click("id=edit-submit");
     $this->waitForPageToLoad("30000");
     try {
-        $this->assertEquals("star wars", $this->getValue("id=edit-search-block-form--2"));
-    } catch (PHPUnit_Framework_AssertionFailedError $e) {
-        array_push($this->verificationErrors, $e->toString());
+      $this->assertEquals("star wars", $this->getValue("id=edit-search-block-form--2"));
+    }
+    catch (PHPUnit_Framework_AssertionFailedError $e) {
+      array_push($this->verificationErrors, $e->toString());
     }
     $this->type("id=edit-search-block-form--2", "");
     $this->type("id=edit-search-block-form--2", "star wa");
@@ -87,17 +92,17 @@ class OpenScan extends PHPUnit_Extensions_SeleniumTestCase
     $this->assertTrue($this->isElementPresent("id=autocomplete"));
     $this->click("css=#ding-facetbrowser-form > div > #facet-type > #expand_more");
     try {
-        $this->assertEquals("star wa", $this->getValue("id=edit-search-block-form--2"));
-    } catch (PHPUnit_Framework_AssertionFailedError $e) {
-        array_push($this->verificationErrors, $e->toString());
+      $this->assertEquals("star wa", $this->getValue("id=edit-search-block-form--2"));
+    }
+    catch (PHPUnit_Framework_AssertionFailedError $e) {
+      array_push($this->verificationErrors, $e->toString());
     }
     $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
     $this->waitForPageToLoad("30000");
   }
 
-  public function testSpecialCharactersAnonymous()
-  {
-    $this->open("/");
+  public function testSpecialCharactersAnonymous() {
+    $this->open("/en");
     $this->type("id=edit-search-block-form--2", "Afskrivning på maskiner");
     $this->click("id=edit-submit");
     $this->waitForPageToLoad("30000");
@@ -107,23 +112,27 @@ class OpenScan extends PHPUnit_Extensions_SeleniumTestCase
     $this->waitForPageToLoad("30000");
     $this->type("id=edit-search-block-form--2", "gæ");
     $this->fireEvent("id=edit-search-block-form--2", "keyup");
-    for ($second = 0; ; $second++) {
-        if ($second >= 60) $this->fail("timeout");
-        try {
-            if ($this->isElementPresent("//*[@id=\"autocomplete\"]/ul/li")) break;
-        } catch (Exception $e) {}
-        sleep(1);
+    for ($second = 0;; $second++) {
+      if ($second >= 60)
+        $this->fail("timeout");
+      try {
+        if ($this->isElementPresent("//*[@id=\"autocomplete\"]/ul/li"))
+          break;
+      }
+      catch (Exception $e) {
+
+      }
+      sleep(1);
     }
 
     $this->assertEquals("gæk, gæk, gæk", $this->getText("//*[@id=\"autocomplete\"]/ul/li[2]/div"));
   }
 
-  public function testSpecialCharactersLoggedIn()
-  {
-    $this->open("/");
+  public function testSpecialCharactersLoggedIn() {
+    $this->open("/en");
     $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->type("id=edit-name", "1111110022");
-    $this->type("id=edit-pass", "5555");
+    $this->type("id=edit-name", TARGET_URL_USER);
+    $this->type("id=edit-pass", TARGET_URL_USER_PASS);
     $this->click("id=edit-submit--2");
     $this->waitForPageToLoad("30000");
     $this->type("id=edit-search-block-form--2", "Afskrivning på maskiner");
@@ -135,12 +144,17 @@ class OpenScan extends PHPUnit_Extensions_SeleniumTestCase
     $this->waitForPageToLoad("30000");
     $this->type("id=edit-search-block-form--2", "gæ");
     $this->fireEvent("id=edit-search-block-form--2", "keyup");
-    for ($second = 0; ; $second++) {
-        if ($second >= 60) $this->fail("timeout");
-        try {
-            if ($this->isElementPresent("//*[@id=\"autocomplete\"]/ul/li")) break;
-        } catch (Exception $e) {}
-        sleep(1);
+    for ($second = 0;; $second++) {
+      if ($second >= 60)
+        $this->fail("timeout");
+      try {
+        if ($this->isElementPresent("//*[@id=\"autocomplete\"]/ul/li"))
+          break;
+      }
+      catch (Exception $e) {
+
+      }
+      sleep(1);
     }
 
     $this->assertEquals("gæk, gæk, gæk", $this->getText("//*[@id=\"autocomplete\"]/ul/li[2]/div"));
@@ -148,8 +162,7 @@ class OpenScan extends PHPUnit_Extensions_SeleniumTestCase
     $this->waitForPageToLoad("30000");
   }
 
-  public function testSubmitAnonymous()
-  {
+  public function testSubmitAnonymous() {
     $this->open("/en");
     $this->type("id=edit-search-block-form--2", "harry potter");
     $this->click("id=edit-submit");
@@ -161,12 +174,11 @@ class OpenScan extends PHPUnit_Extensions_SeleniumTestCase
     $this->assertTrue($this->isElementPresent("css=div.messages.error"));
   }
 
-  public function testSubmitLoggedIn()
-  {
+  public function testSubmitLoggedIn() {
     $this->open("/en");
     $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->type("id=edit-name", "1111110022");
-    $this->type("id=edit-pass", "5555");
+    $this->type("id=edit-name", TARGET_URL_USER);
+    $this->type("id=edit-pass", TARGET_URL_USER_PASS);
     $this->click("id=edit-submit--2");
     $this->waitForPageToLoad("30000");
     $this->assertEquals("My Account", $this->getText("//div[@id='page']/header/section/div/ul/li[3]/a/span"));
@@ -182,4 +194,3 @@ class OpenScan extends PHPUnit_Extensions_SeleniumTestCase
     $this->waitForPageToLoad("30000");
   }
 }
-?>
