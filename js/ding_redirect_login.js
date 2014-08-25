3371df8a156838531a80839e03810384d1055889
Jesper Kristensen <cableman@linuxdev.dk>
2014-08-25 13:41:01 +0200

    Reviewed: Minor coding style changes to match ding code

--- js/ding_redirect_login.js
+++ js/ding_redirect_login.js
@@ -9,14 +9,14 @@
   $(document).ready(function(){
 
     // Get all settings from Drupal.
-    var login_url    = Drupal.settings.ding_redirect.login_url,
-        delay        = Drupal.settings.ding_redirect.delay,
+    var login_url = Drupal.settings.ding_redirect.login_url,
+        delay = Drupal.settings.ding_redirect.delay,
         dialog_title = Drupal.settings.ding_redirect.dialog_title,
-        dialog_text  = Drupal.settings.ding_redirect.dialog_text;
+        dialog_text = Drupal.settings.ding_redirect.dialog_text;
 
     $('.topbar-link-user').on('click touchstart', function(e) {
       // To remove the quick flicker of the login form from the theme, we remove
-      // it here. Alternative is to make the theme overrideable.
+      // it here. Alternative is to make the theme overridable.
       $('.js-topbar-user').remove();
 
       if (delay > 0) {
@@ -25,19 +25,21 @@
         var $dialog = $('<div class="ding-redirect-modal">' + dialog_text + '</div>').dialog({
           'modal': true,
           'title': dialog_title,
-          'closeText' : Drupal.t('close'),
+          'closeText': Drupal.t('close'),
           'close': function(event, ui) {
             canceled = true;
             $dialog.dialog('destroy').remove();
           }
         });
+
         setTimeout(function(){
           if (!canceled) {
             $dialog.dialog('destroy').remove();
             window.location = login_url;
           }
         }, delay);
-      } else {
+      }
+      else {
         // No dialog and delay used, redirect user.
         window.location = login_url;
       }
