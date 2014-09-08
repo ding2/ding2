7fceceb6fa20acc0bee67643e304ace2c9e5eeb0
MM <mm@inlead.dk>
2014-09-08 11:29:52 +0300

    DDBTEST 65 - Create pick libraty test.

---
+++ ddbtest_65.php
@@ -0,0 +1,27 @@
+<?php
+class UserProfile extends PHPUnit_Extensions_SeleniumTestCase
+{
+  protected function setUp()
+  {
+    $this->setBrowser("*firefox");
+    $this->setBrowserUrl("http://ding2tal.easyting.dk/");
+  }
+
+  public function testPickLibrary()
+  {
+    $this->open("/en");
+    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
+    $this->type("id=edit-name", "1111110022");
+    $this->type("id=edit-pass", "5555");
+    $this->click("id=edit-submit--2");
+    $this->waitForPageToLoad("30000");
+    $this->assertEquals("My Account", $this->getText("//div[@id='page']/header/section/div/ul/li[3]/a/span"));
+    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
+    $this->waitForPageToLoad("30000");
+    $this->assertEquals("My account", $this->getText("//div[@id='page']/div/div/div/div/div/div/aside/div/h2/a"));
+    $this->assertEquals("Preferred branch:", $this->getText("//div[@id='page']/div/div/div/div/div/div/div/div[2]/div/div/div/div[7]/div"));
+    $this->assertEquals("Sindal", $this->getText("//div[@id='page']/div/div/div/div/div/div/div/div[2]/div/div/div/div[7]/div[2]/div"));
+    $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
+    $this->waitForPageToLoad("30000");
+  }
+}
