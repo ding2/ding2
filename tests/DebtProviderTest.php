bfa6cf3334d19c2805c4f6e98e01d2354751e6e5
Thomas Fini Hansen <xen@xen.dk>
2015-03-09 10:41:35 +0100

    Implemented payment_received. Closes DDBFBS-11.

--- tests/DebtProviderTest.php
+++ tests/DebtProviderTest.php
@@ -9,6 +9,15 @@ require_once "ProviderTestCase.php";
 require_once 'includes/classes/FBS.php';
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
+if (!function_exists('t')) {
+  /**
+   * DingProviderLoan::__construct() calls this, mock it.
+   */
+  function t($str) {
+    return $str;
+  }
+}
+
 /**
  * Test debt provider functions.
  */
@@ -74,5 +83,52 @@ class ProviderTest extends ProviderTestCase {
       ))
     );
     $this->assertEquals($expected, $res);
+  }
+
+  /**
+   * Test debt payment_received.
+   */
+  public function testPaymentRecieved() {
+    $this->provider = 'debt';
+    // Define DingEntity.
+    $this->requireDing('ding_entity', 'ding_entity.module');
+    // // Define DingProviderUserException.
+    // $this->requireDing('ding_provider', 'ding_provider.exceptions.inc');
+    // Define DingProviderLoan.
+    $this->requireDing('ding_provider', 'ding_provider.debt.inc');
+
+    $json_responses = array(
+      new Reply(
+        array(
+          // Array of...
+          array(
+            // PaymentConfirmation.
+            'orderId' => 'order-123',
+            'confirmationId' => 1,
+            'feeId' => 1,
+            'paymentStatus' => 'paymentRegistered',
+          ),
+          array(
+            // PaymentConfirmation.
+            'orderId' => 'order-123',
+            'confirmationId' => 2,
+            'feeId' => 2,
+            'paymentStatus' => 'paymentRegistered',
+          ),
+        )
+      ),
+    );
+
+    $httpclient = $this->getHttpClient($json_responses);
+
+    // Run through tests.
+    $fbs = fbs_service('1234', '', $httpclient, NULL, TRUE);
+
+    $user = (object) array(
+      'fbs_patron_id' => '123',
+    );
+
+    $res = $this->providerInvoke('payment_received', $user, array(1, 2), 'order-123');
+    $this->assertTrue($res);
   }
 }
