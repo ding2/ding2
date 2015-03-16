bd7fa1f28cc30466d7a38d4da2e5c60e291ae9c9
Vanille <vanille@Rikkes-MacBook-Air.local>
2015-03-16 12:10:54 +0100

    Expanded testdata for loanlist test

--- tests/LoanProviderTest.php
+++ tests/LoanProviderTest.php
@@ -46,6 +46,12 @@ class LoanProviderTest extends ProviderTestCase {
     $this->requireDing('ding_provider', 'ding_provider.loan.inc');
 
     $json_responses = array(
+      // TCLL1: Patron has no loans 
+      new Reply(
+        array()
+      ),
+
+      // TCLL2: Patron has a renewable loan
       new Reply(
         array(
           // Array of...
@@ -54,17 +60,40 @@ class LoanProviderTest extends ProviderTestCase {
             'isRenewable' => TRUE,
             'loanDetails' => array(
               // LoanDetails.
-              'recordId' => '870970-basis:29440425',
-              'dueDate' => '2015-04-01',
-              'loanDate' => '2015-02-26',
-              'materialItemNumber' => 34134,
-              'loanId' => 234,
+              'recordId' => 'RECID30',
+              'dueDate' => 'DUEDATE30',
+              'loanDate' => 'LOANDATE30',
+              'materialItemNumber' => 'ITEMNUM30',
+              'loanId' => 30,
             ),
             'renewalStatusList' => array(),
           ),
         )
       ),
 
+      // TCLL3: Patron has a loan that has reached max limit of renewables
+      new Reply(
+        array(
+          // Array of...
+          array(
+            // Loan.
+            'isRenewable' => FALSE,
+            'loanDetails' => array(
+              // LoanDetails.
+              'recordId' => 'RECID31',
+              'dueDate' => 'DUEDATE31',
+              'loanDate' => 'LOANDATE31',
+              'materialItemNumber' => 'ITEMNUM31',
+              'loanId' => 31,
+            ),
+            'renewalStatusList' => array(
+              'deniedMaxRenewalsReached',
+            ),
+          ),
+        )
+      ),
+
+      // TCLL4: Patron with multiple loans, both renewable and not
       new Reply(
         array(
           // Array of...
@@ -73,11 +102,24 @@ class LoanProviderTest extends ProviderTestCase {
             'isRenewable' => TRUE,
             'loanDetails' => array(
               // LoanDetails.
-              'recordId' => '870970-basis:29440425',
-              'dueDate' => '2015-04-01',
-              'loanDate' => '2015-02-26',
-              'materialItemNumber' => 34134,
-              'loanId' => 234,
+              'recordId' => 'RECID32',
+              'dueDate' => 'DUEDATE32',
+              'loanDate' => 'LOANDATE32',
+              'materialItemNumber' => 'ITEMNUM32',
+              'loanId' => 32,
+            ),
+            'renewalStatusList' => array(),
+          ),
+          array(
+            // Loan.
+            'isRenewable' => TRUE,
+            'loanDetails' => array(
+              // LoanDetails.
+              'recordId' => 'RECID33',
+              'dueDate' => 'DUEDATE33',
+              'loanDate' => 'LOANDATE33',
+              'materialItemNumber' => 'ITEMNUM33',
+              'loanId' => 33,
             ),
             'renewalStatusList' => array(),
           ),
@@ -86,14 +128,29 @@ class LoanProviderTest extends ProviderTestCase {
             'isRenewable' => FALSE,
             'loanDetails' => array(
               // LoanDetails.
-              'recordId' => '870970-basis:29264007',
-              'dueDate' => '2015-04-02',
-              'loanDate' => '2014-01-01',
-              'materialItemNumber' => 3423134,
-              'loanId' => 879879,
+              'recordId' => 'RECID34',
+              'dueDate' => 'DUEDATE34',
+              'loanDate' => 'LOANDATE34',
+              'materialItemNumber' => 'ITEMNUM34',
+              'loanId' => 34,
             ),
             'renewalStatusList' => array(
-              'Come on, let someone else read it.',
+              'deniedReserved',
+            ),
+          ),
+          array(
+            // Loan.
+            'isRenewable' => FALSE,
+            'loanDetails' => array(
+              // LoanDetails.
+              'recordId' => 'RECID35',
+              'dueDate' => 'DUEDATE35',
+              'loanDate' => 'LOANDATE35',
+              'materialItemNumber' => 'ITEMNUM35',
+              'loanId' => 35,
+            ),
+            'renewalStatusList' => array(
+              'deniedOtherReason',
             ),
           ),
         )
@@ -104,45 +161,96 @@ class LoanProviderTest extends ProviderTestCase {
     // Run through tests.
     $fbs = fbs_service('1234', '', $httpclient, NULL, TRUE);
 
+    // TCLL1: Loaner with no loans
     $user = (object) array(
       'creds' => array(
-        'patronId' => '123',
+        'patronId' => 'PATID10',
       ),
     );
 
     // Check success.
     $res = $this->providerInvoke('list', $user);
+    $expected = array();
+    $this->assertEquals($expected, $res);
+
+
+    // TCLL2: Loaner with renewable loan
+    $user = (object) array(
+      'creds' => array(
+        'patronId' => 'PATID11',
+      ),
+    );
+
+    $res = $this->providerInvoke('list', $user);
     $expected = array(
-      234 => new DingProviderLoan(234, array(
-        'ding_entity_id' => '870970-basis:29440425',
-        // 'display_name' , # optional
-        'loan_date' => '2015-02-26',
-        'expiry' => '2015-04-01',
+      30 => new DingProviderLoan(30, array(
+        'ding_entity_id' => 'RECID30',
+        'loan_date' => 'LOANDATE30',
+        'expiry' => 'DUEDATE30',
         'renewable' => TRUE,
-        'materials_number' => 34134,
-        // 'author', #optional
-        // 'publication_year', #optional
-        // 'notes',
+        'materials_number' => 'ITEMNUM30',
       )),
     );
     $this->assertEquals($expected, $res);
 
+    // TCLL3: Loaner with non-renewable loan (max reached)
+    $user = (object) array(
+      'creds' => array(
+        'patronId' => 'PATID12',
+      ),
+    );
+
     $res = $this->providerInvoke('list', $user);
     $expected = array(
-      234 => new DingProviderLoan(234, array(
-        'ding_entity_id' => '870970-basis:29440425',
-        'loan_date' => '2015-02-26',
-        'expiry' => '2015-04-01',
+      31 => new DingProviderLoan(31, array(
+        'ding_entity_id' => 'RECID31',
+        'loan_date' => 'LOANDATE31',
+        'expiry' => 'DUEDATE31',
+        'renewable' => FALSE,
+        'materials_number' => 'ITEMNUM31',
+      )),
+    );
+    $this->assertEquals($expected, $res);
+
+    // TCLL4: Loaner with renewable loan
+    $user = (object) array(
+      'creds' => array(
+        'patronId' => 'PATID13',
+      ),
+    );
+
+    $res = $this->providerInvoke('list', $user);
+    $expected = array(
+      32 => new DingProviderLoan(32, array(
+        'ding_entity_id' => 'RECID32',
+        'loan_date' => 'LOANDATE32',
+        'expiry' => 'DUEDATE32',
         'renewable' => TRUE,
-        'materials_number' => 34134,
+        'materials_number' => 'ITEMNUM32',
       )),
 
-      879879 => new DingProviderLoan(879879, array(
-        'ding_entity_id' => '870970-basis:29264007',
-        'loan_date' => '2014-01-01',
-        'expiry' => '2015-04-02',
+      33 => new DingProviderLoan(33, array(
+        'ding_entity_id' => 'RECID33',
+        'loan_date' => 'LOANDATE33',
+        'expiry' => 'DUEDATE33',
+        'renewable' => TRUE,
+        'materials_number' => 'ITEMNUM33',
+      )),
+
+       34 => new DingProviderLoan(34, array(
+        'ding_entity_id' => 'RECID34',
+        'loan_date' => 'LOANDATE34',
+        'expiry' => 'DUEDATE34',
         'renewable' => FALSE,
-        'materials_number' => 3423134,
+        'materials_number' => 'ITEMNUM34',
+      )),
+
+      35 => new DingProviderLoan(35, array(
+        'ding_entity_id' => 'RECID35',
+        'loan_date' => 'LOANDATE35',
+        'expiry' => 'DUEDATE35',
+        'renewable' => FALSE,
+        'materials_number' => 'ITEMNUM35',
       )),
     );
     $this->assertEquals($expected, $res);
