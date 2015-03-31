7c39774446e90166bf4f963118db06be8ab302e2
Vanille <rikke@reload.dk>
2015-03-31 13:45:00 +0200

    Updated testdata in update password tests

--- tests/UserProviderTest.php
+++ tests/UserProviderTest.php
@@ -26,27 +26,27 @@ class UserProviderTest extends ProviderTestCase {
           'authenticated' => TRUE,
           'patron' => array(
             // Patron.
-            'birthday' => '1946-03-19',
+            'birthday' => '1976-05-03',
             'coAddress' => NULL,
             'address' => array(
               // Address
               'country' => 'Danmark',
-              'city' => 'København',
-              'street' => 'Alhambravej 1',
-              'postalCode' => '1826',
+              'city' => 'Ølstykke',
+              'street' => 'Svendsvej 1',
+              'postalCode' => '3650',
             ),
-            // ISIL of Vesterbro bibliotek
-            'preferredPickupBranch' => '113',
+            // ISIL of Horsens Bibliotek
+            'preferredPickupBranch' => 'DK-761500',
             'onHold' => NULL,
-            'patronId' => 234143,
-            'recieveEmail' => TRUE,
+            'patronId' => 72,
+            'receiveEmail' => FALSE,
             'blockStatus' => NULL,
-            'recieveSms' => FALSE,
-            'emailAddress' => 'onkel@danny.dk',
-            'phoneNumber' => '80345210',
-            'name' => 'Dan Turrell',
+            'receiveSms' => TRUE,
+            'emailAddress' => NULL,
+            'phoneNumber' => '61331330',
+            'name' => 'Patron One',
             'receivePostalMail' => FALSE,
-            'defaultInterestPeriod' => 30,
+            'defaultInterestPeriod' => NULL,
             'resident' => TRUE,
           ),
         )
@@ -65,68 +65,36 @@ class UserProviderTest extends ProviderTestCase {
       new Reply(
         array(
           // AuthenticatedPatron.
-          'authenticated' => TRUE,
-          'patron' => array(
-            // Patron.
-            'birthday' => '1946-03-19',
-            'coAddress' => NULL,
-            'address' => array(
-              // Address
-              'country' => 'Danmark',
-              'city' => 'København',
-              'street' => 'Alhambravej 1',
-              'postalCode' => '1826',
-            ),
-            // ISIL of Vesterbro bibliotek
-            'preferredPickupBranch' => '113',
-            'onHold' => NULL,
-            'patronId' => 234143,
-            'recieveEmail' => TRUE,
-            'blockStatus' => array(
-              // Array of...
-              array(
-                // Blockstatus.
-                'blockedReason' => '3424',
-                'blockedSince' => '1993-10-15',
-                'message' => 'You are dead.',
-              ),
-            ),
-            'recieveSms' => FALSE,
-            'emailAddress' => 'onkel@danny.dk',
-            'phoneNumber' => '80345210',
-            'name' => 'Dan Turrell',
-            'receivePostalMail' => FALSE,
-            'defaultInterestPeriod' => 30,
-            'resident' => TRUE,
-          ),
+          'authenticated' => FALSE,
+          'patron' => NULL,
         )
       ),
     );
     $this->replies($json_responses);
 
     // Check success.
-    $res = $this->providerInvoke('authenticate', '151019463013', '1234');
+    $res = $this->providerInvoke('authenticate', '111111', '1111');
     $expected = array(
       'success' => TRUE,
       'user' => array(
-        'mail' => 'onkel@danny.dk',
+        'mail' => NULL,
         'data' => array(
-          'display_name' => 'Dan Turrell',
+          'display_name' => 'Patron One',
         ),
         'private' => array(
-          'branch' => 113,
+          'branch' => 'DK-761500',
         ),
         'blocked' => FALSE,
       ),
       'creds' => array(
-        'patronId' => 234143,
-        'name' => 'Dan Turrell',
-        'phone' => '80345210',
-        'mail' => 'onkel@danny.dk',
-        'phone_notification' => NULL,
-        'mail_notification' => NULL,
-        'preferred_branch' => '113',
-        'interest_period' => 30,
+        'patronId' => 72,
+        'name' => 'Patron One',
+        'phone' => '61331330',
+        'mail' => '',
+        'phone_notification' => TRUE,
+        'mail_notification' => FALSE,
+        'preferred_branch' => 'DK-761500',
+        'interest_period' => 0,
       ),
     );
     $this->assertEquals($expected, $res);
@@ -138,33 +106,10 @@ class UserProviderTest extends ProviderTestCase {
     );
     $this->assertEquals($expected, $res);
 
-    // Check blocked.
-    $res = $this->providerInvoke('authenticate', '151019463013', '1234');
-    $expected = array(
-      'success' => TRUE,
-      'user' => array(
-        'mail' => 'onkel@danny.dk',
-        'data' => array(
-          'display_name' => 'Dan Turrell',
-        ),
-        'private' => array(
-          'branch' => 113,
-        ),
-        'blocked' => TRUE,
-        'blocks' => array(
-          'You are dead.',
-        ),
-      ),
-      'creds' => array(
-        'patronId' => 234143,
-        'name' => 'Dan Turrell',
-        'phone' => '80345210',
-        'mail' => 'onkel@danny.dk',
-        'phone_notification' => NULL,
-        'mail_notification' => NULL,
-        'preferred_branch' => '113',
-        'interest_period' => 30,
-      ),
+    // Check blocked user (PAT9)
+    $res = $this->providerInvoke('authenticate', '999999', '9999');
+     $expected = array(
+      'success' => FALSE,
     );
     $this->assertEquals($expected, $res);
 }
@@ -182,27 +127,27 @@ class UserProviderTest extends ProviderTestCase {
           'authenticated' => TRUE,
           'patron' => array(
             // Patron.
-            'birthday' => '1946-10-15',
+            'birthday' => '1976-05-03',
             'coAddress' => NULL,
             'address' => array(
               // Address
               'country' => 'Danmark',
-              'city' => 'København',
-              'street' => 'Alhambravej 1',
-              'postalCode' => '1826',
+              'city' => 'Ølstykke',
+              'street' => 'Svendsvej 1',
+              'postalCode' => '3650',
             ),
             // ISIL of Vesterbro bibliotek
-            'preferredPickupBranch' => '113',
+            'preferredPickupBranch' => 'DK-761500',
             'onHold' => NULL,
-            'patronId' => 234143,
-            'recieveEmail' => TRUE,
+            'patronId' => 72,
+            'receiveEmail' => FALSE,
             'blockStatus' => NULL,
-            'recieveSms' => FALSE,
-            'emailAddress' => 'onkel@danny.dk',
-            'phoneNumber' => '80345210',
-            'name' => 'Dan Turrell',
+            'receiveSms' => TRUE,
+            'emailAddress' => NULL,
+            'phoneNumber' => '61331330',
+            'name' => 'Patron One',
             'receivePostalMail' => FALSE,
-            'defaultInterestPeriod' => 30,
+            'defaultInterestPeriod' => NULL,
             'resident' => TRUE,
           ),
         )
@@ -213,27 +158,89 @@ class UserProviderTest extends ProviderTestCase {
           'authenticated' => TRUE,
           'patron' => array(
             // Patron.
-            'birthday' => '1946-10-15',
+            'birthday' => '1976-05-03',
             'coAddress' => NULL,
             'address' => array(
               // Address
               'country' => 'Danmark',
-              'city' => 'København',
-              'street' => 'Alhambravej 1',
-              'postalCode' => '1826',
+              'city' => 'Ølstykke',
+              'street' => 'Svendsvej 1',
+              'postalCode' => '3650',
+            ),
+            // ISIL of Horsens bibliotek
+            'preferredPickupBranch' => 'DK-761500',
+            'onHold' => NULL,
+            'patronId' => 72,
+            'receiveEmail' => FALSE,
+            'blockStatus' => NULL,
+            'receiveSms' => TRUE,
+            'emailAddress' => NULL,
+            'phoneNumber' => '61331330',
+            'name' => 'Patron One',
+            'receivePostalMail' => FALSE,
+            'defaultInterestPeriod' => NULL,
+            'resident' => TRUE,
+          ),
+        )
+      ),
+      new Reply(
+        array(
+          // AuthenticatedPatron.
+          'authenticated' => TRUE,
+          'patron' => array(
+            // Patron.
+            'birthday' => '1976-05-03',
+            'coAddress' => NULL,
+            'address' => array(
+              // Address
+              'country' => 'Danmark',
+              'city' => 'Ølstykke',
+              'street' => 'Svendsvej 1',
+              'postalCode' => '3650',
             ),
             // ISIL of Vesterbro bibliotek
-            'preferredPickupBranch' => '113',
+            'preferredPickupBranch' => 'DK-761500',
             'onHold' => NULL,
-            'patronId' => 234143,
-            'recieveEmail' => TRUE,
+            'patronId' => 72,
+            'receiveEmail' => FALSE,
             'blockStatus' => NULL,
-            'recieveSms' => FALSE,
-            'emailAddress' => 'onkel@danny.dk',
-            'phoneNumber' => '80345210',
-            'name' => 'Dan Turrell',
+            'receiveSms' => TRUE,
+            'emailAddress' => NULL,
+            'phoneNumber' => '61331330',
+            'name' => 'Patron One',
             'receivePostalMail' => FALSE,
-            'defaultInterestPeriod' => 30,
+            'defaultInterestPeriod' => NULL,
+            'resident' => TRUE,
+          ),
+        )
+      ),
+      new Reply(
+        array(
+          // AuthenticatedPatron.
+          'authenticated' => TRUE,
+          'patron' => array(
+            // Patron.
+            'birthday' => '1976-05-03',
+            'coAddress' => NULL,
+            'address' => array(
+              // Address
+              'country' => 'Danmark',
+              'city' => 'Ølstykke',
+              'street' => 'Svendsvej 1',
+              'postalCode' => '3650',
+            ),
+            // ISIL of Horsens bibliotek
+            'preferredPickupBranch' => 'DK-761500',
+            'onHold' => NULL,
+            'patronId' => 72,
+            'receiveEmail' => FALSE,
+            'blockStatus' => NULL,
+            'receiveSms' => TRUE,
+            'emailAddress' => NULL,
+            'phoneNumber' => '61331330',
+            'name' => 'Patron One',
+            'receivePostalMail' => FALSE,
+            'defaultInterestPeriod' => NULL,
             'resident' => TRUE,
           ),
         )
@@ -241,52 +248,96 @@ class UserProviderTest extends ProviderTestCase {
     );
     $this->replies($json_responses);
 
-    $user = (object) array(
+    $patron1 = (object) array(
       'creds' => array(
-        'patronId' => '123',
+        'patronId' => 72,
       ),
     );
 
-    // Check success.
-    $res = $this->providerInvoke('update_pincode', $user, '9999');
+    // Change pincode from 1111 to 0000
+    $res = $this->providerInvoke('update_pincode', $patron1, '0000');
     $expected = array(
       'creds' => array(
-        'patronId' => 234143,
-        'name' => 'Dan Turrell',
-        'phone' => '80345210',
-        'mail' => 'onkel@danny.dk',
-        'phone_notification' => NULL,
-        'mail_notification' => NULL,
-        'preferred_branch' => '113',
-        'interest_period' => 30,
+        'patronId' => 72,
+        'name' => 'Patron One',
+        'phone' => '61331330',
+        'mail' => '',
+        'phone_notification' => TRUE,
+        'mail_notification' => FALSE,
+        'preferred_branch' => 'DK-761500',
+        'interest_period' => 0,
       ),
     );
     $this->assertEquals($expected, $res);
 
 
     // Check that the new pincode works.
-    $res = $this->providerInvoke('authenticate', '151019463013', '9999');
+    $res = $this->providerInvoke('authenticate', '111111', '0000');
     $expected = array(
       'success' => TRUE,
       'user' => array(
-        'mail' => 'onkel@danny.dk',
+        'mail' => NULL,
         'data' => array(
-          'display_name' => 'Dan Turrell',
+          'display_name' => 'Patron One',
         ),
         'private' => array(
-          'branch' => 113,
+          'branch' => 'DK-761500',
         ),
         'blocked' => FALSE,
       ),
       'creds' => array(
-        'patronId' => 234143,
-        'name' => 'Dan Turrell',
-        'phone' => '80345210',
-        'mail' => 'onkel@danny.dk',
-        'phone_notification' => NULL,
-        'mail_notification' => NULL,
-        'preferred_branch' => '113',
-        'interest_period' => 30,
+        'patronId' => 72,
+        'name' => 'Patron One',
+        'phone' => '61331330',
+        'mail' => '',
+        'phone_notification' => TRUE,
+        'mail_notification' => FALSE,
+        'preferred_branch' => 'DK-761500',
+        'interest_period' => 0,
+      ),
+    );
+    $this->assertEquals($expected, $res);
+
+    // Change pincode back from 0000 to 1111
+    $res = $this->providerInvoke('update_pincode', $patron1, '1111');
+    $expected = array(
+      'creds' => array(
+        'patronId' => 72,
+        'name' => 'Patron One',
+        'phone' => '61331330',
+        'mail' => '',
+        'phone_notification' => TRUE,
+        'mail_notification' => FALSE,
+        'preferred_branch' => 'DK-761500',
+        'interest_period' => 0,
+      ),
+    );
+    $this->assertEquals($expected, $res);
+
+
+    // Check that the new pincode works.
+    $res = $this->providerInvoke('authenticate', '111111', '1111');
+    $expected = array(
+      'success' => TRUE,
+      'user' => array(
+        'mail' => NULL,
+        'data' => array(
+          'display_name' => 'Patron One',
+        ),
+        'private' => array(
+          'branch' => 'DK-761500',
+        ),
+        'blocked' => FALSE,
+      ),
+      'creds' => array(
+        'patronId' => 72,
+        'name' => 'Patron One',
+        'phone' => '61331330',
+        'mail' => '',
+        'phone_notification' => TRUE,
+        'mail_notification' => FALSE,
+        'preferred_branch' => 'DK-761500',
+        'interest_period' => 0,
       ),
     );
     $this->assertEquals($expected, $res);
