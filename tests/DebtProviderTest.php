307224a21b83f11628c8dd9cad3981d674d6adf5
Vanille <rikke@reload.dk>
2015-04-07 14:22:26 +0200

    Minor fixes debt test

--- tests/DebtProviderTest.php
+++ tests/DebtProviderTest.php
@@ -44,7 +44,7 @@ class ProviderTest extends ProviderTestCase {
           array(
             // Fee.
             'payableByClient' => TRUE,
-            'amount' => '100',
+            'amount' => 100,
             'paidDate' => NULL,
             'materials' => array(),
             'reasonMessage' => 'For sent afleveret',
@@ -56,7 +56,7 @@ class ProviderTest extends ProviderTestCase {
           array(
             // Fee.
             'payableByClient' => TRUE,
-            'amount' => '150',
+            'amount' => 150,
             'paidDate' => NULL,
             'materials' => array(),
             'reasonMessage' => 'Erstatning',
@@ -68,7 +68,7 @@ class ProviderTest extends ProviderTestCase {
           array(
             // Fee.
             'payableByClient' => TRUE,
-            'amount' => '200',
+            'amount' => 200,
             'paidDate' => NULL,
             'materials' => array(
               // Array of...
@@ -111,26 +111,28 @@ class ProviderTest extends ProviderTestCase {
 
     $res = $this->providerInvoke('list', $user);
     $expected = array(
-      '56' => new DingProviderDebt('56', array(
+      56 => new DingProviderDebt(56, array(
         'date' => '2015-04-07',
         'display_name' => 'For sent afleveret',
-        'amount' => '100',
+        'amount' => 100.0,
         'amount_paid' => 0,
         'invoice_number' => NULL,
         'type' => 'fee',
+        'material_number' => NULL
       )),
-      '57' => new DingProviderDebt('57', array(
+      57 => new DingProviderDebt(57, array(
         'date' => '2015-04-07',
         'display_name' => 'Erstatning',
-        'amount' => '150',
+        'amount' => 150.0,
         'amount_paid' => 0,
         'invoice_number' => NULL,
         'type' => 'compensation',
+        'material_number' => NULL
       )),
-      '58' => new DingProviderDebt('58', array(
+      58 => new DingProviderDebt(58, array(
         'date' => '2015-04-07',
         'display_name' => 'bestillingsgebyr',
-        'amount' => '200',
+        'amount' => 200.0,
         'amount_paid' => 0,
         'invoice_number' => NULL,
         'type' => 'fee',
@@ -159,18 +161,11 @@ class ProviderTest extends ProviderTestCase {
           // Array of...
           array(
             // PaymentConfirmation.
-            'orderId' => 'order-123',
-            'confirmationId' => 1,
-            'feeId' => 1,
-            'paymentStatus' => 'paymentRegistered',
-          ),
-          array(
-            // PaymentConfirmation.
-            'orderId' => 'order-123',
+            'orderId' => '42',
             'confirmationId' => 2,
-            'feeId' => 2,
+            'feeId' => 60,
             'paymentStatus' => 'paymentRegistered',
-          ),
+          ), 
         )
       ),
     );
@@ -179,11 +174,12 @@ class ProviderTest extends ProviderTestCase {
 
     $user = (object) array(
       'creds' => array(
-        'patronId' => '123',
+        'patronId' => '74',
       ),
     );
 
-    $res = $this->providerInvoke('payment_received', $user, array(1, 2), 'order-123');
+    // No problem with repeating this call on same fee as long as same orderId
+    $res = $this->providerInvoke('payment_received', $user, array(60), '42');
     $this->assertTrue($res);
   }
 }
