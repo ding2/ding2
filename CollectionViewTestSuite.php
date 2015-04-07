5489564cc40a3a06a7a49117cff3a1fafa49dbea
amd_k8 <am@inlead.dk>
2015-04-07 18:46:34 +0300

    DDBTEST 80 - User search abstraction. Rework collection view.

--- CollectionViewTestSuite.php
+++ CollectionViewTestSuite.php
@@ -47,64 +47,44 @@ class CollectionView extends PHPUnit_Extensions_SeleniumTestCase {
     $this->testCollectionCoversAnonymous();
   }
 
-//   /**
-//    * Test the collection page as anonymous.
-//    *
-//    * Assume the page should contain some links to other material types.
-//    */
-//   public function testCollectionViewAnonymous() {
-//     $this->open("/" . TARGET_URL_LANG);
-//     $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-//     $this->type("id=edit-search-block-form--2", "frank herbert klit");
-//     $this->click("id=edit-submit");
-//     $this->waitForPageToLoad("30000");
-//     $this->assertContains('klit', $this->getText("css=li.list-item.search-result"), '', true);
-//     $this->click("xpath=(//a[contains(text(),'Klit')])[2]");
-//     $this->waitForPageToLoad("30000");
-//     $this->assertTrue($this->isElementPresent("//div[@id='page']/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div"));
-//     $this->assertTrue($this->isElementPresent("//div[@id='page']/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div[2]"));
-//     $this->assertTrue($this->isElementPresent("link=Bog (8)"));
-//     $this->assertTrue($this->isElementPresent("link=Lydbog (net) (1)"));
-//     $this->assertTrue($this->isElementPresent("link=Lydbog (bånd) (5)"));
-//     $this->click("link=Bog (8)");
-//     $this->assertTrue((bool) preg_match('/^.*ting\/collection\/870970-basis%3A28443471#Bog$/', $this->getLocation()));
-//     $this->click("link=Lydbog (net) (1)");
-//     $this->assertTrue((bool) preg_match('/^.*ting\/collection\/870970-basis%3A28443471#Lydbog%20\(net\)$/', $this->getLocation()));
-//     $this->click("link=Lydbog (bånd) (5)");
-//     $this->assertTrue((bool) preg_match('/^.*ting\/collection\/870970-basis%3A28443471#Lydbog%20\(b%C3%A5nd\)$/', $this->getLocation()));
-//   }
+  /**
+   * Test the collection page as anonymous.
+   *
+   * Assume the page should contain some links to other material types.
+   */
+  public function testCollectionViewAnonymous() {
+    $this->open("/" . $this->config->getLocale());
+    // Search for potential collection item.
+    $this->abstractedPage->userMakeSearch('frank herbert klit');
+    // Check the item title.
+    $this->assertElementContainsText('css=li.list-item.search-result:first .group_ting_right_col_search .heading a', 'Klit');
+    // Click on title.
+    $this->click("css=li.list-item.search-result:first .group_ting_right_col_search .heading a");
+    $this->waitForPageToLoad("30000");
+    // If this is truly a collection item is should have certain
+    // information about item types and the quantity of those.
+    $this->assertTrue($this->isElementPresent("link=Bog (8)"));
+    $this->assertTrue($this->isElementPresent("link=Lydbog (net) (1)"));
+    $this->assertTrue($this->isElementPresent("link=Lydbog (bånd) (5)"));
+    // Test the anchor links.
+    $this->click("link=Bog (8)");
+    $this->assertTrue((bool) preg_match('/^.*ting\/collection\/870970-basis%3A28443471#Bog$/', $this->getLocation()));
+    $this->click("link=Lydbog (net) (1)");
+    $this->assertTrue((bool) preg_match('/^.*ting\/collection\/870970-basis%3A28443471#Lydbog%20\(net\)$/', $this->getLocation()));
+    $this->click("link=Lydbog (bånd) (5)");
+    $this->assertTrue((bool) preg_match('/^.*ting\/collection\/870970-basis%3A28443471#Lydbog%20\(b%C3%A5nd\)$/', $this->getLocation()));
+  }
 
-//   /**
-//    * Test the collections page as logged in user.
-//    *
-//    * @see testCollectionViewAnonymous()
-//    */
-//   public function testCollectionViewLoggedIn() {
-//     $this->open("/" . TARGET_URL_LANG);
-//     $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-//     $this->type("id=edit-name", TARGET_URL_USER);
-//     $this->type("id=edit-pass", TARGET_URL_USER_PASS);
-//     $this->click("id=edit-submit--2");
-//     $this->waitForPageToLoad("30000");
-//     $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-//     $this->type("id=edit-search-block-form--2", "frank herbert klit");
-//     $this->click("id=edit-submit");
-//     $this->waitForPageToLoad("30000");
-//     $this->assertContains('klit', $this->getText("css=li.list-item.search-result"), '', true);
-//     $this->click("xpath=(//a[contains(text(),'Klit')])[2]");
-//     $this->waitForPageToLoad("30000");
-//     $this->assertTrue($this->isElementPresent("//div[@id='page']/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div"));
-//     $this->assertTrue($this->isElementPresent("//div[@id='page']/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div[2]"));
-//     $this->assertTrue($this->isElementPresent("link=Bog (8)"));
-//     $this->assertTrue($this->isElementPresent("link=Lydbog (net) (1)"));
-//     $this->assertTrue($this->isElementPresent("link=Lydbog (bånd) (5)"));
-//     $this->click("link=Bog (8)");
-//     $this->assertTrue((bool) preg_match('/^.*ting\/collection\/870970-basis%3A28443471#Bog$/', $this->getLocation()));
-//     $this->click("link=Lydbog (net) (1)");
-//     $this->assertTrue((bool) preg_match('/^.*ting\/collection\/870970-basis%3A28443471#Lydbog%20\(net\)$/', $this->getLocation()));
-//     $this->click("link=Lydbog (bånd) (5)");
-//     $this->assertTrue((bool) preg_match('/^.*ting\/collection\/870970-basis%3A28443471#Lydbog%20\(b%C3%A5nd\)$/', $this->getLocation()));
-//   }
+  /**
+   * Test the collections page as logged in user.
+   *
+   * @see testCollectionViewAnonymous()
+   */
+  public function testCollectionViewLoggedIn() {
+    $this->open("/" . $this->config->getLocale());
+    $this->abstractedPage->userLogin($this->config->getUser(), $this->config->getPass());
+    $this->testCollectionViewAnonymous();
+  }
 
 //   /**
 //    * Test the ability to bookmark/reserve as anonymous
