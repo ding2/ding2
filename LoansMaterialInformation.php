669b27b0761962fb9342738892bae3e39ee7eb86
amd_k8 <am@inlead.dk>
2015-04-13 16:51:59 +0300

    DDBTEST 80 - Rework loans information page.

--- LoansMaterialInformation.php
+++ LoansMaterialInformation.php
@@ -1,34 +1,60 @@
 <?php
-class Loans extends PHPUnit_Extensions_SeleniumTestCase
-{
-  protected function setUp()
-  {
-    $this->setBrowser("*firefox");
-    $this->setBrowserUrl("http://ding2tal.easyting.dk/");
+
+require_once(__DIR__ . '/autoload.php');
+require_once(__DIR__ . '/bootstrap.php');
+
+class Loans extends PHPUnit_Extensions_SeleniumTestCase {
+  protected $abstraction;
+  protected $config;
+
+  protected function setUp() {
+    $this->abstractedPage = new DDBTestPageAbstraction($this);
+    $this->config = new DDBTestConfig();
+
+    $this->setBrowser($this->config->getBrowser());
+    $this->setBrowserUrl($this->config->getUrl());
   }
 
-  public function testLoansMaterialInformation()
-  {
-    $this->open("/en");
-    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->type("id=edit-name", "1111110022");
-    $this->type("id=edit-pass", "5555");
-    $this->click("id=edit-submit--2");
-    $this->waitForPageToLoad("30000");
-    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->waitForPageToLoad("30000");
-    $this->assertTrue($this->isElementPresent("link=My account"));
-    $this->assertTrue($this->isElementPresent("//div[@id='page']/div/div/div/div/div/div/aside/div/ul/li[3]/a/span"));
-    $this->click("//div[@id='page']/div/div/div/div/div/div/aside/div/ul/li[3]/a/span");
-    $this->waitForPageToLoad("30000");
-    $this->assertTrue($this->isElementPresent("//div[@id='page']/div/div/div/div/div/div/aside/div/ul/li[3]/ul/li/a/span"));
-    $this->assertTrue($this->isElementPresent("//div[@id='page']/div/div/div/div/div/div/aside/div/ul/li[3]/ul/li[2]/a/span"));
-    $this->assertTrue($this->isElementPresent("//div[@id='page']/div/div/div/div/div/div/aside/div/ul/li[3]/ul/li[3]/a/span"));
-    $this->click("//div[@id='page']/div/div/div/div/div/div/aside/div/ul/li[3]/ul/li[3]/a/span");
-    $this->waitForPageToLoad("30000");
-    $this->assertTrue($this->isElementPresent("css=h2.pane-title"));
-    $this->assertTrue($this->isElementPresent("link=Det godes pris : roman"));
-    $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
-    $this->waitForPageToLoad("30000");
+  public function testLoansMaterialInformation() {
+    $this->open('/' . $this->config->getLocale());
+    $this->abstractedPage->waitForPage();
+    $this->abstractedPage->userLogin($this->config->getUser(), $this->config->getPass());
+
+    // Check for user account link.
+    $this->assertElementPresent('link=My Account');
+    $this->click('link=My Account');
+    $this->abstractedPage->waitForPage();
+
+    // Check for user status link.
+    $this->assertElementPresent('link=User status');
+    $this->click('link=User status');
+    $this->abstractedPage->waitForPage();
+
+    // Check for material related links.
+    // Weird, but these are not translated.
+    $this->assertElementPresent('link=Mine bøder');
+    $this->assertElementPresent('link=Mine reserveringer');
+    $this->assertElementPresent('link=Mine hjemlån');
+
+    // Go to loans page.
+    $this->click('link=Mine hjemlån');
+    $this->abstractedPage->waitForPage();
+
+    // Check for page title.
+    $this->assertElementContainsText('css=h2.pane-title', 'Loan list');
+
+    // There should be at least one loan.
+    $this->assertElementPresent('css=.material-item:first');
+
+    // ... and it should be "Det godes pris : roman".
+    $this->assertElementPresent('link=Det godes pris : roman');
+
+    // Click on it's title.
+    $this->click('link=Det godes pris : roman');
+    $this->abstractedPage->waitForPage();
+
+    // Check the page where link landed.
+    $this->assertTrue((bool) preg_match('/^[\s\S]*ting\/object\/870970-basis%3A50659275$/', $this->getLocation()));
+    $this->assertElementContainsText('css=.pane-ting-object .ting-object.view-mode-full .field-name-ting-title h2', 'Det godes pris : roman');
   }
 }
