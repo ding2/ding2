45f5e8473a6e4382b7c4dd63b3288b76b40364c0
amd_k8 <am@inlead.dk>
2015-04-13 17:47:44 +0300

    DDBTEST 80 - Rework loans page.

--- LoansTestSuite.php
+++ LoansTestSuite.php
@@ -1,12 +1,21 @@
 <?php
 
-require_once(dirname(__FILE__) . '/config.inc');
+require_once(__DIR__ . '/autoload.php');
+require_once(__DIR__ . '/bootstrap.php');
 
-class Loans extends PHPUnit_Extensions_SeleniumTestCase
-{
+class Loans extends PHPUnit_Extensions_SeleniumTestCase {
+  protected $abstraction;
+  protected $config;
+
   protected function setUp() {
-    $this->setBrowser(TARGET_BROWSER);
-    $this->setBrowserUrl(TARGET_URL);
+  $this->abstractedPage = new DDBTestPageAbstraction($this);
+    $this->config = new DDBTestConfig();
+
+    $this->setBrowser($this->config->getBrowser());
+    $this->setBrowserUrl($this->config->getUrl());
+
+    $url = $this->config->getLms() . '/patron/loans?borrCard=' . $this->config->getUser() . '&pinCode=' . $this->config->getPass();
+    $this->mock = new SimpleXMLElement($url, 0, TRUE);
   }
 
   /**
@@ -15,90 +24,51 @@ class Loans extends PHPUnit_Extensions_SeleniumTestCase
    * Check that each loan has corrent date, due date and number.
    */
   public function testMaterialInformation() {
-    $this->open("/" . TARGET_URL_LANG);
-    $this->click("link=Login");
-    $this->type("id=edit-name", TARGET_URL_USER);
-    $this->type("id=edit-pass", TARGET_URL_USER_PASS);
-    $this->click("id=edit-submit--2");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("My Account", $this->getText("//div[@id='page']/header/section/div/ul/li[3]/a/span"));
-    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("My account", $this->getText("//div[@id='page']/div/div/div/div/div/div/aside/div/h2/a"));
-    $this->assertEquals("User status", $this->getText("//div[@id='page']/div/div/div/div/div/div/aside/div/ul/li[3]/a/span"));
-    $this->click("//div[@id='page']/div/div/div/div/div/div/aside/div/ul/li[3]/a/span");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("Loan list", $this->getText("//div[@id='page']/div/div/div/div/div/div/div/div/h2"));
-    // Get mock object.
-    $url = "http://alma.am.ci.inlead.dk/web/alma/patron/loans?borrCard=1111110022&pinCode=5555";
-    $mock = new SimpleXMLElement($url, 0, TRUE);
-    //Get DOM
-    $dom = new DOMDocument();
-    // Disable warnings, cause we have wrong html.
-    libxml_use_internal_errors(true);
-    $dom->loadHTML($this->getHtmlSource());
-    $xpath = new DOMXPath($dom);
-    foreach ($mock->getLoansResponse->loans->children() as $l) {
+    $this->open('/' . $this->config->getLocale());
+    $this->abstractedPage->waitForPage();
+    $this->abstractedPage->userLogin($this->config->getUser(), $this->config->getPass());
 
-      $id = (string)$l->attributes()->id;
-      $result = $xpath->query("//form[@id='ding-loan-loans-form']//li[contains(@class, 'material-number')]//div[contains(., '{$id}')]");
-      $this->assertTrue($result->length == 1);
+    // Check for user account link.
+    $this->assertElementPresent('link=My Account');
+    $this->click('link=My Account');
+    $this->abstractedPage->waitForPage();
 
-      $loanDate = date('d-m-Y H:s', strtotime((string)$l->attributes()->loanDate));
-      $result = $xpath->query("//form[@id='ding-loan-loans-form']/div[div[//li[contains(@class, 'loan-date')]//div[contains(., '{$loanDate}')]
-        and //li[contains(@class, 'material-number')]//div[contains(., '{$id}')]]]");
-      $this->assertTrue($result->length == 1);
+    // Check for user status link.
+    $this->assertElementPresent('link=User status');
+    $this->click('link=User status');
+    $this->abstractedPage->waitForPage();
 
-      $loanDueDate = date('d-m-Y H:s', strtotime((string)$l->attributes()->loanDueDate));
-      $result = $xpath->query("//form[@id='ding-loan-loans-form']/div[div[//li[contains(@class, 'expire-date')]//div[contains(., '{$loanDueDate}')]
-        and //li[contains(@class, 'material-number')]//div[contains(., '{$id}')]]]");
-      $this->assertTrue($result->length == 1);
+    // Go to loans page.
+    $this->click('link=Mine hjemlån');
+    $this->abstractedPage->waitForPage();
+
+    // Check for page title.
+    $this->assertElementContainsText('css=h2.pane-title', 'Loan list');
+
+    // Tricky part.
+    // In order to check the data shown, it's required to have the raw
+    // data from the LMS.
+
+    // Start from this number, since nth-child pseuo-selector
+    // counts from all children, no the direct css selector result.
+    $index = 3;
+    foreach ($this->mock->getLoansResponse->loans->children() as $l) {
+      // Compare loan date.
+      $this->assertElementContainsText('css=#ding-loan-loans-form .material-item:nth-child(' . $index . ') .item-information-list .loan-date .item-information-label', 'Loan date:');
+      $loanDate = date('j. F Y', strtotime((string) $l->attributes()->loanDate));
+      $this->assertElementContainsText('css=#ding-loan-loans-form .material-item:nth-child(' . $index . ') .item-information-list .loan-date .item-information-data', $loanDate);
+
+      // Compare due date.
+      $this->assertElementContainsText('css=#ding-loan-loans-form .material-item:nth-child(' . $index . ') .item-information-list .expire-date .item-information-label', 'Return date:');
+      $dueDate = date('j. F Y', strtotime((string) $l->attributes()->loanDueDate));
+      $this->assertElementContainsText('css=#ding-loan-loans-form .material-item:nth-child(' . $index . ') .item-information-list .expire-date .item-information-data', $dueDate);
+
+      // Compare item id.
+      $this->assertElementContainsText('css=#ding-loan-loans-form .material-item:nth-child(' . $index . ') .item-information-list .material-number .item-information-label', 'Material no.:');
+      $id = (string) $l->attributes()->id;
+      $this->assertElementContainsText('css=#ding-loan-loans-form .material-item:nth-child(' . $index . ') .item-information-list .material-number .item-information-data', $id);
+
+      $index++;
     }
-    $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
-    $this->waitForPageToLoad("30000");
-  }
-
-  /**
-   * Test grouping loans by due date.
-   *
-   * Check that each loan grouped by due date.
-   */
-  public function testGroupByDueDate() {
-    $this->open("/" . TARGET_URL_LANG);
-    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->type("id=edit-name", TARGET_URL_USER);
-    $this->type("id=edit-pass", TARGET_URL_USER_PASS);
-    $this->click("id=edit-submit--2");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("My Account", $this->getText("//div[@id='page']/header/section/div/ul/li[3]/a/span"));
-    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("My account", $this->getText("//div[@id='page']/div/div/div/div/div/div/aside/div/h2/a"));
-    $this->assertEquals("User status", $this->getText("//div[@id='page']/div/div/div/div/div/div/aside/div/ul/li[3]/a/span"));
-    $this->click("//div[@id='page']/div/div/div/div/div/div/aside/div/ul/li[3]/a/span");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("Mine bøder", $this->getText("//div[@id='page']/div/div/div/div/div/div/aside/div/ul/li[3]/ul/li/a/span"));
-    $this->assertEquals("Mine reserveringer", $this->getText("//div[@id='page']/div/div/div/div/div/div/aside/div/ul/li[3]/ul/li[2]/a/span"));
-    $this->assertEquals("Mine hjemlån", $this->getText("//div[@id='page']/div/div/div/div/div/div/aside/div/ul/li[3]/ul/li[3]/a/span"));
-    $this->click("//div[@id='page']/div/div/div/div/div/div/aside/div/ul/li[3]/ul/li[3]/a/span");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("Due in 5 days, 16/09/14", $this->getText("css=label.option"));
-    $this->assertEquals("Det godes pris : roman", $this->getText("link=Det godes pris : roman"));
-    $this->assertEquals("Due in 1 week, 24/09/14", $this->getText("//form[@id='ding-loan-loans-form']/div/div[4]/div/label"));
-    $this->assertEquals("Dukken i spejlet", $this->getText("link=Dukken i spejlet"));
-    $this->assertEquals("Misterioso", $this->getText("link=Misterioso"));
-    $this->assertEquals("Due in 2 weeks, 30/09/14", $this->getText("//form[@id='ding-loan-loans-form']/div/div[7]/div/label"));
-    $this->assertEquals("Farlige drømme : kriminalroman", $this->getText("link=Farlige drømme : kriminalroman"));
-    $this->assertEquals("Due in 4 weeks, 10/10/14", $this->getText("//form[@id='ding-loan-loans-form']/div/div[9]/div/label"));
-    $this->assertEquals("Det gemmer sig i mørket og andre gys", $this->getText("link=Det gemmer sig i mørket og andre gys"));
-    $this->assertEquals("Due in 1 month, 13/10/14", $this->getText("//form[@id='ding-loan-loans-form']/div/div[11]/div/label"));
-    $this->assertEquals("Min søster Jodie", $this->getText("link=Min søster Jodie"));
-    $this->assertEquals("Due in 1 month, 28/10/14", $this->getText("//form[@id='ding-loan-loans-form']/div/div[13]/div/label"));
-    $this->assertEquals("Drengen og gården", $this->getText("link=Drengen og gården"));
-    $this->assertEquals("Elsker mig - elsker mig ikke : kærlighedsnoveller fra hele Norden", $this->getText("link=Elsker mig - elsker mig ikke : kærlighedsnoveller fra hele Norden"));
-    $this->assertEquals("Helt vild med heste : noveller, facts, tips og quiz om heste", $this->getText("link=Helt vild med heste : noveller, facts, tips og quiz om heste"));
-    $this->assertEquals("Kampnat", $this->getText("link=Kampnat"));
-    $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
-    $this->waitForPageToLoad("30000");
   }
 }
