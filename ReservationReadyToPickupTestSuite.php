76e7e1632cbffa5d839c240cfe669ea737627461
amd_k8 <am@inlead.dk>
2015-04-14 20:35:14 +0300

    DDBTEST 80 - Rework reservations ready for pickup.

--- ReservationReadyToPickupTestSuite.php
+++ ReservationReadyToPickupTestSuite.php
@@ -1,97 +1,99 @@
 <?php
 
-require_once(dirname(__FILE__) . '/config.inc');
+require_once(__DIR__ . '/autoload.php');
+require_once(__DIR__ . '/bootstrap.php');
 
 class ReservationReadyToPickup extends PHPUnit_Extensions_SeleniumTestCase {
 
   protected function setUp() {
-    $this->setBrowser(TARGET_BROWSER);
-    $this->setBrowserUrl('http://d2t.am.ci.inlead.dk/');
+    $this->abstractedPage = new DDBTestPageAbstraction($this);
+    $this->config = new DDBTestConfig();
+
+    $this->setBrowser($this->config->getBrowser());
+    $this->setBrowserUrl($this->config->getUrl());
+
+    $url = $this->config->getLms() . '/patron/debts?borrCard=' . $this->config->getUser() . '&pinCode=' . $this->config->getPass();
+    $this->mock = new SimpleXMLElement($url, 0, TRUE);
+
+    resetState();
   }
 
   /**
    * Check reservation ready for pickup data on my reservation page.
    */
-  public function testMaterialInformation()
-  {
-    $this->open("/" . TARGET_URL_LANG);
-    $this->click("css=i.icon-user");
-    $this->type("id=edit-name", TARGET_URL_USER);
-    $this->type("id=edit-pass", TARGET_URL_USER_PASS);
-    $this->click("id=edit-submit--2");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("My Account", $this->getText("//div[@id='page']/header/section/div/ul/li[3]/a/span"));
-    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("My account", $this->getText("//div[@id='page']/div/div/div/div/div/div/aside/div/h2/a"));
-    $this->assertEquals("Reservations ready for pick-up", $this->getText("css=a.user-status.user-status-ready-pickup > span.user-status-label"));
-    $this->click("css=a.user-status.user-status-ready-pickup > span.user-status-label");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("255", $this->getText("//form[@id='ding-reservation-reservations-ready-form']/div/div[3]/div[2]/ul/li/div[2]"));
-    $this->assertEquals("Hjørring", $this->getText("//form[@id='ding-reservation-reservations-ready-form']/div/div[3]/div[2]/ul/li[3]/div[2]"));
-    $this->assertEquals("02-09-2014 00:00", $this->getText("//form[@id='ding-reservation-reservations-ready-form']/div/div[3]/div[2]/ul/li[4]/div[2]"));
-    $this->assertEquals("0", $this->getText("//form[@id='ding-reservation-reservations-ready-form']/div/div[4]/div[2]/ul/li/div[2]"));
-    $this->assertEquals("15-09-2014 00:00", $this->getText("//form[@id='ding-reservation-reservations-ready-form']/div/div[4]/div[2]/ul/li[2]/div[2]"));
-    $this->assertEquals("Bogbus", $this->getText("//form[@id='ding-reservation-reservations-ready-form']/div/div[4]/div[2]/ul/li[3]/div[2]"));
-    $this->assertEquals("02-09-2014 00:00", $this->getText("//form[@id='ding-reservation-reservations-ready-form']/div/div[4]/div[2]/ul/li[4]/div[2]"));
-    $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
-    $this->waitForPageToLoad("30000");
-  }
+  public function testReadyReservation() {
+    $this->open('/' . $this->config->getLocale());
+    $this->abstractedPage->waitForPage();
+    $this->abstractedPage->userLogin($this->config->getUser(), $this->config->getPass());
 
-  /**
-   * Check reservation ready for pickup page and items data on it.
-   */
-  public function testReservationsReadyForPickupMaterialInformation()
-  {
-    $this->open("/" . TARGET_URL_LANG);
-    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->type("id=edit-name", TARGET_URL_USER);
-    $this->type("id=edit-pass", TARGET_URL_USER_PASS);
-    $this->click("id=edit-submit--2");
-    $this->waitForPageToLoad("30000");
-    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->waitForPageToLoad("30000");
-    $this->assertTrue($this->isElementPresent("css=h2.pane-title"));
-    $this->assertTrue($this->isElementPresent("css=a.user-status.user-status-ready-pickup > span.user-status-label"));
-    $this->click("css=a.user-status.user-status-ready-pickup > span.user-status-label");
-    $this->waitForPageToLoad("30000");
-    $this->assertTrue($this->isElementPresent("css=h2.pane-title"));
-    $this->assertTrue($this->isElementPresent("css=label.option"));
-    $this->assertTrue($this->isElementPresent("link=The lady"));
-    $this->assertTrue($this->isElementPresent("link=Angels and demons"));
-    $this->click("link=Logout");
-    $this->waitForPageToLoad("30000");
-  }
+    // Check for user account link.
+    $this->assertElementPresent('link=My Account');
+    $this->click('link=My Account');
+    $this->abstractedPage->waitForPage();
 
-  /**
-   * Check deletion of reservation ready for pickup.
-   */
-  public function testDeleteReservationReadyForPickUp()
-  {
-    $this->open("/" . TARGET_URL_LANG);
-    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->type("id=edit-name", TARGET_URL_USER);
-    $this->type("id=edit-pass", TARGET_URL_USER_PASS);
-    $this->click("id=edit-submit--2");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("My Account", $this->getText("//div[@id='page']/header/section/div/ul/li[3]/a/span"));
-    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("My account", $this->getText("link=My account"));
-    $this->click("css=a.user-status.user-status-ready-pickup > span.user-status-label");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("My reservations", $this->getText("css=h2.pane-title"));
-    $this->click("id=edit-reservations-1414248-1414248");
-    $this->assertTrue($this->isElementPresent("//*[@id=\"edit-actions-top-delete\"]"));
-    $this->mouseDown("//*[@id=\"edit-actions-top-delete\"]");
-    sleep(3);
-    $this->assertTrue((bool)preg_match('/^Are you sure you want to delete these reservations[\s\S]$/',$this->getText("//*[@id=\"ding-reservation-delete-reservations-form\"]/div/div")));
-    $this->mouseDown("//*[@id=\"edit-submit--2\"]");
-    sleep(3);
-    $this->assertEquals("Your reservations have been deleted.", $this->getText("//*[@id=\"ui-id-1\"]"));
-    $this->click("//div[4]/div/button");
-    $this->waitForPageToLoad("30000");
-    $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
-    $this->waitForPageToLoad("30000");
+    // Check for reservation ready for pickup quick link.
+    $this->assertElementPresent('link=2 Reservations ready for pick-up');
+    $this->click('link=2 Reservations ready for pick-up');
+    $this->abstractedPage->waitForPage();
+
+    // Check for correct page heading.
+    $this->assertElementContainsText('css=h2.pane-title', 'My reservations');
+
+    // Next section roughly checks the markup.
+    // This relies on the dummy LMS service, so the data should be pre-defined.
+    $ready_for_pickup = array(
+      array(
+        '255',
+        '',
+        'Hjørring',
+        '2. September 2014',
+      ),
+      array(
+        '0',
+        '15. September 2014',
+        'Bogbus',
+        '2. September 2014',
+      ),
+    );
+    for ($i = 3, $j = 0; $i <= count($ready_for_pickup) + 2; $i++, $j++) {
+      // Check pickup id field.
+      $this->assertElementContainsText('css=#ding-reservation-reservations-ready-form .material-item:nth-child(' . $i . ') li.pickup-id .item-information-label', 'Pickup id:');
+      if (!empty($ready_for_pickup[$j][0])) {
+        $this->assertElementContainsText('css=#ding-reservation-reservations-ready-form .material-item:nth-child(' . $i . ') li.pickup-id .item-information-data', $ready_for_pickup[$j][0]);
+      }
+
+      $this->assertElementContainsText('css=#ding-reservation-reservations-ready-form .material-item:nth-child(' . $i . ') li.pickup-date .item-information-label', 'Pickup date:');
+      if (!empty($ready_for_pickup[$j][1])) {
+        $this->assertElementContainsText('css=#ding-reservation-reservations-ready-form .material-item:nth-child(' . $i . ') li.pickup-date .item-information-data', $ready_for_pickup[$j][1]);
+      }
+
+      $this->assertElementContainsText('css=#ding-reservation-reservations-ready-form .material-item:nth-child(' . $i . ') li.pickup-branch .item-information-label', 'Pickup branch:');
+      if (!empty($ready_for_pickup[$j][2])) {
+        $this->assertElementContainsText('css=#ding-reservation-reservations-ready-form .material-item:nth-child(' . $i . ') li.pickup-branch .item-information-data', $ready_for_pickup[$j][2]);
+      }
+
+      $this->assertElementContainsText('css=#ding-reservation-reservations-ready-form .material-item:nth-child(' . $i . ') li.created-date .item-information-label', 'Created date:');
+      if (!empty($ready_for_pickup[$j][3])) {
+        $this->assertElementContainsText('css=#ding-reservation-reservations-ready-form .material-item:nth-child(' . $i . ') li.created-date .item-information-data', $ready_for_pickup[$j][3]);
+      }
+    }
+
+    // Test the ability to delete a reservation ready for pickup.
+    // Check if the checkbox for certain item is present, then click it.
+    $this->assertElementPresent('css=#ding-reservation-reservations-ready-form .material-item:nth-child(4) #edit-reservations-1414140-1414140');
+    $this->click('css=#ding-reservation-reservations-ready-form .material-item:nth-child(4) #edit-reservations-1414140-1414140');
+
+    // A delete button should appear.
+    $this->abstractedPage->waitForElement('css=#edit-actions-top-delete');
+    $this->mouseDown('css=#edit-actions-top-delete');
+
+    // This should trigger a popup confirmation.
+    $this->abstractedPage->waitForElement('css=.ding-popup-content #ding-reservation-delete-reservations-form');
+    $this->mouseDown('css=.ding-popup-content #ding-reservation-delete-reservations-form input[type="submit"]');
+    // Wait for ajax to finish.
+    sleep(5);
+    $this->abstractedPage->refresh();
+    // Check the item is deleted.
+    $this->assertElementNotPresent('css=#ding-reservation-reservations-ready-form .material-item:nth-child(4)');
   }
 }
