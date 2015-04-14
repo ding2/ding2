75ad5a96bb966558245a9b3c0e513699bbe97a50
amd_k8 <am@inlead.dk>
2015-04-14 20:42:00 +0300

    DDBTEST 80 - Test the rrp titles.

--- ReservationReadyToPickupTestSuite.php
+++ ReservationReadyToPickupTestSuite.php
@@ -47,31 +47,39 @@ class ReservationReadyToPickup extends PHPUnit_Extensions_SeleniumTestCase {
         '',
         'Hjørring',
         '2. September 2014',
+        'The lady',
       ),
       array(
         '0',
         '15. September 2014',
         'Bogbus',
         '2. September 2014',
+        'Angels and demons'
       ),
     );
     for ($i = 3, $j = 0; $i <= count($ready_for_pickup) + 2; $i++, $j++) {
+      // Check title field.
+      $this->assertElementContainsText('css=#ding-reservation-reservations-ready-form .material-item:nth-child(' . $i . ') h3.item-title', $ready_for_pickup[$j][4]);
+
       // Check pickup id field.
       $this->assertElementContainsText('css=#ding-reservation-reservations-ready-form .material-item:nth-child(' . $i . ') li.pickup-id .item-information-label', 'Pickup id:');
       if (!empty($ready_for_pickup[$j][0])) {
         $this->assertElementContainsText('css=#ding-reservation-reservations-ready-form .material-item:nth-child(' . $i . ') li.pickup-id .item-information-data', $ready_for_pickup[$j][0]);
       }
 
+      // Check pickup date field.
       $this->assertElementContainsText('css=#ding-reservation-reservations-ready-form .material-item:nth-child(' . $i . ') li.pickup-date .item-information-label', 'Pickup date:');
       if (!empty($ready_for_pickup[$j][1])) {
         $this->assertElementContainsText('css=#ding-reservation-reservations-ready-form .material-item:nth-child(' . $i . ') li.pickup-date .item-information-data', $ready_for_pickup[$j][1]);
       }
 
+      // Check pickup branch field.
       $this->assertElementContainsText('css=#ding-reservation-reservations-ready-form .material-item:nth-child(' . $i . ') li.pickup-branch .item-information-label', 'Pickup branch:');
       if (!empty($ready_for_pickup[$j][2])) {
         $this->assertElementContainsText('css=#ding-reservation-reservations-ready-form .material-item:nth-child(' . $i . ') li.pickup-branch .item-information-data', $ready_for_pickup[$j][2]);
       }
 
+      // Check pickup created field.
       $this->assertElementContainsText('css=#ding-reservation-reservations-ready-form .material-item:nth-child(' . $i . ') li.created-date .item-information-label', 'Created date:');
       if (!empty($ready_for_pickup[$j][3])) {
         $this->assertElementContainsText('css=#ding-reservation-reservations-ready-form .material-item:nth-child(' . $i . ') li.created-date .item-information-data', $ready_for_pickup[$j][3]);
