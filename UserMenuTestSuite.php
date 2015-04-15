e5242b50c774536217fc36da3611bff87871ae29
amd_k8 <am@inlead.dk>
2015-04-15 14:37:52 +0300

    DDBTEST 80 - Rework user menu.

--- UserMenuTestSuite.php
+++ UserMenuTestSuite.php
@@ -1,106 +1,57 @@
 <?php
 
-require_once(dirname(__FILE__) . '/config.inc');
+require_once(__DIR__ . '/autoload.php');
 
 class UserMenu extends PHPUnit_Extensions_SeleniumTestCase {
+  protected $abstractedPage;
+  protected $config;
 
   protected function setUp() {
-    $this->setBrowser(TARGET_BROWSER);
-    $this->setBrowserUrl(TARGET_URL);
+    $this->abstractedPage = new DDBTestPageAbstraction($this);
+    $this->config = new DDBTestConfig();
+    $this->setBrowser($this->config->getBrowser());
+    $this->setBrowserUrl($this->config->getUrl());
   }
 
   /**
    * Check loan link and loan page in user profile.
    */
-  public function testClickOnLoans()
-  {
-    $this->open("/" . TARGET_URL_LANG);
-    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->type("id=edit-name", TARGET_URL_USER);
-    $this->type("id=edit-pass", TARGET_URL_USER_PASS);
-    $this->click("id=edit-submit--2");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("My Account", $this->getText("//div[@id='page']/header/section/div/ul/li[3]/a/span"));
-    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->waitForPageToLoad("30000");
-    $this->assertTrue($this->isElementPresent("//div[@id='page']/div/div/div/div/div/div/aside/div/h2/a"));
-    $this->assertEquals("Loans", $this->getText("//div[@id='page']/div/div/div/div/div/div/div/div/div/a[3]/span[2]"));
-    $this->click("//div[@id='page']/div/div/div/div/div/div/div/div/div/a[3]/span[2]");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("Loan list", $this->getText("//div[@id='page']/div/div/div/div/div/div/div/div/h2"));
-    $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
-    $this->waitForPageToLoad("30000");
-  }
+  public function testUserMenu() {
+    $this->open('/' . $this->config->getLocale());
+    $this->abstractedPage->waitForPage();
+    $this->abstractedPage->userLogin($this->config->getUser(), $this->config->getPass());
 
-  /**
-   * Check reservation link and reservation page.
-   */
-  public function testClickOnReservation()
-  {
-    $this->open("/" . TARGET_URL_LANG);
-    $this->click("link=Login");
-    $this->type("id=edit-name", TARGET_URL_USER);
-    $this->type("id=edit-pass", TARGET_URL_USER_PASS);
-    $this->click("id=edit-submit--2");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("My Account", $this->getText("//div[@id='page']/header/section/div/ul/li[3]/a/span"));
-    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("My account", $this->getText("link=My account"));
-    $this->assertTrue($this->isElementPresent("css=a.user-status.user-status-reservation > span.user-status-label"));
-    $this->click("css=a.user-status.user-status-reservation > span.user-status-label");
-    $this->waitForPageToLoad("30000");
-    $this->assertEquals("My reservations", $this->getText("css=h2.pane-title"));
-    $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
-    $this->waitForPageToLoad("30000");
-  }
+    // Expect user page link.
+    $this->assertElementPresent('link=My Account');
+    $this->click('link=My Account');
+    $this->abstractedPage->waitForPage();
 
-  /**
-   * Check bookmark link and bookmark page.
-   */
-  public function testClickOnBookmark()
-  {
-    $this->open("/" . TARGET_URL_LANG);
-    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->type("id=edit-name", TARGET_URL_USER);
-    $this->type("id=edit-pass", TARGET_URL_USER_PASS);
-    $this->click("id=edit-submit--2");
-    $this->waitForPageToLoad("30000");
-    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->waitForPageToLoad("30000");
-    $this->assertTrue($this->isElementPresent("link=My account"));
-    $this->assertTrue($this->isElementPresent("//div[@id='page']/div/div/div/div/div/div/aside/div/ul/li[5]/a/span"));
-    $this->click("//div[@id='page']/div/div/div/div/div/div/aside/div/ul/li[5]/a/span");
-    $this->waitForPageToLoad("30000");
-    $this->assertTrue($this->isElementPresent("css=label.option"));
-    $this->assertTrue($this->isElementPresent("//form[@id='ding-bookmark-import-form']/div/div/label"));
-    $this->assertTrue($this->isElementPresent("//div[@id='user-bookmarks']/div/aside/div/ul/li[5]/a/span"));
-    $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
-    $this->waitForPageToLoad("30000");
-  }
+    // Check the page we are now.
+    $this->assertElementContainsText('css=.pane-title', 'Your user loan status');
 
-  /**
-   * Check debts link and page.
-   */
-  public function testLinkToMyDebts()
-  {
-    $this->open("/" . TARGET_URL_LANG);
-    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->type("id=edit-name", TARGET_URL_USER);
-    $this->type("id=edit-pass", TARGET_URL_USER_PASS);
-    $this->click("id=edit-submit--2");
-    $this->waitForPageToLoad("30000");
-    $this->click("//div[@id='page']/header/section/div/ul/li[3]/a/span");
-    $this->waitForPageToLoad("30000");
-    $this->assertTrue($this->isElementPresent("css=h2.pane-title"));
-    $this->assertTrue($this->isElementPresent("link=My account"));
-    $this->assertTrue($this->isElementPresent("css=span.user-status-label"));
-    $this->click("css=span.user-status-label");
-    $this->waitForPageToLoad("30000");
-    $this->assertTrue($this->isElementPresent("css=h2.pane-title"));
-    $this->assertTrue($this->isElementPresent("id=edit-pay-selected"));
-    $this->assertTrue($this->isElementPresent("id=edit-pay-all"));
-    $this->click("//div[@id='page']/header/section/div/ul/li[5]/a/span");
-    $this->waitForPageToLoad("30000");
+    // Check for the user menu.
+    $this->assertElementPresent('link=User status');
+    $this->click('link=User status');
+    $this->abstractedPage->waitForPage();
+
+    // Check for menu items.
+    $this->assertElementPresent('link=Mine bøder');
+    $this->assertElementPresent('link=Mine reserveringer');
+    $this->assertElementPresent('link=Mine hjemlån');
+
+    // Check debts page.
+    $this->click('link=Mine bøder');
+    $this->abstractedPage->waitForPage();
+    $this->assertElementContainsText('css=.pane-title', 'My debts');
+
+    // Check reservations page.
+    $this->click('link=Mine reserveringer');
+    $this->abstractedPage->waitForPage();
+    $this->assertElementContainsText('css=.pane-title', 'My reservations');
+
+    // Check loans page.
+    $this->click('link=Mine hjemlån');
+    $this->abstractedPage->waitForPage();
+    $this->assertElementContainsText('css=.pane-title', 'Loan list');
   }
 }
