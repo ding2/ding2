43df9043de01ee65a0ca6c465c0bb050026ae2d6
Morten Bartvig <bartvig@gmail.com>
2015-04-28 08:45:00 +0000

    Bug #597. Added previously removed styles in CKeditor for aligning images on left and on right.

--- js/ding_content.editor_config.js
+++ js/ding_content.editor_config.js
@@ -84,4 +84,6 @@ CKEDITOR.stylesSet.add('default',[
   {name:'Condensed Table',element:'table',attributes:{'class': 'table-condensed'}},
   {name:'Borderless Table',element:'table',attributes:{'class': 'table-borderless'}},
   {name:'Striped  Table',element:'table',attributes:{'class': 'table-striped'}},
+  {name:'Image on Left',element:'img',attributes:{style:'padding: 5px; margin-right: 5px',border:'2',align:'left'}},
+  {name:'Image on Right',element:'img',attributes:{style:'padding: 5px; margin-left: 5px',border:'2',align:'right'}}
 ]);
