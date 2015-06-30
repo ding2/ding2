0015f38cfb6a718298dea3768a9b1f97267d08ae
Agnete Moos <agms@sonderborg.dk>
2015-06-30 10:07:01 +0200

    Add styling of related content blocks

--- templates/ting-search-context.tpl.php
+++ templates/ting-search-context.tpl.php
@@ -1,24 +1,24 @@
 <?php
 /**
  * @file
- * Default theme implementation for displaying ting new materials results.
+ * Default theme implementation for displaying related content.
  *
+ * @see template_preprocess_ting_search_context()
  */
 ?>
 <?php if ($columns) : ?>
-  <div class="ting-search-context">
-    <div class="group-blocks--inner  group-blocks--four">
+  <div class="ting-search-context-blocks--wrapper">
+    <div class="ting-search-context-blocks--inner  ting-search-context-blocks--four">
       <?php foreach ($columns as $columns_class => $column) : ?>
         <div class="<?php print $columns_class ?>">
-          <a href="<?php print $column['url'] ?>"> 
-            <?php print $column['image'] ?>
-          <h2>  <?php print $column['title'] ?> </h2></a>
+          <a href="<?php print $column['url'] ?>">
+          <?php print $column['image'] ?><h2 class="heading"><?php print $column['title'] ?></h2></a>
         </div>
     <?php endforeach; ?>
     </div></div>
   <?php endif; ?>
 
-    
+
 
 
 
