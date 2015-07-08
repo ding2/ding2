775b2a8815ee5f1ec3fb7d48c718086ad126daa9
Agnete Moos <agms@sonderborg.dk>
2015-07-08 13:08:59 +0200

    Basic carousel

--- js/ting_search_context.js
+++ js/ting_search_context.js
@@ -1,56 +1,118 @@
 /**
- * @file ding.availability.js
- * JavaScript behaviours for fetching and displaying availability.
+ * @file
+ * Handles the carousels loading of content. The carousel can be loaded in three
+ * different locations on the search page. A Drupal.settings variable indicates
+ * the desired location.
+ * two selectors to change tabs based on breaks points (which is handle by the
+ * theme).
+ *
+ * For large screens the normal tab list (ul -> li) is used while on small
+ * screens (mobile/tables) a select dropdown is used.
+ *
  */
-
-(function($) {
+(function ($) {
   "use strict";
 
-  Drupal.behaviors.tingSearchContextGetNodes = {
-    attach: function(context, settings) {
+  var TingSearchContext = (function() {
+
+    var carousel = null;
+    var content;
+
+
+
+    /**
+     * Private: Makes an ajax call to the server to get new content for the
+     * carousel. The search context is send along with the ajax request.
+     */
+    function _fetch() {
       $.ajax({
-        type: "POST",
-        url: Drupal.settings.basePath + "ting/ting_search_context/content/ajax",
-        dataType: "json",
+        type: 'post',
+        url : Drupal.settings.basePath + 'ting/ting_search_context/content/ajax',
+        dataType : 'json',
         data: {
-          "search_context" : settings.ting_search_context
+          'search_context' : Drupal.settings.ting_search_context
         },
-        success: function (data) {
-          if (data != null) {
-            var container;
-            container = ting_search_context_get_container(settings.ting_search_context_position);
-            container.html(data);
-          }
+        success : function(data) {
+          content = data;
+          // Update the carousel.
+          _update();
         }
       });
-
     }
-  };
+
+    /**
+     * Private: Select carousel-posistion on page based on variable in Drupal.settings.
+     * Sets the carousel element.
+     */
+    function _set_element() {
+      var position = Drupal.settings.ting_search_context_position;
+
+      $('.pane-search-context').each(function(index) {
+
+        if ($(this).hasClass(position)) {
+          carousel = $(this).find('.rs-carousel-items');
+        }
+      });
+    }
+
+    /**
+     * Private: Updates the carousel after content has been fetched.
+     */
+    function _update() {
+      // Remove spinner.
+      $('.rs-carousel-inner .ajax-loader').addClass('element-hidden');
+
+      // Update content.
+      $('.rs-carousel .rs-carousel-runner').append(content);
+
+      // Show navigation arrows.
+      $('.rs-carousel-action-prev').show();
+      $('.rs-carousel-action-next').show();
+
+      // Get the carousel running.
+      carousel.carousel('refresh');
+      carousel.carousel('goToPage', 0);
+    }
+
+
+
+    /**
+     * Public: Init the carousel and fetch content.
+     */
+    function init() {
+
+      // Pick and set the carousel element.
+      _set_element();
+
+      // Exit if no carousel element is selected.
+      if(carousel === null) {
+        return;
+      }
+
+      // Start the carousel.
+      carousel.carousel({
+        noOfRows: 1,
+        orientation: 'horizontal',
+        itemsPerTransition: 'auto'
+      });
+
+      // Fetch content for the carousel.
+      _fetch();
+    }
+
+    /**
+     * Expoes public functions.
+     */
+    return {
+        name: 'ting_search_context',
+        init: init
+    };
+  })();
 
   /**
-   * Choose position for related content based on search_context_position_setting
-   *
-   * Deside which element should hold the related content based on
-   * the search_context_position_setting
-   *
-   * @param position
-   *   Class name of element to hold the related content.
-   * @returns container
-   *   Element choosen to hold the related content.
+   * Start the carousel when the document is ready.
    */
-  function ting_search_context_get_container(position) {
-    //the default position is below the search result so if there is no class-match the last occurance is the best choice.
-    var container;
-    container = $(".pane-search-context").last;
-
-    $(".pane-search-context").each(function( index ) {
-      if ($(this).hasClass(position)) {
-        container = $(this);
-      }
-    });
-    return container;
-  }
-
+  $(document).ready(function() {
+    TingSearchContext.init();
+  });
 })(jQuery);
-
-
