5a749c1f85fcb904eeae81611446590d3165fe33
Mikkel Ricky <rimi@rimi.local>
2015-08-12 09:01:23 +0200

    Issue #1399 by rimi-itk: Fixed preview template

--- modules/bpi/templates/bpi-preview-item.tpl.php
+++ modules/bpi/templates/bpi-preview-item.tpl.php
@@ -13,21 +13,21 @@
     <div class="details-left">
       <p class="item-details-author">
         <span class="item-details-author-label details-label"><?php echo bpi_label_mapper('author'); ?>: </span>
-        <span class="item-details-author-value details-value"><?php echo l($item['author'], 'admin/bpi', array('query' => _bpi_build_query('author', $item['author']))); ?></span>
+        <span class="item-details-author-value details-value"><?php echo l($item['author'], 'admin/bpi', array('query' => _bpi_get_filter_query('author', $item['author']))); ?></span>
       </p>
       <p class="item-details-agency">
         <span class="item-details-agency-label details-label"><?php echo bpi_label_mapper('agency'); ?>: </span>
-        <span class="item-details-agency-value details-value"><?php echo l($item['agency_name'], 'admin/bpi', array('query' => _bpi_build_query('agency', $item['agency_id']))); ?></span>
+        <span class="item-details-agency-value details-value"><?php echo l($item['agency_name'], 'admin/bpi', array('query' => _bpi_get_filter_query('agency', $item['agency_id']))); ?></span>
       </p>
     </div>
     <div class="details-right">
       <p class="item-details-category">
         <span class="item-details-category-label details-label"><?php echo bpi_label_mapper('category'); ?>: </span>
-        <span class="item-details-category-value details-value"><?php echo l($item['category'], 'admin/bpi', array('query' => _bpi_build_query('category', $item['category']))); ?></span>
+        <span class="item-details-category-value details-value"><?php echo l($item['category'], 'admin/bpi', array('query' => _bpi_get_filter_query('category', $item['category']))); ?></span>
       </p>
       <p class="item-details-audience">
         <span class="item-details-audience-label details-label"><?php echo bpi_label_mapper('audience'); ?>: </span>
-        <span class="item-details-audience-value details-value"><?php echo l($item['audience'], 'admin/bpi', array('query' => _bpi_build_query('audience', $item['audience']))); ?></span>
+        <span class="item-details-audience-value details-value"><?php echo l($item['audience'], 'admin/bpi', array('query' => _bpi_get_filter_query('audience', $item['audience']))); ?></span>
       </p>
       <p class="item-details-material">
         <span class="details-label"><?php echo bpi_label_mapper('material'); ?>: </span>
