f57ba88f9f273dc328df0352bfadfc1f9d58889b
makcumm <mm@inlead.dk>
2015-11-24 10:40:38 +0100

    Issue #49 by Inlead: Change class name for red text style.

--- modules/ding_content/js/ding_content.editor_config.js
+++ modules/ding_content/js/ding_content.editor_config.js
@@ -85,5 +85,5 @@ CKEDITOR.stylesSet.add('default',[
   {name:'Striped  Table',element:'table',attributes:{'class': 'table-striped'}},
   {name:'Image on Left',element:'img',attributes:{style:'padding: 5px; margin-right: 5px',border:'2',align:'left'}},
   {name:'Image on Right',element:'img',attributes:{style:'padding: 5px; margin-left: 5px',border:'2',align:'right'}},
-  {name:'Red text', element:'span',attributes:{class:'ding_red_text'}}
+  {name:'Red text', element:'span',attributes:{class:'red-highlight'}}
 ]);
