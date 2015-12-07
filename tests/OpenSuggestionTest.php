50c98547b68846ece05b19e5d696d4b4f3294185
AM <am@inlead.dk>
2015-12-07 12:01:06 +0200

    Opensuggestion test.

--- tests/OpenSuggestionTest.php
+++ tests/OpenSuggestionTest.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once(__DIR__ . '/../bootstrap.php');
+
 class OpenSuggestionTest extends PHPUnit_Extensions_SeleniumTestCase {
   protected $abstractedPage;
   protected $config;
@@ -7,18 +9,54 @@ class OpenSuggestionTest extends PHPUnit_Extensions_SeleniumTestCase {
   protected function setUp() {
     $this->abstractedPage = new DDBTestPageAbstraction($this);
     $this->config = new DDBTestConfig();
-
     $this->setBrowser($this->config->getBrowser());
     $this->setBrowserUrl($this->config->getUrl());
+    resetState($this->config->getLms());
+  }
 
-    }
+  /**
+   * Test the autosuggestion functionality in search form for
+   * anonymous user.
+   *
+   * Check some pre-tested values to be autosuggested.
+   */
+  public function testSearchAutosuggestionAnonymous() {
+    $this->open('/' . $this->config->getLocale());
+    // Type something in search field and force autocomplete
+    // to be shown. Check simple query.
+    $this->type('css=#edit-search-block-form--2', 'nors');
+    $this->fireEvent('css=#edit-search-block-form--2', 'keyup');
+    $this->abstractedPage->waitForElement('css=#autocomplete');
+    // Check the first result from autocomplete.
+    $this->abstractedPage->waitForElement('css=#autocomplete ul li:first');
+    $this->assertElementContainsText('css=#autocomplete ul li:first', 'norsk');
+    // Check utf8 characters.
+    $this->type('css=#edit-search-block-form--2', 'gæk');
+    $this->fireEvent('css=#edit-search-block-form--2', 'keyup');
+    $this->abstractedPage->waitForElement('css=#autocomplete');
+    // Check the first result from autocomplete.
+    $this->abstractedPage->waitForElement('css=#autocomplete ul li:first');
+    $this->assertElementContainsText('css=#autocomplete ul li:first', 'gækkebreve');
+    // Check that active suggestion is highlighted.
+    // Force 'down' key press.
+    $this->keyDown('css=#edit-search-block-form--2', '\\40');
+    $this->assertEquals('selected', $this->getAttribute('css=#autocomplete ul li:first@class'));
+    // Click the first result and check the search form to be
+    // populated with this text.
+    $this->mouseDown('css=#autocomplete ul li:first');
+    $this->assertEquals('gækkebreve', $this->getValue('css=#edit-search-block-form--2'));
+  }
 
-  public function testLoansMaterialInformation() {
-    $this->windowMaximize();
+  /**
+   * Test the autosuggestion functionality in search form for
+   * logged in user.
+   *
+   * @see testSearchAutosuggestionAnonymous()
+   */
+  public function testSearchAutosuggestionLoggedIn() {
     $this->open('/' . $this->config->getLocale());
     $this->abstractedPage->waitForPage();
-    $this->type("id=edit-search-block-form--2", "abba");
-    $this->fireEvent("id=edit-search-block-form--2", 'keyup');
-    $this->abstractedPage->waitForElement('css=#autocomplete');
+    $this->abstractedPage->userLogin($this->config->getUser(), $this->config->getPass());
+    $this->testSearchAutosuggestionAnonymous();
   }
 }
