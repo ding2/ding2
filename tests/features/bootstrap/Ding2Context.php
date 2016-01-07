7b2f540d095c16dccf11e2cb2d01cf3128504ee3
Thomas Fini Hansen <xen@xen.dk>
2016-01-07 10:57:35 +0100

    Add library user login step definition.

---
+++ tests/features/bootstrap/Ding2Context.php
@@ -0,0 +1,52 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+/**
+ * Provides step definitions for interacting with Ding2.
+ */
+class Ding2Context implements Context, SnippetAcceptingContext
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+
+
+    /** @var \Drupal\DrupalExtension\Context\DrupalContext */
+    private $drupalContext;
+
+    /** @BeforeScenario */
+    public function gatherContexts(BeforeScenarioScope $scope)
+    {
+        $environment = $scope->getEnvironment();
+
+        $this->drupalContext = $environment->getContext('Drupal\DrupalExtension\Context\DrupalContext');
+    }
+
+    /**
+     * @Given I am logged in as a library user
+     * @When I log in as a library user
+     */
+    public function iAmLoggedInAsALibraryUser()
+    {
+        $name = $this->drupalContext->getRandom()->name(8);
+        $user = (object) array(
+            'name' => $name,
+            'pass' => substr($name, -4),
+        );
+        $this->drupalContext->user = $user;
+        $this->drupalContext->login();
+    }
+
+}
