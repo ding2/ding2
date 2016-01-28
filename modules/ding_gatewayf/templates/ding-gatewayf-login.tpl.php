03149e9d078a0efd76e13b406db626a29c2b7220
Jesper Kristensen <cableman@linuxdev.dk>
2016-01-28 17:42:38 +0100

    Issue #659 by cableman: Add style to the gatewayf login link

--- modules/ding_gatewayf/templates/ding-gatewayf-login.tpl.php
+++ modules/ding_gatewayf/templates/ding-gatewayf-login.tpl.php
@@ -6,14 +6,10 @@
  *
  * Available variables:
  *  - $link: link to the login as a render array.
- *
- * @TODO: The classes here are not prefixed with gatewayf, but just wayf. It's
- *        this way to support sites that have already used the other WAYF module
- *        and can be changed later on.
  */
 ?>
-<div class="ding-wayf--login-block">
-  <span class="ding-wayf--login-info">
-    <?php print drupal_render($link); ?>
+<div class="ding-gatewayf--login-block js-topbar-user">
+  <span class="ding-gatewayf--login-info">
+    <?php print drupal_render($link) ?>
   </span>
 </div>
