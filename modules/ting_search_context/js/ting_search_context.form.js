466ce2bd3eac45e99b7c85a49d50941f4da3a120
Arni <al@randersbib.dk>
2016-02-02 10:29:02 +0100

    Added module ting_search_context for code review

---
+++ modules/ting_search_context/js/ting_search_context.form.js
@@ -0,0 +1,25 @@
+(function($) {
+
+	Drupal.behaviors.tingSearchContextForm = {
+
+		attach: function(context, settings) {
+			// Hide rating select box on the last field item.
+			$("#edit-field-ting-search-context tr:last-child td:nth-child(2) div:nth-child(2)", context).hide();
+			// Change hanlder on context name select box.
+			$(".field_ting_search_context_name", context).change(function(e) {
+				var ratingFormItem = $(this).parent().next();
+				$(this).val() == 0 ? ratingFormItem.hide() : ratingFormItem.show();
+			});			
+			// CLick handler for the reset button.
+			$(".field_ting_search_context_remove", context).click(function(e) {
+				e.preventDefault();
+				var ratingFormItem = $(this).prev();
+				var nameFormItem = ratingFormItem.prev();
+				ratingFormItem.hide();
+				nameFormItem.find("select").val(0);
+			});			
+		}
+
+	};	
+
+})(jQuery);
