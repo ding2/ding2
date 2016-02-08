7741e8005baf3062fa82d1d8f25b0160fe0a7587
Jesper Kristensen <cableman@linuxdev.dk>
2016-02-08 14:39:08 +0100

    Issue #659 by cableman: Theme function for wrapping pages in the right DDBasic classes

---
+++ modules/ding_frontend/templates/ding-frontend-panel-wrapper.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ * Default template for ding_frontend_panel_wrapper theme function.
+ *
+ * It's used to wrap non-panel pages in the right classes to get the right look.
+ *
+ * @param $variables
+ *   An associative array containing:
+ */
+?>
+<?php print $variables['element']['#children'] ?>
