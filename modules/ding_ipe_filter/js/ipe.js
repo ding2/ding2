b5123cbddb4e651d7ee51b946c0ccc5ee43e970a
Nicolae Grigoriu <ng@inlead.dk>
2016-02-12 16:31:51 +0200

    Issue #1599 by Inlead: Add Ding IPE Filter module.

---
+++ modules/ding_ipe_filter/js/ipe.js
@@ -0,0 +1,19 @@
+(function ($) {
+  $('.ipe-trigger').live('click', function(e) {
+    var links = '#ipe-add-' + $(this).attr('target_region');
+    var link_offset = Math.abs(parseInt(screen.width) - parseInt($(this).parent().offset().left));
+    var direction = (link_offset > 800) ? 'left' : 'right';
+    var options = {
+      'my': direction + ' top',
+      'at': 'left bottom',
+      'of': $(this),
+      'offset': '0 3'
+    };
+    $(links).fadeToggle('fast').position(options);
+    return false;
+  });
+
+  $('.ipe-popup').live('mouseleave', function() {
+    $(this).fadeToggle('fast');
+  });
+})(jQuery);
