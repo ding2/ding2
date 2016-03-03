9a2740ca342bc4ab420dbae35692735eb9dff840
Martin Cording <guddo@me.com>
2016-03-03 21:25:46 +0100

    1638 by Inlead: Move auto focus implementation to common js file.

--- themes/ddbasic/scripts/ddbasic.common.js
+++ themes/ddbasic/scripts/ddbasic.common.js
@@ -1,6 +1,33 @@
 (function($) {
 
   /**
+   * Auto focus inputs.
+   */
+  function auto_focus_inputs() {
+    // Focus on username form field when entering login form.
+    $('#user-login-form #edit-name').focus();
+
+    // Focus on username form field when clicking login tab.
+    $('.topbar-link-user').bind('click', function(event) {
+      $('#user-login-form #edit-name').focus();
+    });
+
+    // Unfocus login form when user wants to scroll with key buttons (i.e. clicks     the up or down button).
+    $('#user-login-form').bind('keydown', function(event) {
+      var keymap = {
+        up: 38,
+        down: 40
+      };
+
+      // Only unfocus if we press the up or down key.
+      if (keymap.up == event.which || keymap.down == event.which) {
+        document.activeElement.blur();
+      }
+    });
+
+  }
+
+  /**
    * Toggle opening hours
    */
   function toggle_opening_hours() {
@@ -71,6 +98,9 @@
 
   // When ready start the magic.
   $(document).ready(function () {
+    // Auto focus inputs.
+    auto_focus_inputs();
+
     // Toggle opening hours.
     toggle_opening_hours();
 
