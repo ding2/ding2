177378d14c31b64ea8af676afee669c38414ece2
Simon Holt <simonholt83@gmail.com>
2016-03-12 00:58:42 +0100

    Added Webtrends profile select tracking

---
+++ js/ting_field_search.webtrends.js
@@ -0,0 +1,21 @@
+
+(function($) {
+
+  Drupal.behaviors.tingFieldSearchWebtrends = {
+    attach: function(context, settings) {
+      if (typeof dcsMultiTrack === "undefined") {
+        return;
+      }
+
+      // Track when a profile is selected if Webtrends is selected.
+      $("#edit-ting-field-search", context).change(function() {
+        var profile_name = $(this).children(":selected").val();
+        var uri = "/ting-field-search/profile-selected/" + profile_name;
+
+        dcsMultiTrack("WT.ti", profile_name, "DCS.uri", uri);
+        alert(profile_name + ": " + profile_title);
+      });
+    }
+  };
+
+})(jQuery);
