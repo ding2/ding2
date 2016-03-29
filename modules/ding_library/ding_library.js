15969011063f04b0ce3b6fab1ce87e2e67b029ae
pjohans <pjohans@gmail.com>
2016-03-29 08:13:16 +0200

    Issue 773 by Pjohans: fixed scrutinizer coding styles issues

--- modules/ding_library/ding_library.js
+++ modules/ding_library/ding_library.js
@@ -1,13 +1,14 @@
 (function ($) {
-    Drupal.behaviors.pathAutoDisable = {
-        attach: function (context) {
-            // fake a disabled checkbox
-            var check = $('#edit-path-pathauto');
-            check.attr('checked', 'checked');
-            check.css('opacity','0.5');
-            check.change(function(context){
-                check.attr('checked', 'checked');
-            })
-        }
+  "use strict";
+  Drupal.behaviors.pathAutoDisable = {
+    attach: function () {
+      // fake a disabled checkbox
+      var check = $('#edit-path-pathauto');
+      check.attr('checked', 'checked');
+      check.css('opacity', '0.5');
+      check.change(function () {
+        check.attr('checked', 'checked');
+      });
     }
+  };
 })(jQuery);
