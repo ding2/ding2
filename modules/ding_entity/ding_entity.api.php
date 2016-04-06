8b8439ae22bc2521d09099c10bd9d29852f023a5
Thomas Fini Hansen <xen@xen.dk>
2016-04-06 21:12:28 +0200

    1445: Unlock fields
    Remove hook_ding_entity_fields() and rely on virtual_field and features
    for field addition an management.
    
    Removes the hook implementations.
    Update hooks to unlock the old fields.
    Add the proper virtual_field definitions to the fields.

--- modules/ding_entity/ding_entity.api.php
+++ modules/ding_entity/ding_entity.api.php
@@ -52,50 +52,6 @@ function hook_ding_entity_collection_view(&$object, $view_mode) {
 }
 
 /**
- * Tell ding_entity about ding_entity fields, so it can create the
- * proper fields and instances.
- *
- * @return
- *   An array whose keys are the names of the field, and the value is
- *   an array with the following key value pairs:
- *   - field: An array of field settings, as accepted by
- *     field_create_field. field_name and type defaults to the same as
- *     the name of the field.
- *   - instance: Instance settings, as accepted by
- *     field_create_instance, with the difference that entity_type and
- *     bundle will be set automatically depending on ding_entity_type,
- *     and ding_entity_type itself that defaults to 'ding_entity'.
- */
-function hook_ding_entity_fields() {
-  return array(
-    'ding_availability_item' => array(
-      'field' => array(
-        'type' => 'ding_availability_item',
-        'locked' => TRUE,
-        'storage' => array(
-          'type' => 'virtual_field',
-        ),
-      ),
-      'instance' => array(
-        'label' => t('Availability information'),
-      ),
-    ),
-    'ding_availability_holdings' => array(
-      'field' => array(
-        'type' => 'ding_availability_holdings',
-        'locked' => TRUE,
-        'storage' => array(
-          'type' => 'virtual_field',
-        ),
-      ),
-      'instance' => array(
-        'label' => t('Holdings information'),
-      ),
-    ),
-  );
-}
-
-/**
  * Add buttons to an ding entity.
  *
  * Return an array of buttons to add.
