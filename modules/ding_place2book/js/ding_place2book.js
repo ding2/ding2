b5cda22a479039384831f49de388dbd692f50830
Voykov Alexandr <av@inlead.dk>
2016-04-07 12:08:11 +0300

    Issue #1404 by Inlead: Get info via XHR.

---
+++ modules/ding_place2book/js/ding_place2book.js
@@ -0,0 +1,25 @@
+/**
+ * @file
+ *
+ * Provide event nodes/pages with ticket info from Place2book
+ */
+(function ($) {
+
+  Drupal.behaviors.ding_place2book = {
+    attach: function (context, settings) {
+      $('.place2book').once('place2book', function () {
+        var $this = $(this);
+        var event_id = $this.data('event-id');
+        var event_maker_id = $this.data('event-maker-id');
+        $.ajax({
+          url: Drupal.settings.basePath + 'ding/p2b/event_maker/' + event_maker_id + '/event/' + event_id,
+          type: 'GET',
+          success: function (data) {
+            $('.place2book').html(JSON.parse(data));
+          }
+        });
+      });
+    }
+  };
+
+})(jQuery);
