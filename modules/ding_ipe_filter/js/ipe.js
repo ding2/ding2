1225488ed723a702318c7321fb9059e8b9faf77b
Nicolae Grigoriu <ng@inlead.dk>
2016-05-26 00:24:18 +0300

    Issue #1599 by Inlead: Toggle hide show of the menu based on click event.

--- modules/ding_ipe_filter/js/ipe.js
+++ modules/ding_ipe_filter/js/ipe.js
@@ -1,21 +1,32 @@
 (function ($) {
   "use strict";
-
+  $('html').click(function() {
+    $('.ipe-popup').addClass('hide');
+    $('.ipe-popup').removeClass('top');
+  });
   $(document).on('click', '.ipe-trigger', function() {
-    var links = '#ipe-add-' + $(this).attr('target_region');
-    var link_offset = Math.abs(parseInt(screen.width) - parseInt($(this).parent().offset().left));
-    var direction = (link_offset > 800) ? 'left' : 'right';
-    var options = {
-      'my': direction + ' top',
-      'at': 'left bottom',
-      'of': $(this),
-      'offset': '0 3'
-    };
-    $(links).fadeToggle('fast').position(options);
+    // Hide all elements before open other.
+    $('.ipe-popup').addClass('hide');
+    $('.ipe-popup').removeClass('top');
+
+    var menu = '#ipe-add-' + $(this).attr('target_region');
+    // Move menu from the left if it is too close to the border.
+    if (parseInt($(this).parent().offset().left) < 300) {
+      $(menu).toggleClass('left');
+    }
+    // Move menu on top if it is too close to bottom of the page.
+    if (parseInt($(this).parent().offset().top) - parseInt(screen.height) > 1400) {
+      $(menu).toggleClass('top');
+    }
+    // Display element on click.
+    $(menu).toggleClass('hide');
+
     return false;
   });
-
+  
   $(document).on('mouseleave', '.ipe-popup', function() {
-    $(this).fadeToggle('fast');
+    $(this).toggleClass('hide');
+    $(this).removeClass('left');
+    $(this).removeClass('top');
   });
 })(jQuery);
