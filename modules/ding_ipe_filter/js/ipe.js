4ec64e4c90f5e31ff4c7edc2a3e4b8722b7e35f1
Nicolae Grigoriu <ng@inlead.dk>
2016-05-30 11:07:29 +0300

    Issue #1599 by Inlead: Refactor popup menu.

--- modules/ding_ipe_filter/js/ipe.js
+++ modules/ding_ipe_filter/js/ipe.js
@@ -7,17 +7,18 @@
   });
   $(document).on('click', '.ipe-trigger', function() {
     // Hide all elements before open other.
+    $(this).addClass('active');
     $('.ipe-popup').addClass('hide');
     $('.ipe-popup').removeClass('top');
     $('.ipe-popup').removeClass('left');
 
     var menu = '#ipe-add-' + $(this).attr('target_region');
     // Move menu from the left if it is too close to the border.
-    if (parseInt($(this).parent().offset().left) < 800) {
+    if (parseInt($(this).parent().offset().left) < 700) {
       $(menu).toggleClass('left');
     }
     // Move menu on top if it is too close to bottom of the page.
-    if (parseInt($(this).parent().offset().top) > 1800) {
+    if (parseInt($(this).parent().offset().top) > 2200) {
       $(menu).toggleClass('top');
     }
     // Display element on click.
@@ -30,5 +31,6 @@
     $(this).toggleClass('hide');
     $(this).removeClass('left');
     $(this).removeClass('top');
+    $('.ipe-trigger').removeClass('active');
   });
 })(jQuery);
