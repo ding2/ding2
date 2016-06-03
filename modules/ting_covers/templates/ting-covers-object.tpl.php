cbf79fb5ab30f64b61503d7ffd8eb4471c92aeec
Anna Caraman <ac@inleadmedia.com>
2016-06-03 11:46:30 +0300

    EASYOPAC-94 - Covers pop-up styles and fixes.

--- modules/ting_covers/templates/ting-covers-object.tpl.php
+++ modules/ting_covers/templates/ting-covers-object.tpl.php
@@ -1,6 +1,9 @@
 <?php $close_button = drupal_render($elements['#close_button']); ?>
 
-<div id="work-cover-<?php print $elements['#id'] ?>" class="work-cover"<?php print $elements['#no_image_style'] ?>>
+<div id="work-cover-<?php print $elements['#id'] ?>" class="work-cover"<?php 
+  if (isset($elements['#no_image_style']))
+  print $elements['#no_image_style'] 
+?>>
   <div class="work-cover-image">
     <div class="<?php print( implode(' ', $elements['#classes'])); ?>">
       <?php print drupal_render($elements['#image']) ?>
