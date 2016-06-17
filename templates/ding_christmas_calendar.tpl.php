e5feddfb02bb085cad8129ca55eb9b9148247d1b
Voykov Alexandr <av@inlead.dk>
2016-06-17 13:59:07 +0300

    DDBCMS-105 - Changes and improvments.

--- templates/ding_christmas_calendar.tpl.php
+++ templates/ding_christmas_calendar.tpl.php
@@ -5,7 +5,12 @@
  */
 $a = 1;
 ?>
-
+<div class="calendar-summary">
+  <?php print $summary; ?>
+</div>
+<div class="calendar-body">
+  <?php print $body; ?>
+</div>
 <div id="ding-christmas-calendar-content" style="background-image: url(<?php print $background;?>)">
   <?php print $table ?>
 </div>
