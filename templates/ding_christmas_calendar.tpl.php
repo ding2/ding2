54331cee7c14c8f2c36be3f379e202958c7b762d
Voykov Alexandr <av@inlead.dk>
2016-06-17 09:37:07 +0300

    DDBCMS-105 - Main functionality.

---
+++ templates/ding_christmas_calendar.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * Represents a template for displaying Christmas calendar.
+ */
+$a = 1;
+?>
+
+<div id="ding-christmas-calendar-content" style="background-image: url(<?php print $background;?>)">
+  <?php print $table ?>
+</div>
