baad7f0bb56d02b069c16c7966e383c2e54a25c0
Simon Holt <simonholt83@gmail.com>
2016-06-21 16:12:20 +0200

    Fix check for dscMultitrack

--- js/ting_field_search.webtrends.js
+++ js/ting_field_search.webtrends.js
@@ -3,16 +3,15 @@
 
   Drupal.behaviors.tingFieldSearchWebtrends = {
     attach: function(context, settings) {
-      if (typeof dcsMultiTrack === "undefined") {
-        return;
-      }
 
       // Track when a profile is selected if Webtrends is selected.
       $("#edit-ting-field-search", context).change(function() {
-        var profile_name = $(this).children(":selected").val();
-        var uri = "/ting-field-search/profile-selected/" + profile_name;
+        if (typeof dcsMultiTrack !== "undefined") {
+          var profile_name = $(this).children(":selected").val();
+          var uri = "/ting-field-search/profile-selected/" + profile_name;
 
-        dcsMultiTrack("WT.ti", profile_name, "DCS.uri", uri);
+          dcsMultiTrack("WT.ti", profile_name, "DCS.uri", uri);
+        }
       });
     }
   };
