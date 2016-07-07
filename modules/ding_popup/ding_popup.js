19af2b9c1097fdd8ec47259152049dbf9770a3e5
Thomas Fini Hansen <xen@xen.dk>
2016-07-07 10:36:00 +0200

    Fix code style

--- modules/ding_popup/ding_popup.js
+++ modules/ding_popup/ding_popup.js
@@ -1,39 +1,40 @@
 
 (function ($) {
 
-/**
- * Add a keypress handler on text and password fields that catches
- * return and submits the form by triggering the first submit
- * button. Otherwise the browser standard handler is used, and it
- * doesn't post by AJAX.
- *
- * @todo possibly support more input types.
- */
-Drupal.behaviors.ding_popup_form_submit = {
-  attach: function (context, settings) {
-    $('.ui-dialog-content input[type=text]:not(.ding-popup-processed), .ui-dialog-content input[type=password]:not(.ding-popup-processed)').addClass('ding-popup-processed').each(function () {
-      $(this).keypress(function (event) {
-        if (event.which == 13) {
-          $($(this.form).find('input[type=submit]').get(0)).trigger('mousedown');
-          return false;
-        }
+  /**
+   * Add keypress handler.
+   *
+   * This catches return and submits the form by triggering the first
+   * submit button. Otherwise the browser standard handler is used,
+   * and it doesn't post by AJAX.
+   *
+   * @todo possibly support more input types.
+   */
+  Drupal.behaviors.ding_popup_form_submit = {
+    attach: function (context, settings) {
+      $('.ui-dialog-content input[type=text]:not(.ding-popup-processed), .ui-dialog-content input[type=password]:not(.ding-popup-processed)').addClass('ding-popup-processed').each(function () {
+        $(this).keypress(function (event) {
+          if (event.which == 13) {
+            $($(this.form).find('input[type=submit]').get(0)).trigger('mousedown');
+            return false;
+          }
+        });
       });
-    });
-  }
-};
+    }
+  };
 
-/**
- * Object to handle popups.
- */
-Drupal.ding_popup = {
-  refresh: false,
-  states: {},
-  dialogs: {},
+  /**
+   * Object to handle popups.
+   */
+  Drupal.ding_popup = {
+    refresh: false,
+    states: {},
+    dialogs: {},
 
-  setState: function (response) {
-    var self = this;
-    if (this.dialogs[response.name] == undefined) {
-      this.dialogs[response.name] = $('<div class="ding-popup-content"></div>').dialog({
+    setState: function (response) {
+      var self = this;
+      if (this.dialogs[response.name] == undefined) {
+        this.dialogs[response.name] = $('<div class="ding-popup-content"></div>').dialog({
           autoOpen: false,
           modal: true,
           draggable: false,
@@ -54,67 +55,66 @@ Drupal.ding_popup = {
             }
 
           }
-      });
-    }
-    // Pass dialog options on the the actual dialog.
-    // We could check the options for validity, but for now we just
-    // pass everything on to the dialog.
-    if (response.extra_data.dialog_options) {
-      this.dialogs[response.name].dialog('option', response.extra_data.dialog_options);
-    }
-    this.dialogs[response.name].dialog('option', {'title': response.title});
-    this.dialogs[response.name].html(response.data);
-
-    Drupal.attachBehaviors(this.dialogs[response.name]);
-    this.dialogs[response.name].dialog('open');
-  },
-
-  open: function(response) {
-    if (this.states[response.name] == undefined) {
-      this.states[response.name] = [];
-    }
-    if (response['resubmit']) {
-      this.states[response.name].push(response);
-    }
-    this.setState(response);
-  },
-
-  close: function(response) {
-    while (this.states[response.name].length > 0) {
-      var state = this.states[response.name].pop();
-      Drupal.detachBehaviors(this.dialog);
-
-      // User login have been preformed, so page need to be reloaded.
-      if (state.name === 'ding_user') {
-        this.refresh = true;
+        });
       }
+      // Pass dialog options on the the actual dialog.
+      // We could check the options for validity, but for now we just
+      // pass everything on to the dialog.
+      if (response.extra_data.dialog_options) {
+        this.dialogs[response.name].dialog('option', response.extra_data.dialog_options);
+      }
+      this.dialogs[response.name].dialog('option', {'title': response.title});
+      this.dialogs[response.name].html(response.data);
 
-      // Add in extra post vars.
-      $.extend(state['orig_ajax'].options.data, state['extra_data']);
-      // Call original ajax callback.
-      state['orig_ajax'].eventResponse(state['orig_ajax'], null);
+      Drupal.attachBehaviors(this.dialogs[response.name]);
+      this.dialogs[response.name].dialog('open');
+    },
+
+    open: function(response) {
+      if (this.states[response.name] == undefined) {
+        this.states[response.name] = [];
+      }
+      if (response['resubmit']) {
+        this.states[response.name].push(response);
+      }
+      this.setState(response);
+    },
+
+    close: function(response) {
+      while (this.states[response.name].length > 0) {
+        var state = this.states[response.name].pop();
+        Drupal.detachBehaviors(this.dialog);
+
+        // User login have been preformed, so page need to be reloaded.
+        if (state.name === 'ding_user') {
+          this.refresh = true;
+        }
+
+        // Add in extra post vars.
+        $.extend(state['orig_ajax'].options.data, state['extra_data']);
+        // Call original ajax callback.
+        state['orig_ajax'].eventResponse(state['orig_ajax'], null);
+      }
+      if (this.dialogs[response.name].refresh_on_close) {
+        alert('refresh');
+      }
+      this.dialogs[response.name].dialog('close');
     }
-    if (this.dialogs[response.name].refresh_on_close) {
-      alert('refresh');
-    }
-    this.dialogs[response.name].dialog('close');
-  }
-};
+  };
 
-/**
- * Command to create a popup.
- */
-Drupal.ajax.prototype.commands['ding_popup'] = function (ajax, response, status) {
-  response['orig_ajax'] = ajax;
-  Drupal.ding_popup.open(response);
-};
+  /**
+   * Command to create a popup.
+   */
+  Drupal.ajax.prototype.commands['ding_popup'] = function (ajax, response, status) {
+    response['orig_ajax'] = ajax;
+    Drupal.ding_popup.open(response);
+  };
 
-/**
- * Command to close a popup.
- */
-Drupal.ajax.prototype.commands['ding_popup_close'] = function (ajax, response, status) {
-  Drupal.ding_popup.close(response);
-};
-
+  /**
+   * Command to close a popup.
+   */
+  Drupal.ajax.prototype.commands['ding_popup_close'] = function (ajax, response, status) {
+    Drupal.ding_popup.close(response);
+  };
 
 })(jQuery);
