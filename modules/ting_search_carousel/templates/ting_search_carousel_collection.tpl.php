921abab2c008ff44cdec6c37599c85082edf7ea7
Thomas Fini Hansen <xen@xen.dk>
2016-08-24 18:18:13 +0200

    1770: Clean up carousel cover theming

--- modules/ting_search_carousel/templates/ting_search_carousel_collection.tpl.php
+++ modules/ting_search_carousel/templates/ting_search_carousel_collection.tpl.php
@@ -1,10 +1,17 @@
 <?php
 /**
  * @file
+ * Default carousel item template.
  *
+ * Available variables:
+ * - $collection: Legacy item object, do not use.
+ * - $title: Title of item (sanitized).
+ * - $creator: Author of item (sanitized).
+ * - $image: Render array for rendering the cover.
+ * - $path: Path to the collection/material.
  */
 ?>
 <li class="carousel-item">
-  <a href="/ting/collection/<?php print $collection->id; ?>" class="carousel-item-image"><img src="<?php print $collection->image; ?>" alt=""/></a>
-  <a href="/ting/collection/<?php print $collection->id; ?>" class="carousel-item-title"><?php print check_plain($collection->title); ?></a>
+  <a href="<?php print $path; ?>" class="carousel-item-image"><?php print render($image); ?></a>
+  <a href="<?php print $path; ?>" class="carousel-item-title"><?php print $title; ?></a>
 </li>
