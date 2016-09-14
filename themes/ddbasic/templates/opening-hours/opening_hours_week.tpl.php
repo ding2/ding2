367c3bd4774128a7fbc2873d93f1c53318556001
Martin Dahl <martin@b14.dk>
2016-09-14 11:56:23 +0200

    1685: Opening hours buttons

--- themes/ddbasic/templates/opening-hours/opening_hours_week.tpl.php
+++ themes/ddbasic/templates/opening-hours/opening_hours_week.tpl.php
@@ -1,12 +1,10 @@
-<?php
-
-// We need to include the preface, or the openeing_hours module will trigger an
-// error in JavaScript.
-if (!empty($preface)):
-  print $preface;
-endif;
-?>
 <div class="opening-hours-week">
+  <?php
+  // We need to include the preface, or the openeing_hours module will trigger an
+  // error in JavaScript.
+  if (!empty($preface)):
+    // print $preface;
+  endif;
+  ?>
   <?php print render($table); ?>
 </div>
-
