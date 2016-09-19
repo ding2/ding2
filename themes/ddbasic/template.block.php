cb67574704934cc3d7b3d67bc023d3af4ef8f535
Kristian Rømer Pedersen <kristian@s003b.b14cms.dk>
2016-09-19 12:29:04 +0200

    Info-text added to ding_user_loan_number block if it's empty

--- themes/ddbasic/template.block.php
+++ themes/ddbasic/template.block.php
@@ -1,19 +1,19 @@
 <?php
-  
+
 function ddbasic_block_view_alter(&$data, $block) {
-  
+
   switch ($block->module . '__' . $block->delta) {
     case 'ting__ting_object_types':
       $object = menu_get_object('ting_object', 2);
-      
+
       if ($object && $collection = ting_collection_load($object->id)) {
         $items = array();
-        
+
         foreach ($collection->entities as $entity) {
           $item = ting_object_view($entity, 'teaser');
           $items[] = render($item);
         }
-        
+
         // Only display block if there are more than on item.
         if (count($items) > 1) {
           $data->content = array(
@@ -23,6 +23,11 @@ function ddbasic_block_view_alter(&$data, $block) {
         } else {
           $data = FALSE;
         }
+      }
+      break;
+    case 'ding_user_frontend__ding-user-loan-number':
+      if (empty($data['content'])) {
+        $data['content'] = t('You do not currently have any loans, reservations or debts.');
       }
       break;
   }
