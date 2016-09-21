3851b7b1eae0ea557720f892577b23920162230a
Kristian Rømer Pedersen <kristian@s003b.b14cms.dk>
2016-09-21 17:27:02 +0200

    1685: Styling openening hours week.

--- themes/ddbasic/template.opening_hours.php
+++ themes/ddbasic/template.opening_hours.php
@@ -5,27 +5,27 @@
  */
 function ddbasic_preprocess_ding_ddbasic_all_opening_hours(&$variables) {
   $today = strtotime('today');
-  
+
   if (!empty($variables['today'])) {
     $today = $variables['today'];
   }
-  
+
   $structured = array();
   $categories = array();
-    
+
   // TODO Get rid of the magical nodequeue number.
   $order = array();
   foreach (nodequeue_load_nodes(1, FALSE, 0, FALSE) as $node) {
     $order[$node->title] = $node->nid;
   }
-  
+
   // Get and sort all the instances in the given timespan.
   $instances = opening_hours_instance_load_multiple(
     array_values($order),
     date('Y-m-d', $today),
     date('Y-m-d', $today)
   );
-  
+
   // Fill in closed instances.
   foreach ($order as $library_nid) {
     $closed = TRUE;
@@ -34,7 +34,7 @@ function ddbasic_preprocess_ding_ddbasic_all_opening_hours(&$variables) {
         $closed = FALSE;
       }
     }
-    
+
     if ($closed === TRUE) {
       $instances[] = (object) array(
         'instance_id' => -1,
@@ -48,15 +48,15 @@ function ddbasic_preprocess_ding_ddbasic_all_opening_hours(&$variables) {
       );
     }
   }
-  
+
   usort($instances, '_ddbasic_opening_hours_sort');
-  
+
   $order = array_flip($order);
-  
+
   foreach ($instances as $instance) {
     $category_weight = _ddbasic_opening_hours_get_category_weight($instance->category_tid);
     $library = $order[$instance->nid];
-    
+
     if (!isset($structured[$library])) {
       $structured[$library] = array(
         'cols' => array(),
@@ -70,7 +70,7 @@ function ddbasic_preprocess_ding_ddbasic_all_opening_hours(&$variables) {
     } else {
       $notice = '';
     }
-    
+
     if (empty($instance->closed)) {
       $structured[$library]['cols'][$category_weight] = t('@from - @to', array(
         '@from' => $instance->start_time,
@@ -80,15 +80,15 @@ function ddbasic_preprocess_ding_ddbasic_all_opening_hours(&$variables) {
     else {
       $structured[$library]['cols'][$category_weight] = $notice;
     }
-    
+
     if (!isset($categories[$category_weight])) {
       $categories[$category_weight] = _ddbasic_opening_hours_get_category_name($instance->category_tid);
     }
   }
-  
+
   // Order the structure by the nodequeue order.
   $structured = array_merge(array_intersect_key(array_flip($order), $structured), $structured);
-  
+
   $variables['table'] = _ddbasic_opening_hours_table(
     t('Today: @date', array('@date' => format_date(time(), 'ding_date_only'))),
     $categories,
@@ -107,7 +107,7 @@ function ddbasic_preprocess_opening_hours_week(&$variables) {
   $today = strtotime('today');
   $structured = array();
   $categories = array();
-  
+
   if (isset($GLOBALS['ddbasic_opening_hours_week_timespan'])) {
     $timespan = $GLOBALS['ddbasic_opening_hours_week_timespan'];
   }
@@ -118,7 +118,7 @@ function ddbasic_preprocess_opening_hours_week(&$variables) {
     date('Y-m-d', $timespan[0]),
     date('Y-m-d', $timespan[1])
   );
-  
+
   // Add closed days.
   for ($day = $timespan[0]; $day <= $timespan[1]; $day += 86400) {
     $date = date('Y-m-d', $day);
@@ -128,7 +128,7 @@ function ddbasic_preprocess_opening_hours_week(&$variables) {
         $closed = FALSE;
       }
     }
-    
+
     if ($closed === TRUE) {
       $instances[] = (object) array(
         'instance_id' => -1,
@@ -142,9 +142,9 @@ function ddbasic_preprocess_opening_hours_week(&$variables) {
       );
     }
   }
-  
+
   usort($instances, '_ddbasic_opening_hours_sort');
-  
+
   // Insert the instances into the structured array.
   foreach ($instances as $instance) {
     $category_weight = _ddbasic_opening_hours_get_category_weight($instance->category_tid);
@@ -156,18 +156,18 @@ function ddbasic_preprocess_opening_hours_week(&$variables) {
         'cols' => array(),
         'extra' => array(),
       );
-      
+
       if ($time == $today) {
         $structured[$day]['extra']['class'] = array('today');
       }
     }
-    
+
     if(!empty($instance->notice)) {
       $notice = '<span class="notice">' . $instance->notice . '</span>';
     } else {
       $notice = '';
     }
-    
+
     if (empty($instance->closed)) {
       $structured[$day]['cols'][$category_weight] = t('@from - @to', array(
         '@from' => $instance->start_time,
@@ -185,7 +185,7 @@ function ddbasic_preprocess_opening_hours_week(&$variables) {
 
   // Sort the categories by key (weight).
   ksort($categories);
-  
+
   $variables['table'] = _ddbasic_opening_hours_table(
     // TODO Use preset date types from admin/config/regional/date-time instead
     // of the custom format, so it can be customized in the interface.
@@ -197,23 +197,23 @@ function ddbasic_preprocess_opening_hours_week(&$variables) {
     $categories,
     $structured
   );
-  
+
   drupal_add_library('system', 'drupal.ajax');
-  $variables['table']['#prefix'] = l(
-    'Previous',
-    'ding-ddbasic/opening-hours/week/' . $variables['node']->nid
-    . '/' . strtotime('last monday', $timespan[0])
-    . '/' . strtotime('last sunday', $timespan[0]),
-    array(
-      'attributes' => array('class' => array('use-ajax')),
-    )
-  ) . l(
+  $variables['table']['#suffix'] = l(
     'Next',
     'ding-ddbasic/opening-hours/week/' . $variables['node']->nid
     . '/' . strtotime('next monday', $timespan[1])
     . '/' . strtotime('next sunday', $timespan[1]),
     array(
-      'attributes' => array('class' => array('use-ajax')),
+      'attributes' => array('class' => array('use-ajax', 'button-next')),
+    )
+  ) . l(
+    'Previous',
+    'ding-ddbasic/opening-hours/week/' . $variables['node']->nid
+    . '/' . strtotime('last monday', $timespan[0])
+    . '/' . strtotime('last sunday', $timespan[0]),
+    array(
+      'attributes' => array('class' => array('use-ajax', 'button-previous')),
     )
   );
 }
@@ -231,7 +231,7 @@ function _ddbasic_opening_hours_table($title, $categories, $structured) {
     // header sticky offset (see the tableheader.js in root/misc).
     '#sticky' => FALSE,
   );
-  
+
   foreach ($structured as $label => $row) {
     if (!isset($row['name'])) {
       $cols = array($label);
