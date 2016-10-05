196778187094d5f762da4cf3287fc9e8c2fd5d30
Kristian Rømer Pedersen <kristian@s003b.b14cms.dk>
2016-10-05 16:00:08 +0200

    1685: Implementation and styling of ding-faq-question

--- themes/ddbasic/templates/node/node--ding-faq-question.tpl.php
+++ themes/ddbasic/templates/node/node--ding-faq-question.tpl.php
@@ -1,18 +1,14 @@
 <article class="<?php print $classes; ?> node-full"<?php print $attributes; ?>>
   <div class="inner">
-  	<div class="left">
+    <div class="left">
   		<?php print render($content['group_left']); ?>
   	</div>
   	<div class="right">
   		<h1><?php print $title; ?></h1>
-  		<?php print render($content['group_right']['field_ding_eresource_category']); ?>
-  		<div class="section meta">
-  			<?php print render($content['group_right']['field_ding_eresource_access']); ?>
-  		</div>
   		<?php print render($content['group_right']); ?>
   	</div>
   	<div class="buttons">
-    	<?php print l(t('E-resources'), 'e-materialer'); ?>
+    	<?php print l(t('FAQ'), 'faq'); ?>
   	</div>
   </div>
 </article>
