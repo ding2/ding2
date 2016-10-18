49eb4e8cf25e69f3314172ed912109d1e8541cbb
Kristian Rømer Pedersen <kristian@s003b.b14cms.dk>
2016-10-18 15:56:58 +0200

    1685: Fixing TODOs from core team.

--- themes/ddbasic/templates/opening-hours/opening_hours_week.tpl.php
+++ themes/ddbasic/templates/opening-hours/opening_hours_week.tpl.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <div class="<?php print $classes; ?>">
   <?php
   // We need to include the preface, or the openeing_hours module will trigger an
@@ -6,5 +7,17 @@
     // print $preface;
   endif;
   ?>
+=======
+<?php
+/**
+ * @file
+ * Template for rendering opening hours week.
+ *
+ * ddbasic specific variables:
+ * - $table: Table of opening hours
+ */
+?>
+<div class="opening-hours-week">
+>>>>>>> Fixing TODO's from core team. Commit adds file descriptions, variable lists and function descriptions. Deletes unused files and code.
   <?php print render($table); ?>
 </div>
