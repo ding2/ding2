167f3276bdf49fed72b6d08f68b41244aa7234a3
CFE <CFE@1284514.no-reply.drupal.org>
2016-11-02 12:16:47 +0100

    1685: Fix for opening hours

--- themes/ddbasic/templates/opening-hours/opening_hours_week.tpl.php
+++ themes/ddbasic/templates/opening-hours/opening_hours_week.tpl.php
@@ -1,23 +1,12 @@
-<<<<<<< HEAD
-<div class="<?php print $classes; ?>">
-  <?php
-  // We need to include the preface, or the openeing_hours module will trigger an
-  // error in JavaScript.
-  if (!empty($preface)):
-    // print $preface;
-  endif;
-  ?>
-=======
 <?php
 /**
  * @file
  * Template for rendering opening hours week.
  *
- * ddbasic specific variables:
- * - $table: Table of opening hours
+ * Ddbasic specific variables:
+ * - $table: Table of opening hours.
  */
 ?>
-<div class="opening-hours-week">
->>>>>>> Fixing TODO's from core team. Commit adds file descriptions, variable lists and function descriptions. Deletes unused files and code.
+<div class="<?php print $classes; ?>">
   <?php print render($table); ?>
 </div>
