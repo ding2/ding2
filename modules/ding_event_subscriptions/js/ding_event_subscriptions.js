cda71f3709e97a4f3c01b3efa391bf540441daf7
av <av@inlead.dk>
2016-11-04 11:56:08 +0200

    EASYOPAC-317 - CR fixes.

--- modules/ding_event_subscriptions/js/ding_event_subscriptions.js
+++ modules/ding_event_subscriptions/js/ding_event_subscriptions.js
@@ -1,26 +1,29 @@
 (function ($) {
-  "use strict";
+  'use strict';
   function copyToClipboard(element) {
     var $temp = $("<input>");
-    $("body").append($temp);
+    $('body').append($temp);
     var url = $(element).data('domain') + '/' + $(element).data('reference');
     $temp.val(url).select();
-    document.execCommand("copy");
+    document.execCommand('copy');
     $temp.remove();
   }
 
-  $(document).ready(function () {
-    $('.ding-event-subscriptions').on('click', function (e) {
-      e.preventDefault();
-      window.location.href = $(this).data('reference');
-      copyToClipboard($(this));
-      $(this).tooltip({
-        items: ".ding-event-subscriptions",
-        content: Drupal.t("Link was copied to clipboard")
+  Drupal.behaviors.exampleModule = {
+    attach: function (context, settings) {
+      $('.ding-event-subscriptions', context).on('click', function (e) {
+        e.preventDefault();
+        window.location.href = $(this).data('reference');
+        copyToClipboard($(this));
+        $(this).tooltip({
+          items: '.ding-event-subscriptions',
+          content: Drupal.t('Link was copied to clipboard')
+        });
+        $(this).tooltip('open');
+      }).on('mouseover', function () {
+        $(this).tooltip('disable');
       });
-      $(this).tooltip("open");
-    }).on('mouseover', function () {
-      $(this).tooltip("disable");
-    });
-  });
+    }
+  };
+
 })(jQuery);
