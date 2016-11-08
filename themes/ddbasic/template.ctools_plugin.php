9065b88d7d4f55c50c0113f8bbbee60de92ba290
Kristian Rømer Pedersen <kristian@s003b.b14cms.dk>
2016-11-08 15:01:21 +0100

    Add fax-number to contact panel for libraries. Styling of fax-number and opening hours on library

--- themes/ddbasic/template.ctools_plugin.php
+++ themes/ddbasic/template.ctools_plugin.php
@@ -8,6 +8,9 @@ function ddbasic_ctools_plugin_pre_alter(&$plugin, &$info) {
     case 'ting_search search_result_count':
       $plugin['render callback'] = 'ddbasic_ctools_plugin__ting_search_result_count_content_type_render';
       break;
+    case 'ding_library contact':
+      $plugin['render callback'] = 'ddbasic_ctools_plugin__ding_library_contact_content_type_render';
+      break;
   }
 }
 
@@ -42,6 +45,45 @@ function ddbasic_ctools_plugin__ting_search_result_count_content_type_render($su
       )
     );
   }
+
+  return $block;
+}
+
+function ddbasic_ctools_plugin__ding_library_contact_content_type_render($subtype, $conf, $panel_args, $context = NULL) {
+  $block = new stdClass();
+  if (empty($context->data)) {
+    return $block;
+  }
+  $node = $context->data;
+
+  $content = ' <div class="library-contact">';
+  if (!empty($node->field_ding_library_phone_number['und'])) {
+    $content .= '
+      <div class="library-contact-phone">
+        <span class="library-contact-phone-label">' . t('Phone:') . '</span>
+        <span class="library-contact-phone">' . $node->field_ding_library_phone_number['und'][0]['value'] . '</span>
+      </div>';
+  }
+  if (!empty($node->field_ding_library_fax_number['und'])) {
+    $content .= '
+        <div class="library-contact-fax">
+          <span class="library-contact-fax-label">' . t('Fax:') . '</span>
+          <span class="library-contact-fax">' . $node->field_ding_library_fax_number['und'][0]['value'] . '</span>
+        </div>
+    ';
+  }
+  if (!empty($node->field_ding_library_mail['und'])) {
+    $content .= '
+        <div class="library-contact-email">
+          <span class="library-contact-email-label">' . t('Email:') . '</span>
+          <span class="library-contact-email">' . l($node->field_ding_library_mail['und'][0]['email'], 'mailto:' . $node->field_ding_library_mail['und'][0]['email']) . '</span>
+        </div>
+    ';
+  }
+  $content .= '</div>';
+
+  $block->title = t('Contact');
+  $block->content = $content;
 
   return $block;
 }
