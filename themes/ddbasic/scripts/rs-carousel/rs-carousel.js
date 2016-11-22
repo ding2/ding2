c8740cc6d5a61e91ead0fa2351072676556e5037
Kristian Rømer Pedersen <kristian@s003b.b14cms.dk>
2016-11-22 12:26:31 +0100

    1685: Styling for new version of rs-carousel module.

---
+++ themes/ddbasic/scripts/rs-carousel/rs-carousel.js
@@ -0,0 +1,10 @@
+(function($) {
+
+  Drupal.behaviors.ding_carousel = {
+    attach: function(context, settings) {
+      // Wrap select list
+      $( "select.rs-carousel-select-tabs" ).wrap( "<div class='form-type-select rs-carousel-select'><div class='select-wrapper'></div></div>" );
+    }
+  };
+
+})(jQuery);
