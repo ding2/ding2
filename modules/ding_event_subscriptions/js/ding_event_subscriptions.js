967ecbca6ece3bc4c3a4bc271cedca0ee4787036
Mihaela Stavila <ms@inleadmedia.dk>
2016-12-16 12:01:17 +0200

    EASYOPAC-386 - Add icons for subsribe events.

--- modules/ding_event_subscriptions/js/ding_event_subscriptions.js
+++ modules/ding_event_subscriptions/js/ding_event_subscriptions.js
@@ -15,15 +15,16 @@
       $('.ding-event-subscriptions', context).on('click', function (e) {
         e.preventDefault();
         window.location.href = $(this).data('reference');
-        copyToClipboard($(this));
-        $(this).tooltip({
-          items: '.ding-event-subscriptions',
-          content: Drupal.t('Link was copied to clipboard')
-        });
-        $(this).tooltip('open');
-      }).on('mouseover', function () {
-        $(this).tooltip('disable');
-      });
+        // copyToClipboard($(this));
+        // $(this).tooltip({
+        //   items: '.ding-event-subscriptions',
+        //   content: Drupal.t('Link was copied to clipboard')
+        // });
+        // $(this).tooltip('open');
+      })
+      // .on('mouseover', function () {
+      //   $(this).tooltip('disable');
+      // });
     }
   };
 
