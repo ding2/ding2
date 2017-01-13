d45f742d339c64b150d42dd3193b7a6ac816849e
Thomas Fini Hansen <xen@xen.dk>
2017-01-13 10:38:03 +0100

    2118: Removed dev dependencies from FBS

--- modules/fbs/vendor/composer/autoload_real.php
+++ modules/fbs/vendor/composer/autoload_real.php
@@ -23,37 +23,30 @@ class ComposerAutoloaderInit02d64f0805d91972c8cefd85d0c38c31
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit02d64f0805d91972c8cefd85d0c38c31', 'loadClassLoader'));
 
-        $includePaths = require __DIR__ . '/include_paths.php';
-        array_push($includePaths, get_include_path());
-        set_include_path(join(PATH_SEPARATOR, $includePaths));
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        if ($useStaticLoader) {
+            require_once __DIR__ . '/autoload_static.php';
 
-        $map = require __DIR__ . '/autoload_namespaces.php';
-        foreach ($map as $namespace => $path) {
-            $loader->set($namespace, $path);
-        }
+            call_user_func(\Composer\Autoload\ComposerStaticInit02d64f0805d91972c8cefd85d0c38c31::getInitializer($loader));
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
 
-        $map = require __DIR__ . '/autoload_psr4.php';
-        foreach ($map as $namespace => $path) {
-            $loader->setPsr4($namespace, $path);
-        }
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
 
-        $classMap = require __DIR__ . '/autoload_classmap.php';
-        if ($classMap) {
-            $loader->addClassMap($classMap);
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
         }
 
         $loader->register(true);
 
-        $includeFiles = require __DIR__ . '/autoload_files.php';
-        foreach ($includeFiles as $file) {
-            composerRequire02d64f0805d91972c8cefd85d0c38c31($file);
-        }
-
         return $loader;
     }
-}
-
-function composerRequire02d64f0805d91972c8cefd85d0c38c31($file)
-{
-    require $file;
 }
