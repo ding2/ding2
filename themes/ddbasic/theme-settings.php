78e7f1723e2742e36e2f24aa2c7b935eea15c199
CFE <CFE@1284514.no-reply.drupal.org>
2017-03-28 13:19:46 +0200

    2226: Fixed color module errors

--- themes/ddbasic/theme-settings.php
+++ themes/ddbasic/theme-settings.php
@@ -21,6 +21,7 @@ function ddbasic_form_system_theme_settings_alter(&$form, $form_state) {
   );
 
   $form['#validate'][] = 'ddbasic_form_system_theme_settings_validate';
+  $form['#submit'][] = 'ddbasic_form_system_theme_settings_submit';
 }
 
 /**
@@ -33,5 +34,21 @@ function ddbasic_form_system_theme_settings_validate($form, &$form_state) {
 
     case 'secondary': $form_state['values']['palette']['text'] = $form_state['values']['palette']['secondary'];
       break;
+  }
+}
+
+/**
+ * Custom submit for the theme_settings form.
+ */
+function ddbasic_form_system_theme_settings_submit($form, &$form_state) {
+  $theme_name = $form_state['build_info']['args'][0];
+  // Add all the css files to the color module info array.
+  $theme_path = drupal_realpath(drupal_get_path('theme', $theme_name)) . '/';
+  $cssfiles = array_merge(
+    file_scan_directory($theme_path . '/sass_css', '/\.css$/')
+  );
+  $form_state['values']['info']['css'] = array();
+  foreach ($cssfiles as $cssfile) {
+    $form_state['values']['info']['css'][] = str_replace($theme_path, '', drupal_realpath($cssfile->uri));
   }
 }
