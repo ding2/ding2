9cacd7c6b1d1c828c887770b73c30d7a370a2036
Thomas Fini Hansen <xen@xen.dk>
2017-04-11 15:18:30 +0200

    1458: Add custom message when not reservable

--- modules/ding_availability/templates/ding-holdings.tpl.php
+++ modules/ding_availability/templates/ding-holdings.tpl.php
@@ -1,16 +1,19 @@
 <?php
+
 /**
  * @file
- * Displays holdings information
+ * Displays holdings information.
  *
  * Available variables:
  *  - $holdings: Html showing detailed holdings information.
  *  - $total_count: Total amount of copies.
  *  - $reserved_count: Amount of reservations.
  *  - $ordered_count: Amount in acquisition.
- *  - $total_plus_ordered_count: Sum of Total + Amount in acquisition
+ *  - $total_plus_ordered_count: Sum of total + amount in acquisition.
+ *  - $reserved_text: Text description of amount of reservations.
+ *  - $acquisition_text: Text Description of amount in acquisition.
+ *  - $total_text: Text description of total + amount in acquisition.
  */
-
 ?>
 <p><?php print "$total_text $reserved_text $acquisition_text"; ?></p>
 
