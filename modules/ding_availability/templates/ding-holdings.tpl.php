008bb14b5072efbb7e7505edfd4028c791a8de80
Thomas Fini Hansen <xen@xen.dk>
2017-04-11 15:14:54 +0200

    1458: Move texts from template to preprocess

--- modules/ding_availability/templates/ding-holdings.tpl.php
+++ modules/ding_availability/templates/ding-holdings.tpl.php
@@ -11,12 +11,6 @@
  *  - $total_plus_ordered_count: Sum of Total + Amount in acquisition
  */
 
-$reserved_text = format_plural($reserved_count, 'There is 1 user in queue to loan the material.', 'There are @count users in queue to loan the material.');
-$acquisition_text = '';
-if ($ordered_count) {
-  $acquisition_text = format_plural($ordered_count, '1 copy in acquisition.', '@count copies are in acquisition.', array('@count' => $ordered_count));
-}
-$total_text = format_plural($total_plus_ordered_count, 'We have 1 copy.', 'We have @count copies.', array('@count' => $total_plus_ordered_count));
 ?>
 <p><?php print "$total_text $reserved_text $acquisition_text"; ?></p>
 
