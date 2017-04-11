4325651cd5f960b17a6bab0662f94cfa652b6cfe
Thomas Fini Hansen <xen@xen.dk>
2017-04-11 10:14:28 +0200

    1618: Clean up blacklisting code

--- modules/fbs/src/ExternalCatalogApi.php
+++ modules/fbs/src/ExternalCatalogApi.php
@@ -25,10 +25,6 @@ class ExternalCatalogApi extends SwaggerApi
         $request->addParameter("query", "recordid", $recordid);
         $request->addParameter("query", "exclude", $exclude);
 
-        // Filter out blacklisted branches.
-        $fbs_branches_blacklist = variable_get('fbs_branches_blacklist', array());
-        $request->addParameter("query", "exclude", array_keys($fbs_branches_blacklist));
-
         $request->defineResponse(200, "", array('\\FBS\\Model\\Availability'));
         $request->defineResponse("400", 'bad request', null);
         $request->defineResponse("401", 'client unauthorized', null);
@@ -91,10 +87,6 @@ class ExternalCatalogApi extends SwaggerApi
         $request->addParameter("query", "recordid", $recordid);
         $request->addParameter("query", "exclude", $exclude);
 
-        // Filter out blacklisted branches.
-        $fbs_branches_blacklist = variable_get('fbs_branches_blacklist', array());
-        $request->addParameter("query", "exclude", array_keys($fbs_branches_blacklist));
-
         $request->defineResponse(200, "", array('\\FBS\\Model\\HoldingsForBibliographicalRecord'));
         $request->defineResponse("400", 'bad request', null);
         $request->defineResponse("401", 'client unauthorized', null);
@@ -104,4 +96,3 @@ class ExternalCatalogApi extends SwaggerApi
 
 
 }
-
