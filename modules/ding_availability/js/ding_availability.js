a94792fe02e80706cc74a6ecbc6235b66bc55420
Thomas Fini Hansen <xen@xen.dk>
2017-04-20 14:02:49 +0200

    1460: Fix up codestyle

--- modules/ding_availability/js/ding_availability.js
+++ modules/ding_availability/js/ding_availability.js
@@ -1,23 +1,23 @@
 /**
- * @file ding.availability.js
+ * @file
  * JavaScript behaviours for fetching and displaying availability.
  */
 
-(function($) {
+(function ($) {
   "use strict";
 
   // Cache of fetched availability information.
   Drupal.DADB = {};
 
-  $(document).ready(function() {
+  $(document).ready(function () {
     var settings = Drupal.settings;
     var ids = [];
     var html_ids = [];
 
     // Extract entity ids and add them to the settings array.
     if (settings.hasOwnProperty('ding_availability')) {
-      $.each(settings.ding_availability, function(id, entity_ids) {
-        $.each(entity_ids, function(index, entity_id) {
+      $.each(settings.ding_availability, function (id, entity_ids) {
+        $.each(entity_ids, function (index, entity_id) {
           if (Drupal.DADB[entity_id] === undefined) {
             Drupal.DADB[entity_id] = null;
             ids.push(entity_id);
@@ -27,7 +27,7 @@
       });
     }
 
-    $.each(html_ids, function(index, id) {
+    $.each(html_ids, function (index, id) {
       $('#' + id).addClass('pending');
     });
 
@@ -38,13 +38,13 @@
       $.ajax({
         dataType: "json",
         url: path,
-        success: function(data) {
-          $.each(data, function(id, item) {
+        success: function (data) {
+          $.each(data, function (id, item) {
             // Update cache.
             Drupal.DADB[id] = item;
           });
 
-          $.each(settings.ding_availability, function(id, entity_ids) {
+          $.each(settings.ding_availability, function (id, entity_ids) {
             if (id.match(/^availability-/)) {
               // Update availability indicators.
               ding_availability_update_availability(id, entity_ids);
@@ -63,19 +63,18 @@
     else {
       // Apply already fetched availability, if any.
       if (settings.hasOwnProperty('ding_availability')) {
-        $.each(settings.ding_availability, function(id, entity_ids) {
+        $.each(settings.ding_availability, function (id, entity_ids) {
           ding_availability_update_availability(id, entity_ids);
         });
       }
     }
   });
 
-
   /**
    * Update availability information.
    *
    * Add classes to reservation texts and display reservation button based on
-   * availability information
+   * availability information.
    *
    * @param id
    *   HTML id of the availability to update.
@@ -89,8 +88,8 @@
     var element = $('#' + id);
     element.removeClass('pending').addClass('processed');
 
-    $.each(entity_ids, function(index, entity_id) {
-      // Reserve button
+    $.each(entity_ids, function (index, entity_id) {
+      // Reserve button.
       var reserve_button = element.parents('.ting-object:first, .material-item:first').find('a[id$=' + entity_id + '].reserve-button');
 
       if (Drupal.DADB[entity_id]) {
@@ -101,7 +100,7 @@
         if (available) {
           element.addClass('available');
 
-          // Add class to reserve button
+          // Add class to reserve button.
           if (reserve_button.length) {
             reserve_button.addClass('available');
           }
@@ -109,7 +108,7 @@
         else {
           element.addClass('unavailable');
 
-          // Add class to reserve button
+          // Add class to reserve button.
           if (reserve_button.length) {
             reserve_button.addClass('unavailable');
           }
@@ -118,7 +117,7 @@
         if (reservable) {
           element.addClass('reservable');
 
-          // Add class to reserve button
+          // Add class to reserve button.
           if (reserve_button.length) {
             reserve_button.addClass('reservable');
           }
@@ -126,7 +125,7 @@
         else {
           element.addClass('not-reservable');
 
-          // Add class to reserve button
+          // Add class to reserve button.
           if (reserve_button.length) {
             reserve_button.addClass('not-reservable');
           }
