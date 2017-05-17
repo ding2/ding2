2fc9d70fed117a148de6109ce18546e5dad6ed60
Nicolae Grigoriu <ng@inlead.dk>
2017-05-17 16:47:04 +0300

    1599 by Inlead: Use custom styles.

--- modules/ding_ipe_filter/js/ipe.js
+++ modules/ding_ipe_filter/js/ipe.js
@@ -1,16 +1,16 @@
 (function ($) {
   "use strict";
   $('html').click(function() {
-    $('.ipe-popup').addClass('hide');
-    $('.ipe-popup').removeClass('top');
-    $('.ipe-popup').removeClass('left');
+    $('.ipe-popup').addClass('element-hidden');
+    $('.ipe-popup').removeClass('ding-ipe-popup-top');
+    $('.ipe-popup').removeClass('ding-ipe-popup-left');
   });
   $(document).on('click', '.ipe-trigger', function() {
     // Hide all elements before open other.
     $(this).addClass('active');
-    $('.ipe-popup').addClass('hide');
-    $('.ipe-popup').removeClass('top');
-    $('.ipe-popup').removeClass('left');
+    $('.ipe-popup').addClass('element-hidden');
+    $('.ipe-popup').removeClass('ding-ipe-popup-top');
+    $('.ipe-popup').removeClass('ding-ipe-popup-left');
 
     var menu = '#ipe-add-' + $(this).attr('target_region');
     // Move menu from the left if it is too close to the border.
@@ -21,18 +21,18 @@
     var containerHeight = $('.panels-ipe-display-container').height();
     var menuPosition = $(this).parent().offset().top;
     if (parseInt(containerHeight - menuPosition) < 0) {
-      $(menu).toggleClass('top');
+      $(menu).toggleClass('ding-ipe-popup-top');
     }
     // Display element on click.
-    $(menu).toggleClass('hide');
+    $(menu).toggleClass('element-hidden');
 
     return false;
   });
-  
+
   $(document).on('mouseleave', '.ipe-popup', function() {
-    $(this).toggleClass('hide');
-    $(this).removeClass('left');
-    $(this).removeClass('top');
+    $(this).toggleClass('element-hidden');
+    $(this).removeClass('ding-ipe-popup-left');
+    $(this).removeClass('ding-ipe-popup-top');
     $('.ipe-trigger').removeClass('active');
   });
 })(jQuery);
