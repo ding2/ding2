f9fe965e92575b89bec1747b23d3a28b95b143da
Nicolae Grigoriu <ng@inlead.dk>
2017-06-19 14:20:30 +0300

    EASYOPAC-494 - Use php 5.4 array syntax.

--- modules/ting_covers_addi/lib/addi-client/AdditionalInformationService.php
+++ modules/ting_covers_addi/lib/addi-client/AdditionalInformationService.php
@@ -108,15 +108,15 @@ class AdditionalInformationService {
       // Assume that unusual item id's should be treated as localIdentifiers.
       // This wraps both v2.1 and v2.6 of moreinfo.
       if (preg_match('/[a-z]+/i', $value)) {
-        $ids[] = [
+        $ids[] = array(
           'localIdentifier' => $value,
           'libraryCode' => $this->group,
-        ];
+        );
       }
       else {
-        $ids[] = [
+        $ids[] = array(
           $type => $value,
-        ];
+        );
       }
     }
 
