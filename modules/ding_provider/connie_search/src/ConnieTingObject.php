910f6a4833b364478a45fa8e5dedcc894a14ae43
Mads H. Danquah <mads@danquah.dk>
2017-08-15 14:15:04 +0200

    Added a new search_search provider function and corrosponding tests.
    BBS-45

---
+++ modules/ding_provider/connie_search/src/ConnieTingObject.php
@@ -0,0 +1,201 @@
+<?php
+/**
+ * @file
+ * The ConnieTingObject class.
+ */
+
+namespace Connie\Search;
+
+use Ting\TingObjectInterface;
+
+class ConnieTingObject implements TingObjectInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTitle() {
+    return 'Connie and the Sorcerer\'s Stone';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getShortTitle() {
+    return 'Connie';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getAge() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getAudience() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDescription() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getExtent() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormat() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getGenere() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIsbn() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getMusician() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPegi() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPublisher() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getReferenced() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getReplacedBy() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getReplaces() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getRights() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getSeriesDescription() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getSource() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getSpatial() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getSpoken() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getSubTitles() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTracks() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getURI() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getVersion() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isPartOf() {
+    return [''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getType() {
+    return '';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getYear() {
+    return '';
+  }
+}
---
+++ modules/ding_provider/connie_search/src/ConnieTingObjectCollection.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @file
+ * The ConnieTingObjectCollection class.
+ */
+
+namespace Connie\Search;
+
+use Ting\TingObjectCollectionInterface;
+
+class ConnieTingObjectCollection implements TingObjectCollectionInterface {
+
+  /**
+   * @var \Ting\TingObjectInterface[]
+   */
+  protected $objects;
+
+  /**
+   * ConnieObjectCollection constructor.
+   *
+   * @param \Ting\TingObjectInterface[] $objects
+   *   The objects in the collection.
+   */
+  public function __construct($objects) {
+    $this->objects = $objects;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getObjects() {
+    return $this->objects;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPrimaryObject() {
+    // Get the first array-entry (without modifying the array).
+    $objects = array_values($this->objects);
+    return empty($this->tingObjects) ? NULL : array_shift($objects);
+  }
+
+  /**
+   * Simple factory for returning a collection with a single object.
+   *
+   * @return \Connie\Search\ConnieTingObjectCollection
+   *   The collection.
+   */
+  public static function getSingleCollection() {
+    return new self([new ConnieTingObject()]);
+  }
+}
