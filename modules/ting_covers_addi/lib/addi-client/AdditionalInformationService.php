241e1b6d457cfbe0634dc58b385ad3b7bd7b5064
holt83 <simoh@vejlebib.dk>
2017-08-22 16:11:05 +0200

    2582: Use the new WSDL URI for moreinfo

--- modules/ting_covers_addi/lib/addi-client/AdditionalInformationService.php
+++ modules/ting_covers_addi/lib/addi-client/AdditionalInformationService.php
@@ -107,9 +107,7 @@ class AdditionalInformationService {
     );
 
     // New moreinfo service.
-    $client = new SoapClient($this->wsdlUrl . '/moreinfo.wsdl', array(
-      'location' => $this->wsdlUrl,
-    ));
+    $client = new SoapClient($this->wsdlUrl . '/?wsdl');
 
     // Record the start time, so we can calculate the difference, once
     // the addi service responds.
