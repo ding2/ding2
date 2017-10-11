39e55e5c9c9e1f4869114d3acab9d6c0150586e4
CFE <CFE@1284514.no-reply.drupal.org>
2017-10-11 09:53:56 +0200

    2928: Only remove empty holding info

--- modules/ding_availability/js/ding_availability_labels.js
+++ modules/ding_availability/js/ding_availability_labels.js
@@ -115,8 +115,6 @@
         var reservable = status['reservable'];
         var available = status['available'];
 
-        $('.js-available, .js-reservable, .js-unavailable', groups_wrapper).remove()
-
         var group = null;
         if ($('.js-online', groups_wrapper).length !== 0) {
           group = $('.js-online', groups_wrapper);
@@ -147,6 +145,15 @@
 
         // Move the element into that type.
         group.append(element);
+
+        // Remove empty groups.
+        $('.js-available, .js-reservable, .js-unavailable', groups_wrapper).each(function () {
+          var $this = $(this);
+
+          if ($this.children('.search-result--availability-link').length === 0) {
+            $this.remove();
+          }
+        });
       }
 
       /**
