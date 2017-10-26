91b0878c9592c4ec21372d13a7c75073cbf3a23f
Jesper Kristensen <cableman@linuxdev.dk>
2017-10-26 23:47:12 +0200

    2671: Added theme function and render arrays

--- modules/ding_ipe_filter/js/ding_ipe_filter.js
+++ modules/ding_ipe_filter/js/ding_ipe_filter.js
@@ -1,10 +1,12 @@
 (function ($) {
   "use strict";
+
   $('html').click(function() {
     $('.ipe-popup').addClass('element-hidden');
     $('.ipe-popup').removeClass('ding-ipe-popup-top');
     $('.ipe-popup').removeClass('ding-ipe-popup-left');
   });
+
   $(document).on('click', '.ipe-trigger', function() {
     // Hide all elements before open other.
     $(this).addClass('active');
@@ -13,16 +15,19 @@
     $('.ipe-popup').removeClass('ding-ipe-popup-left');
 
     var menu = '#ipe-add-' + $(this).attr('target_region');
+
     // Move menu from the left if it is too close to the border.
     if (parseInt($(this).parent().offset().left) < 700) {
       $(menu).toggleClass('left');
     }
+
     // Move menu on top if it is too close to bottom of the page.
     var containerHeight = $('.panels-ipe-display-container').height();
     var menuPosition = $(this).parent().offset().top;
     if (parseInt(containerHeight - menuPosition) < 0) {
       $(menu).toggleClass('ding-ipe-popup-top');
     }
+
     // Display element on click.
     $(menu).toggleClass('element-hidden');
 
@@ -41,6 +46,7 @@
       // Close modal window on cancel button click.
       $('#edit-cancel', context).on('click', function() {
         Drupal.CTools.Modal.dismiss();
+
         return false;
       });
     }
