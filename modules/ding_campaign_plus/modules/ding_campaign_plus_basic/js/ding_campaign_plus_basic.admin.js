aacbc527840c54fe28f87365ee52d0689fabcf01
Jesper Kristensen <cableman@linuxdev.dk>
2017-11-13 16:51:44 +0100

    2177: Added campaign selection strategy with trigger modules

--- modules/ding_campaign_plus/modules/ding_campaign_plus_basic/js/ding_campaign_plus_basic.admin.js
+++ modules/ding_campaign_plus/modules/ding_campaign_plus_basic/js/ding_campaign_plus_basic.admin.js
@@ -6,6 +6,7 @@
   'use strict';
 
   // The select type that have auto-complete callbacks.
+  // @TODO: Get these from the backend via drupal.settings.
   var auto_complete_types = [
     'page',
     'event',
@@ -14,37 +15,6 @@
     'group',
     'taxonomy'
   ];
-
-  /**
-   * Change handler for facet type selection.
-   *
-   * Fix facet selection boxes as #stats don't work with ajax forms and
-   * "add another".
-   *
-   * @param event
-   *   The event that triggered the handler.
-   */
-  function facetTypeChangeHandler(event) {
-    var fieldset = $(event.target).parentsUntil('fieldset');
-    switch (this.value) {
-      case 'facet.type':
-        $('div[class$="facet-value-select-type"]', fieldset).show();
-        $('div[class$="facet-value-select-source"]', fieldset).hide();
-        $('div[class$="facet-value"]', fieldset).hide();
-        break;
-
-      case 'facet.acSource':
-        $('div[class$="facet-value-select-source"]', fieldset).show();
-        $('div[class$="facet-value-select-type"]', fieldset).hide();
-        $('div[class$="facet-value"]', fieldset).hide();
-        break;
-
-      default:
-        $('div[class$="facet-value-select-type"]', fieldset).hide();
-        $('div[class$="facet-value-select-source"]', fieldset).hide();
-        $('div[class$="facet-value"]', fieldset).show();
-    }
-  }
 
   /**
    * Rebuild auto-complete callbacks when rule type is changed.
@@ -70,7 +40,7 @@
     // Add auto complete if the value is one that has aut-complete support.
     if (auto_complete_types.indexOf(value) !== -1) {
       $context.find('input.autocomplete')
-        .val('/ding_campaign_plus/autocomplete/' + value)
+        .val('/ding_campaign_plus/basic/autocomplete/' + value)
         .removeClass('autocomplete-processed')
         .end()
         .find('input.form-text')
@@ -81,33 +51,11 @@
   }
 
   /**
-   * Handle facet selection boxes.
-   *
-   * @type {{attach: Drupal.behaviors.ding_campaign_plus_facet_selectors.attach}}
-   */
-  Drupal.behaviors.ding_campaign_plus_facet_selectors = {
-    attach: function (context, settings) {
-      var facet_selectors = $('select[multiple="multiple"]', context);
-      facet_selectors.parent().hide();
-
-      var fact_type_selectors = $('.js-fact-type', context);
-
-      // Bind change handler to the facet type selection dropdown.
-      fact_type_selectors.once('ding-campaign-plus-facet-type').each(function() {
-        $(this).bind('change', facetTypeChangeHandler);
-      });
-
-      // Ensures that the UI selections are initialized correct.
-      fact_type_selectors.trigger('change');
-    }
-  };
-
-  /**
    * Handle trigger type auto-complete callbacks.
    *
-   * @type {{attach: Drupal.behaviors.ding_campaign_plus_other_triggers.attach}}
+   * @type {{attach: Drupal.behaviors.ding_campaign_plus_basic_triggers.attach}}
    */
-  Drupal.behaviors.ding_campaign_plus_other_triggers = {
+  Drupal.behaviors.ding_campaign_plus_basic_triggers = {
     attach: function (context, settings) {
       var local_context = $('#ding-campaign-triggers', context);
 
