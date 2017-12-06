bbe2fc16701244b849826e84029c7ef1c186eaf2
Alex Damaschin <ad@peytz.dk>
2017-12-06 11:06:33 +0200

    EASYOPAC-720 - New campaign widget 'Grid campaign'.

---
+++ modules/ding_campaign/templates/ding-campaign-grid.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Default template for grid image campaign item.
+ *
+ * Variables:
+ *  $image
+ *  $title
+ *  $description
+ */
+?>
+<div class="ding-campaign-grid-item">
+  <div class="ding-campaign-grid-image"><?php print $image; ?></div>
+  <h2 class="ding-campaign-grid-title"><?php print $title; ?></h2>
+  <?php if (isset($description)): ?>
+      <div class="ding-campaign-grid-description"><?php print $description; ?></div>
+  <?php endif; ?>
+</div>
