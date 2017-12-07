40e636fbe3fdbacb12449a9ab4624f7327ecfa17
Mihaela Stavila <ms@inleadmedia.com>
2017-12-07 09:03:21 +0200

    EASYOPAC-720 - Changed tpl file.

--- modules/ding_campaign/templates/ding-campaign-grid.tpl.php
+++ modules/ding_campaign/templates/ding-campaign-grid.tpl.php
@@ -10,9 +10,12 @@
  */
 ?>
 <div class="ding-campaign-grid-item">
-  <div class="ding-campaign-grid-image"><?php print $image; ?></div>
+  <div class="campaign-info">
+    <div class="ding-campaign-grid-image"><?php print $image; ?>
+      <?php if (isset($description)): ?>
+        <div class="ding-campaign-grid-description"><?php print $description; ?></div>
+      <?php endif; ?>
+    </div>
+  </div>
   <h2 class="ding-campaign-grid-title"><?php print $title; ?></h2>
-  <?php if (isset($description)): ?>
-      <div class="ding-campaign-grid-description"><?php print $description; ?></div>
-  <?php endif; ?>
 </div>
