3990e5e85128e120dd8f63a6d816190943c440ee
Carsten Feilberg <capcaf@gmail.com>
2017-12-12 17:15:00 +0100

    3147: Formatting LogMessages
    Fixed a minor issue of resetting messages, as well as a number of formatting problems reported by the scrutinizer.

--- tests/behat/features/bootstrap/Page/LogMessages.php
+++ tests/behat/features/bootstrap/Page/LogMessages.php
@@ -1,34 +1,46 @@
 <?php
 
+/**
+ * @file
+ * Implements messages to be logged to the output for the benefit of the
+ * tester. Essentially the messages are a queue, which can be added to
+ * until it is emptied, whereby all messages queued up will be printed.
+ */
 
 namespace Page;
 
 use SensioLabs\Behat\PageObjectExtension\PageObject\Factory;
 use SensioLabs\Behat\PageObjectExtension\PageObject\Page;
 
-class LogMessages extends Page
-{
+class LogMessages extends Page {
   /**
+   * Contains all log messages from verbose mode.
+   *
    * @var string $messageCollection
-   * contains all log messages from verbose mode
+   *   This is where we store all the queued messages.
    */
   protected $messageCollection = '';
 
   /**
-   * @return string
    * Resets the log messages. They can only be read once.
+   *
+   * @return string
+   *   The string to be shown.
    */
   public function getMessages() {
-    return $this->messageCollection;
+    $tmp = $this->messageCollection;
     $this->messageCollection = "";
+    return $tmp;
   }
 
 
   /**
-   * log_msg - prints message on log if condition is true.
+   * Log_msg - prints message on log if condition is true.
    *
-   * @param $ifTrue
-   * @param $msg
+   * @param boolean $ifTrue
+   *   True indicates verbose mode, so we will print the timestamp, otherwise not.
+   * @param string $msg
+   *   This is the string we will show if the condition is true.
    */
   public function logMsg($ifTrue, $msg) {
     if ($ifTrue) {
@@ -39,19 +51,21 @@ class LogMessages extends Page
   }
 
   /**
-   * log_timestamp - puts a timestamp in the log. Good for debugging timing issues.
-   * @param $ifTrue
-   * @param $msg
+   * Log_timestamp - puts a timestamp in the log. Good for debugging timing issues.
+   *
+   * @param bool $ifTrue
+   *   True indicates verbose mode, so we will print the timestamp, otherwise not.
+   * @param string $msg
+   *   This is the message we will print before the timestamp. Default empty.
    */
-  public function logTimestamp($ifTrue, $msg) {
-    // this is so we can use this function with verbose-checking
+  public function logTimestamp($ifTrue, $msg = "") {
+    // This is so we can use this function with verbose-checking.
     if ($ifTrue) {
-      // get the microtime, format it and print it.
+      // Get the microtime, format it and print it.
       $t = microtime(true);
-      $micro = sprintf("%06d",($t - floor($t)) * 1000000);
-      $d = new DateTime( date('Y-m-d H:i:s.'.$micro, $t) );
+      $micro = sprintf("%06d", ($t - floor($t)) * 1000000);
+      $d = new DateTime(date('Y-m-d H:i:s.' . $micro, $t));
       $this->messageCollection = $this->messageCollection . $msg . " " . $d->format("Y-m-d H:i:s.u") . "\n";
     }
   }
-
 }
