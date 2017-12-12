1eeca5482ed39f5f614b825692acbddba0b6d6ee
Carsten Feilberg <capcaf@gmail.com>
2017-12-12 20:53:35 +0100

    3147: Fixed formatting DataManager
    Changed comments and indentation plus spaces according to scrutinizer.

--- tests/behat/features/bootstrap/DataManager.php
+++ tests/behat/features/bootstrap/DataManager.php
@@ -1,18 +1,37 @@
 <?php
 
+/**
+ * @file
+ * DataManager. Handles data from files.
+ */
+
+
+
+/**
+ * Class DataManager
+ */
 class DataManager extends \Page\PageBase
 {
   /**
+   * Filename.
+   *
    * @var string $filename
    */
   protected $filename = '';
 
   /**
+   * Flag for reservable only.
+   *
    * @var bool $onlyReservable
+   *    True if only finding reservable objects.
    */
   protected $onlyReservable = false;
+
   /**
-   * @return mixed getFilename
+   * GetFilename.
+   *
+   * @return mixed
+   *    The current filename.
    */
   public function getFilename()
   {
@@ -20,25 +39,26 @@ class DataManager extends \Page\PageBase
   }
 
   /**
-   * @param bool $reservable
+   * GetRandomPID
+   *
    * @return mixed|string
+   *
    * @throws Exception
    */
   public function getRandomPID() {
     $marray = [];
     if (is_readable($this->filename)) {
 
-      // now open, and read in all lines until no more data is returned, or we found a match
+      // Now open, and read in all lines until no more data is returned, or we found a match.
       $mfilehandle = fopen($this->filename, "r");
-      $foundit=false;
 
       $fline = fgets($mfilehandle);
-      if ($fline!="") {
+      if ($fline != "") {
         $marray[] = $fline;
       }
-      // now go through the rest of the file
+      // Now go through the rest of the file.
       while (($fline = fgets($mfilehandle)) !== false) {
-        if ($fline!="") {
+        if ($fline != "") {
           $marray[] = $fline;
         }
       }
@@ -47,33 +67,32 @@ class DataManager extends \Page\PageBase
         return "File '" . $this->filename . "' was empty";
       }
 
-      $tmp = explode("\t", $marray[(random_int(0, count($marray)-1))] );
-      if (count($tmp)!=2) {
+      $tmp = explode("\t", $marray[(random_int(0, count($marray)-1))]);
+      if (count($tmp) != 2) {
         return "File '" . $this->filename . "' expected to have two columns.";
       }
       $max = 200;
-      while (--$max>0 && $this->onlyReservable && !$this->isReservable($tmp[0])) {
-        $tmp = explode("\t", $marray[(random_int(0, count($marray)-1))] );
-        if (count($tmp)!=2) {
+      while (--$max > 0 && $this->onlyReservable && !$this->isReservable($tmp[0])) {
+        $tmp = explode("\t", $marray[(random_int(0, count($marray)-1))]);
+        if (count($tmp) != 2) {
           return "File '" . $this->filename . "' expected to have two columns.";
         }
       }
-      if ($max==0) {
+      if ($max == 0) {
         return "Error - no reservable information found";
       }
-      // return the PID
+      // Return the PID.
       return $tmp[0];
     }
   }
 
-  /* returns false only if material is reservable according to Connie rules.
-   ** todo: how to do this on other providers?
-    *
-    */
-  private function isReservable($mpid)
-  {
+  /**
+   * Returns false only if material is reservable according to Connie rules.
+   * @todo: how to do this on other providers?
+   */
+  private function isReservable($mpid) {
     $xx = explode(":", $mpid);
-    $provider = $xx[count($xx)-1];
+    $provider = $xx[count($xx) - 1];
     if ($provider % 2 == 0) {
       if (($provider / 10) % 2 == 0) {
         return true;
@@ -83,15 +102,22 @@ class DataManager extends \Page\PageBase
   }
 
   /**
-   * @param $setFilename
+   * SetFilename
+   *
+   * @param string $setFilename
+   *    Filename to use.
    */
   public function setFilename($setFilename) {
     $this->filename = $setFilename;
   }
 
+  /**
+   * SetReservable.
+   *
+   * @param bool $truefalse
+   *    Set flag for whether material should be reservable or not
+   */
   public function setReservable($truefalse) {
     $this->onlyReservable = $truefalse;
   }
-
-
 }
