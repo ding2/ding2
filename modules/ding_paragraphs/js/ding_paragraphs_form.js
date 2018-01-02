46b450f1fd02e6ead02ca26fc1a7e280f21ce1b6
Martin Yde Granath <my@aarhus.dk>
2018-01-02 15:08:21 +0100

    3021: Disabled paragraph dragging when any paragraphs are open

---
+++ modules/ding_paragraphs/js/ding_paragraphs_form.js
@@ -0,0 +1,29 @@
+/**
+ * @file
+ *
+ * Disables dragging in paragraphs when any paragraphs are open.
+ */
+
+(function ($) {
+  Drupal.behaviors.ding_paragraphs_form = {
+    attach: function (context) {
+      'use strict';
+
+      $('.field-type-paragraphs').once(function () {
+        // Limit the number of times to act on DOMSubtreeModified.
+        var active = false;
+        $(this).on('DOMSubtreeModified', function () {
+          if (!active) {
+            active = true;
+            if($("input[name$='collapse_button']").length > 0) {
+              $(".field-type-paragraphs .field-multiple-drag").hide();
+            }
+            else {
+              $(".field-type-paragraphs .field-multiple-drag").show();
+            }
+          }
+        })
+      });
+    }
+  }
+})(jQuery);
