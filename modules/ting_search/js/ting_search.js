6a14ebeefcef556f1f077ec33460534dc8b723c7
Kasper Garnaes <kasper.garnaes@gmail.com>
2018-01-29 13:15:14 +0100

    Revert "Merge pull request #934 from b14/b2983v3"
    This reverts commit 7e5fb16b326c6804945fbc1577b16d1975acaaa3, reversing
    changes made to a1726c9cc62419ee90327a985a3c6b90d241553f.

--- modules/ting_search/js/ting_search.js
+++ modules/ting_search/js/ting_search.js
@@ -17,22 +17,6 @@
       return true;
     });
 
-    // The search overlay fails in FF and Safari. 
-    // When the form submit is triggered, any changes to the DOM
-    // is not redrawn in browser. We need to add the search-
-    // overlay before formsubmit. 
-    // Therefore we take over the submit, and put the submit
-    // in a timeout function. That does the trick.
-    // @see https://stackoverflow.com/questions/22043493/unable-to-make-style-changes-to-the-dom-after-form-submit-in-safari?rq=1
-    $('#search-block-form').submit(function(event) {
-      event.preventDefault();
-      Drupal.TingSearchOverlay();
-      var form = this;
-      setTimeout(function () {
-        form.submit();
-      }, 0);
-    });
-
     // Add search link to the different links on the search result page.
     $('.search-results a').live('click', function() {
       if ($(this).not('[target="_blank"]').length) {
