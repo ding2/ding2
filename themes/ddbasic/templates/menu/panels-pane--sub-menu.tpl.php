dc56bb5f3b07cf32b2c1a0b729bcd69852141233
Martin Cording <guddo@me.com>
2018-02-09 12:50:30 +0100

    EASYOPAC-726 - Wrap submenu around panel-pane-inner.

--- themes/ddbasic/templates/menu/panels-pane--sub-menu.tpl.php
+++ themes/ddbasic/templates/menu/panels-pane--sub-menu.tpl.php
@@ -19,12 +19,14 @@
  */
 ?>
 <div class="<?php print $classes; ?>" <?php print $id; ?>>
-  <?php if ($admin_links): ?>
-    <?php print $admin_links; ?>
-  <?php endif; ?>
+  <div class="panel-pane-inner">
+    <?php if ($admin_links): ?>
+      <?php print $admin_links; ?>
+    <?php endif; ?>
 
-  <?php if (!empty($title)): ?>
-    <h2 class="sub-menu-title"><?php print $title; ?></h2>
-  <?php endif; ?>
-  <?php print render($content); ?>
+    <?php if (!empty($title)): ?>
+      <h2 class="sub-menu-title"><?php print $title; ?></h2>
+    <?php endif; ?>
+    <?php print render($content); ?>
+  </div>
 </div>
