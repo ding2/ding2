b5dd26032ff801b60637d2ed3eb6feab72f87a37
Martin Yde Granath <my@aarhus.dk>
2018-03-01 12:28:37 +0100

    3329: Fixed dropdown bug in paragraphs

--- modules/ding_paragraphs/js/ding_paragraphs_form.js
+++ modules/ding_paragraphs/js/ding_paragraphs_form.js
@@ -8,6 +8,13 @@
   Drupal.behaviors.ding_paragraphs_form = {
     attach: function (context) {
       'use strict';
+      // Hide table drag on page load and ajax load.
+      if ($("input[name$='collapse_button']").length > 0) {
+        $(".field-type-paragraphs .field-multiple-drag").hide();
+      }
+      else {
+        $(".field-type-paragraphs .field-multiple-drag").show();
+      }
 
       $('.field-type-paragraphs').once(function () {
         // Limit the number of times to act on DOMSubtreeModified.
