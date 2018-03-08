26b2f186d032c7490556501cc96abcc31d2ecb8a
Carsten Feilberg <capcaf@gmail.com>
2018-03-08 17:49:56 +0100

    3147: Minor scrutinizer problems fixed 3
    Fixed string type hint in function. Should not be there.

--- tests/behat/features/bootstrap/Page/SearchPage.php
+++ tests/behat/features/bootstrap/Page/SearchPage.php
@@ -435,7 +435,7 @@ class SearchPage extends PageBase {
    * @return string
    *    The transcribed version of the input string.
    */
-  public function replaceWithDoubleAa(string $inputStr) {
+  public function replaceWithDoubleAa($inputStr) {
     $outputStr = mb_ereg_replace('Å', "aa", $inputStr);
     return mb_strtolower($outputStr);
   }
