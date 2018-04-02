b875471d61b173f626e620df3eafd81f25e7cd6b
AnatolyMuntean <anatolii.mn@gmail.com>
2018-04-02 16:34:38 +0300

    EASYOPAC-823 - Fix for libraries messed with events.

--- themes/ddbasic/template.php
+++ themes/ddbasic/template.php
@@ -1356,13 +1356,18 @@ function ddbasic_views_pre_render(&$view){
     foreach ($view->result as &$item) {
       $node = node_load($item->nid);
 
+      // TODO: Library nodes can be encountered, which can be a view flaw.
+      if ($node->type != 'ding_event') {
+        continue;
+      }
+
       $field = $node->field_ding_event_date['und'][0];
       $val = $field['value'];
       if ($val == $field['value2']) {
-         $date = new DateTime($val, new DateTimeZone($field['timezone_db']));
-         $date->setTimezone(new DateTimeZone($field['timezone']));
-         $date = $date->format('H:i');
-         $field['rendered']['#markup'] = $date . ' - ' . t('All day');
+        $date = new DateTime($val, new DateTimeZone($field['timezone_db']));
+        $date->setTimezone(new DateTimeZone($field['timezone']));
+        $date = $date->format('H:i');
+        $field['rendered']['#markup'] = $date . ' - ' . t('All day');
       }
     }
   }
