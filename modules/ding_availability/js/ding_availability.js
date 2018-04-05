3cd7bdeaba9466b35f84d679f3868c5eeaa6e5d4
Jesper Kristensen <cableman@linuxdev.dk>
2018-04-05 10:20:08 +0200

    2223: Fixed code style issues

--- modules/ding_availability/js/ding_availability.js
+++ modules/ding_availability/js/ding_availability.js
@@ -9,6 +9,124 @@
   // Cache of fetched availability information.
   Drupal.DADB = {};
 
+  /**
+   * Load information from the backend.
+   *
+   * @param {string} mode
+   *   The type of information to fetch "holdings" or "items".
+   * @param {array} ids
+   *   The ids to fetch information for.
+   */
+  function ding_availability_fetch(mode, ids) {
+    var path = Drupal.settings.basePath + 'ding_availability/' + mode;
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: path,
+      data: { 'ids': ids },
+      success: function (data) {
+        $.each(data, function (id, item) {
+          // Update cache.
+          Drupal.DADB[id] = item;
+        });
+
+        $.each(Drupal.settings.ding_availability, function (id, entity_ids) {
+          if (id.match(/^availability-/)) {
+            // Update availability indicators.
+            ding_availability_update_availability(id, entity_ids);
+          }
+          else {
+            // Update holding information.
+            ding_availability_update_holdings(id, entity_ids);
+          }
+        });
+      },
+      error: function () {
+        $('div.loader').remove();
+      }
+    });
+  }
+
+  /**
+   * Update availability information.
+   *
+   * Add classes to reservation texts and display reservation button based on
+   * availability information.
+   *
+   * @param id
+   *   HTML id of the availability to update.
+   * @param entity_ids
+   *   Entity id for which to update availability information.
+   */
+  function ding_availability_update_availability(id, entity_ids) {
+    var available = false;
+    var reservable = false;
+    var element = $('#' + id);
+    element.removeClass('pending').addClass('processed');
+
+    $.each(entity_ids, function (index, entity_id) {
+      // Reserve button.
+      var reserve_button = element.parents('.ting-object:first, .material-item:first').find('a[id$=' + entity_id + '].reserve-button');
+
+      if (Drupal.DADB[entity_id]) {
+        var available = available || Drupal.DADB[entity_id]['available'];
+        var reservable = reservable || Drupal.DADB[entity_id]['reservable'];
+
+        // Special handling for periodicals.
+        if (typeof Drupal.DADB[entity_id]['is_periodical'] !== 'undefined' &&
+          Drupal.DADB[entity_id]['is_periodical']) {
+          // The main object of a periodical is neither available nor
+          // reservable, the individual issues is.
+          available = reservable = false;
+        }
+        var classes = [];
+
+        classes.push(available ? 'available' : 'unavailable');
+        classes.push(reservable ? 'reservable' : 'not-reservable');
+
+        $.each(classes, function (i, class_name) {
+          element.addClass(class_name);
+
+          // Add class to reserve button.
+          if (reserve_button.length) {
+            reserve_button.addClass(class_name);
+          }
+        });
+
+        if (available && !reservable) {
+          reserve_button.removeClass('available').addClass('unavailable');
+        }
+      }
+      else {
+        reserve_button.addClass('not-reservable');
+      }
+    });
+  }
+
+  /**
+   * Update availability holdings information.
+   *
+   * Insert HTML with information about where the entities are located.
+   *
+   * @param id
+   *   HTML id of the availability to update.
+   * @param entity_ids
+   *   Entity id for which to update availability holdings information.
+   */
+  function ding_availability_update_holdings(id, entity_ids) {
+    $.each(entity_ids, function (i, entity_id) {
+      if (Drupal.DADB[entity_id] && (Drupal.DADB[entity_id]['holdings'])) {
+        // Insert/update holding information for material.
+        $('#' + id).html(Drupal.DADB[entity_id].html);
+      }
+    });
+  }
+
+  /**
+   * Attach the availability behaviors to the page.
+   *
+   * The will be re-atcched at every page content update.
+   */
   Drupal.behaviors.ding_availability = {
     attach: function (context) {
       var settings = Drupal.settings;
@@ -75,118 +193,4 @@
       }
     }
   };
-
-  /**
-   * Load information from the backend.
-   *
-   * @param {string} mode
-   *   The type of information to fetch "holdings" or "items".
-   * @param {array} ids
-   *   The ids to fetch information for.
-   */
-  function ding_availability_fetch(mode, ids) {
-    var path = Drupal.settings.basePath + 'ding_availability/' + mode;
-    $.ajax({
-      type: "POST",
-      dataType: "json",
-      url: path,
-      data: { 'ids': ids },
-      success: function (data) {
-        $.each(data, function (id, item) {
-          // Update cache.
-          Drupal.DADB[id] = item;
-        });
-
-        $.each(Drupal.settings.ding_availability, function (id, entity_ids) {
-          if (id.match(/^availability-/)) {
-            // Update availability indicators.
-            ding_availability_update_availability(id, entity_ids);
-          }
-          else {
-            // Update holding information.
-            ding_availability_update_holdings(id, entity_ids);
-          }
-        });
-      },
-      error: function () {
-        $('div.loader').remove();
-      }
-    });
-  }
-
-  /**
-   * Update availability information.
-   *
-   * Add classes to reservation texts and display reservation button based on
-   * availability information.
-   *
-   * @param id
-   *   HTML id of the availability to update.
-   * @param entity_ids
-   *   Entity id for which to update availability information.
-   */
-  function ding_availability_update_availability(id, entity_ids) {
-    var available = false;
-    var reservable = false;
-    var element = $('#' + id);
-    element.removeClass('pending').addClass('processed');
-
-    $.each(entity_ids, function (index, entity_id) {
-      // Reserve button.
-      var reserve_button = element.parents('.ting-object:first, .material-item:first').find('a[id$=' + entity_id + '].reserve-button');
-
-      if (Drupal.DADB[entity_id]) {
-        var available = available || Drupal.DADB[entity_id]['available'];
-        var reservable = reservable || Drupal.DADB[entity_id]['reservable'];
-
-        // Special handling for periodicals.
-        if (typeof Drupal.DADB[entity_id]['is_periodical'] !== 'undefined' &&
-            Drupal.DADB[entity_id]['is_periodical']) {
-          // The main object of a periodical is neither available nor
-          // reservable, the individual issues is.
-          available = reservable = false;
-        }
-        var classes = [];
-
-        classes.push(available ? 'available' : 'unavailable');
-        classes.push(reservable ? 'reservable' : 'not-reservable');
-
-        $.each(classes, function (i, class_name) {
-          element.addClass(class_name);
-
-          // Add class to reserve button.
-          if (reserve_button.length) {
-            reserve_button.addClass(class_name);
-          }
-        });
-
-        if (available && !reservable) {
-          reserve_button.removeClass('available').addClass('unavailable');
-        }
-      }
-      else {
-        reserve_button.addClass('not-reservable');
-      }
-    });
-  }
-
-  /**
-   * Update availability holdings information.
-   *
-   * Insert HTML with information about where the entities are located.
-   *
-   * @param id
-   *   HTML id of the availability to update.
-   * @param entity_ids
-   *   Entity id for which to update availability holdings information.
-   */
-  function ding_availability_update_holdings(id, entity_ids) {
-    $.each(entity_ids, function (i, entity_id) {
-      if (Drupal.DADB[entity_id] && (Drupal.DADB[entity_id]['holdings'])) {
-        // Insert/update holding information for material.
-        $('#' + id).html(Drupal.DADB[entity_id].html);
-      }
-    });
-  }
-
 })(jQuery);
