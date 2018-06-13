439b607439ac74f80d569a40739ff5a0c11c4ca6
Alex Damaschin <ad@peytz.dk>
2018-06-13 15:48:24 +0300

    3608 by Inlead: Fix Scrutinizer issues.

--- themes/ddbasic/templates/place2book_ticketsinfo.tpl.php
+++ themes/ddbasic/templates/place2book_ticketsinfo.tpl.php
@@ -2,12 +2,15 @@
 
 /**
  * @file
- * Theme implementation for displaying place2book ticket info
+ * Theme implementation for displaying place2book ticket info.
+ *
  * (support for ding_place2book module)
  *
  * Variables:
- * $place2book_id: the ID for the event at place2book.com  (if it exists for the ding event)
- * $url: the full URL to the event at place2book.com (if it exists for the ding event)
+ * $place2book_id: the ID for the event at place2book.com  (if it exists for
+ *   the ding event)
+ * $url: the full URL to the event at place2book.com (if it exists for the ding
+ *   event)
  * $type: the type of information to display. 4 kinds exists:
  * - event-over
  * - closed-admission
@@ -20,23 +23,31 @@
 
 switch ($type) {
   case 'event-over':
-    print '<div class="place2book-tickets-label">'. t('Tickets') . '</div><div class="place2book-tickets-info">' . t('The event has already taken place') . '</div>';
+    print '<div class="place2book-tickets-label">' . t('Tickets') . '</div><div class="place2book-tickets-info">' . t('The event has already taken place') . '</div>';
     break;
 
   case 'closed-admission':
-    print '<div class="place2book-tickets-label">'. t('Tickets') . '</div><div class="place2book-tickets-info">' . t('Not open for ticket sale') . '</div>';
+    print '<div class="place2book-tickets-label">' . t('Tickets') . '</div><div class="place2book-tickets-info">' . t('Not open for ticket sale') . '</div>';
     break;
 
   case 'no-tickets-left':
-    print '<div class="place2book-tickets-label">'. t('Tickets') . '</div><div class="place2book-tickets-info">' . t('Sold out') . '</div>';
+    print '<div class="place2book-tickets-label">' . t('Tickets') . '</div><div class="place2book-tickets-info">' . t('Sold out') . '</div>';
     break;
 
   case 'order-link':
-    print l(t('Book a ticket'), $url, array('attributes' => array('class' => array('ticket-available', 'button'), 'target' => '_blank')));
+    print l(t('Book a ticket'), $url, [
+      'attributes' => [
+        'class' => [
+          'ticket-available',
+          'button',
+        ],
+        'target' => '_blank',
+      ],
+    ]);
     break;
 
   case 'sale-not-started':
-    print '<div class="place2book-tickets-label">'. t('Tickets') . '</div><div class="place2book-tickets-info">' . t('Ticket sale has not yet started for this event') . '</div>';
+    print '<div class="place2book-tickets-label">' . t('Tickets') . '</div><div class="place2book-tickets-info">' . t('Ticket sale has not yet started for this event') . '</div>';
     break;
 
   default:
