015e0ea6fe67dc09097d7aebf7a0c4c4d6658038
Simon Holt <simonholt83@gmail.com>
2018-08-16 02:26:17 +0200

    1663: Javascript fix

---
+++ modules/ding_event/js/ding_event_date_repeat_form.js
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+
+(function($) {
+  'use strict';
+
+  $(function() {
+    $('#repeat-settings-fieldset').hide();
+    $('#edit-field-ding-event-date-und-0-rrule-exceptions').hide();
+    $('#edit-field-ding-event-date-und-0-rrule-additions').hide();
+  });
+
+})(jQuery);
