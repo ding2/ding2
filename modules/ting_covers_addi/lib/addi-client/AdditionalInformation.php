d8b8a9bf4e8e5132f52c05644b36fa842d3b57ba
holt83 <simoh@vejlebib.dk>
2018-10-12 13:12:52 +0200

    3892: Scrutinizer fixes

--- modules/ting_covers_addi/lib/addi-client/AdditionalInformation.php
+++ modules/ting_covers_addi/lib/addi-client/AdditionalInformation.php
@@ -10,6 +10,9 @@ class AdditionalInformation {
   public $detailUrl;
   public $externUrl;
 
+  /**
+   * AdditionalInformation constructor.
+   */
   public function __construct($thumbnail_url, $detail_url, $extern_url) {
     $this->thumbnailUrl = $thumbnail_url;
     $this->detailUrl = $detail_url;
