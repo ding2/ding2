faf3977ac7b4285633e1ff4aa98c4b099d5bc05b
Alex <bozeak@gmail.com>
2018-10-19 11:07:26 +0300

    EASYOPAC-941 - Event page filters looks broken after changes.

--- modules/ding_event/templates/views-exposed-form-widget.tpl.php
+++ modules/ding_event/templates/views-exposed-form-widget.tpl.php
@@ -1,17 +1,18 @@
 <?php
+
 /**
  * @file
- * Views Exposed Form Widget template/
+ * Views Exposed Form Widget template.
  */
 
 $element_name = $element['#name'] . '[]';
 $element_id = $element['#id'];
 ?>
 
-<div class="views-exposed-form-widget" id="<?php print  $element_id; ?>">
+<div class="views-exposed-form-widget" id="<?php print $element_id; ?>">
   <h2 class="sub-menu-title"><?php print $title; ?></h2>
   <ul class="sub-menu">
-    <?php foreach ($items as $key => $item): ?>
+    <?php foreach ($items as $key => $item) : ?>
       <li>
         <input type="checkbox" id="<?php print $element_id . '-' . $key; ?>" name="<?php print $element_name; ?>" value="<?php print $key; ?>">
         <label for="<?php print $element_id . '-' . $key; ?>" class="menu-item"><?php print $item; ?></label>
