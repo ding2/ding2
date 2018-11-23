c683ec505686c56a3522cb77c20e0902127eeab4
Jesper Kristensen <cableman@linuxdev.dk>
2018-11-23 20:07:32 +0100

    3957: Trying to fix webtrekk race-condition

--- modules/ding_campaign_plus/js/ding_campaign_plus_trekk.js
+++ modules/ding_campaign_plus/js/ding_campaign_plus_trekk.js
@@ -5,6 +5,8 @@
 (function ($) {
   'use strict';
 
+  var queue = [];
+
   // Use custom event fired when campaign is loaded to track the campaign.
   $(document).on('campaignPlusLoaded', function (event, campaignId) {
     // The global wt (webtrekk) object is only loaded on approved domains. So to
@@ -15,5 +17,24 @@
         campaignAction: 'view'
       });
     }
+    else {
+      queue.push({
+        campaignId: campaignId,
+        campaignAction: 'view'
+      });
+    }
   });
+
+  // Try to busy wait for the "wt" variable to exists in global space to push
+  // variables to webtrekk. There will always be a race-condition here and
+  // this was the best try to get around it.
+  var timer = setInterval(processQueue, 500);
+  function processQueue() {
+    if (typeof(wt) !== 'undefined') {
+      clearInterval(timer);
+      for (var i in queue) {
+        wt.sendinfo(queue[i]);
+      }
+    }
+  }
 })(jQuery);
