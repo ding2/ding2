1b143c46c31a627bdc7708aa0b4ae6ade7e86f2a
holt83 <simoh@vejlebib.dk>
2018-12-21 12:49:44 +0100

    4039: Fix search spinner on ting object views
    Need to show search spinner when clicking on search links, so make the
    selector more specific to target those. This is also fixes the unwanted
    search spinner on ding entity buttons like the reserve button.
    Remove the ting-collection selector since the class is not used and
    there's no need for it when we already target the objects in the
    collection.
    Use the ting_object class instead of the wrapper, since this is always
    present on both collection on object view.

--- modules/ting/js/ting.js
+++ modules/ting/js/ting.js
@@ -6,15 +6,8 @@
   "use strict";
 
   $(document).ready(function() {
-    // Ensure overlay on collection view links.
-    $('.ting-collection-wrapper a[href*="/ting/"]').on('click', function() {
-      if ($(this).not('[target="_blank"]').length) {
-        Drupal.TingSearchOverlay();
-      }
-    });
-
-    // Ensure overlay on object view links.
-    $('.ting-object-wrapper a[href*="/ting/"]').on('click', function() {
+    // Ensure search overlay on search links in a ting object view.
+    $('.ting-object a[href^="/search/ting/"]').on('click', function() {
       if ($(this).not('[target="_blank"]').length) {
         Drupal.TingSearchOverlay();
       }
