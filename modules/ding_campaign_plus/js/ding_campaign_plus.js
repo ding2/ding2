845091401ce4db8ac4e6476cd1324efcc8cd5fac
Thomas Fini Hansen <xen@xen.dk>
2019-01-03 12:21:12 +0100

    3971: Fix up code style

--- modules/ding_campaign_plus/js/ding_campaign_plus.js
+++ modules/ding_campaign_plus/js/ding_campaign_plus.js
@@ -10,12 +10,12 @@
    * Load lazy loaded campaigns.
    */
   Drupal.behaviors.ding_campaign_plus_lazy_load = {
-    attach: function (context, settings) {
-      $('[data-ding-campaign-plus-cid]').once(function () {
+    attach: function (context) {
+      $('[data-ding-campaign-plus-cid]', context).once(function () {
         var campaign = $(this);
         var cid = campaign.data('ding-campaign-plus-cid');
         var url = Drupal.settings.basePath + Drupal.settings.pathPrefix + 'ding_campaigns_plus/' + cid;
-        $.get(url, {}, function (response, status) {
+        $.get(url, {}, function (response) {
           campaign.replaceWith(response);
         });
 
@@ -29,8 +29,8 @@
    * @type {{attach: Drupal.behaviors.ding_campaign_plus_basic_triggers.attach}}
    */
   Drupal.behaviors.ding_campaign_plus_node_edit = {
-    attach: function (context, settings) {
-      $('.form-item-ding-campaign-plus-auto-generate-keywords').once(function () {
+    attach: function (context) {
+      $('.form-item-ding-campaign-plus-auto-generate-keywords', context).once(function () {
         if (!$(':input[name="ding_campaign_plus_auto_generate[enable]"]').is(':checked')) {
           $(this).hide();
         }
