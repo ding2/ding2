41821fb708b608d8c19a12a7c24d608beb571995
Jesper Kristensen <cableman@linuxdev.dk>
2019-01-18 13:12:51 +0100

    3826: Added ding_auth
    This modules was developed during beta testing of adgangsplatformen

---
+++ modules/ding_auth/modules/ding_auth_registration/templates/ding-auth-acceptance.tpl.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file
+ * Default template for user self registration acceptance page.
+ *
+ * Note: The page have pane classes to make it look correct in DDB default theme
+ *       this is not optimal, but the only other solution would be to use
+ *       exiting_pages module to convert this to an panel page (but it seams
+ *       faster for performance and easier to just use the classes).
+ */
+?>
+<div class="empty-sidebars default-layout">
+  <div class="layout-wrapper">
+    <div class="pane-content">
+      <div class="primary-content ding-auth">
+        <h2 class="pane-title"><?php print t('Self registration form'); ?></h2>
+        <div class="ding-auth--registration-acceptance">
+          <?php print $variables['element']['#children'] ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
---
+++ modules/ding_auth/modules/ding_auth_registration/templates/ding-auth-registration.tpl.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @file
+ * Default template for user self registration information page.
+ *
+ * Available variables:
+ *  - $title: Headline and page title.
+ *  - $content: The content for the page.
+ *  - $link: Link to login page.
+ *
+ * Note: The page have pane classes to make it look correct in DDB default theme
+ *       this is not optimal, but the only other solution would be to use
+ *       exiting_pages module to convert this to an panel page (but it seams
+ *       faster for performance and easier to just use the classes).
+ */
+?>
+<div class="empty-sidebars default-layout">
+  <div class="layout-wrapper">
+    <div class="pane-content">
+      <div class="primary-content ding-auth">
+        <h2 class="pane-title"><?php print render($title); ?></h2>
+        <div class="ding-auth--registration-information">
+          <?php print render($content); ?>
+        </div>
+        <div class="ding-auth--link-wrapper">
+          <?php print render($link); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
