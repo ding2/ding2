d5cee5be0576d4a235be47f29e20f3855915b27b
Jesper Kristensen <cableman@linuxdev.dk>
2019-01-24 16:34:44 +0100

    3826: Enabled AJAX based redirect login

--- modules/p2/ding_entity_rating/js/ding_entity_rating_widget.js
+++ modules/p2/ding_entity_rating/js/ding_entity_rating_widget.js
@@ -100,7 +100,8 @@
     },
 
     sendRating: function (path, index, callback) {
-      var url = Drupal.settings.basePath + 'ding_entity_rating/' + path + '/' + index,
+      var current_path = window.location.pathname;
+      var url = Drupal.settings.basePath + 'ding_entity_rating/' + path + '/' + index + '?destination=' + current_path,
         $dummy = $('<a href="' + url + '"></a>'),
         drupal_ajax = new Drupal.ajax('fake', $dummy, {
           url: url,
