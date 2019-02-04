f7bddca18eab49b8951b39410fb07190279bbcba
Mathias Larsen <mathias@reload.dk>
2019-02-04 14:24:56 +0100

    4131: Rename ting_covers_rest to ting_covers_ddb
    Also fixes default URL

--- modules/ting_covers_ddb/lib/Configuration.php
+++ modules/ting_covers_ddb/lib/Configuration.php
@@ -81,7 +81,7 @@ class Configuration
      *
      * @var string
      */
-    protected $host = 'https://example.com/coverservice/2.0';
+    protected $host = 'https://cover.dandigbib.org/api';
 
     /**
      * User agent of the HTTP request, set to "OpenAPI-Generator/{version}/PHP" by default
@@ -436,7 +436,7 @@ class Configuration
     {
         return array(
           array(
-            "url" => "https://example.com/coverservice/2.0",
+            "url" => "https://cover.dandigbib.org/api",
             "description" => "DDB Cover serrvice",
           )
         );
