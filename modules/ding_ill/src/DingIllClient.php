6d30a485b0e099733fb722985b4a0c5a603a23dc
Mathias Larsen <mathias@reload.dk>
2019-02-11 22:48:10 +0100

    3800: Don't return exception

--- modules/ding_ill/src/DingIllClient.php
+++ modules/ding_ill/src/DingIllClient.php
@@ -5,6 +5,7 @@
  * Client for OpenPlatform.
  */
 
+use DingIllRequest;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 
@@ -48,7 +49,7 @@ class DingIllClient {
     $this->token = $token;
 
     // Initialize the instantiated GuzzleHttp Client.
-    $this->client = new Client(array('base_uri' => $hostname));
+    $this->client = new Client();
   }
 
   /**
@@ -61,21 +62,27 @@ class DingIllClient {
    * @param DingIllRequest $request
    *   The Ding ILL request object.
    *
-   * @return Psr\Http\Message\ResponseInterface|RequestException
-   *   The GuzzleHttp response or an exception.
+   * @return DingIllResponse
+   *   The ding ill response.
    */
   public function request($method, $type, DingIllRequest $request) {
-    try {
-      $response = $this->client->request($method, '/' . $type, array(
-        'access_token' => $this->token,
-        'body' => (array) $request,
-      ));
+    $response_object = new DingIllResponse();
 
-      return $response->getBody();
+    try {
+      $response = $this->client->request($method, $this->hostname . '/' . $type,
+        array(
+          'form_params' => array(
+            'access_token' => $this->token,
+            'pids' => $request->getMaterials(),
+            'pickUpBranch' => $request->getPickupBranch(),
+          ),
+        )
+      );
+      return $response_object::createFromResponse($response);
     }
     catch (RequestException $e) {
       watchdog_exception('ding_ill', $e);
-      return $e->getMessage();
+      return $response_object::createFromRequestException($e);
     }
   }
 
