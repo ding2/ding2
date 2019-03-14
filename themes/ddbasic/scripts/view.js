399ccfb5844aee992e8c253a5e8c383a33a663bb
Nicolae Grigoriu <ng@inlead.dk>
2019-03-14 16:23:01 +0200

    3974 by Inlead: Remove unused variables.

--- themes/ddbasic/scripts/view.js
+++ themes/ddbasic/scripts/view.js
@@ -187,7 +187,6 @@
         $('.view-ding-event.max-two-rows .view-elements .view-elements-inner .group-row').each(function () {
           var rows = $(this).children('.views-row'),
               row_total = 0,
-              row_order = 0,
               has_image,
               doc_style = document.documentElement.style;
 
@@ -206,9 +205,6 @@
                 row_total = row_total + 1;
                 has_image = false;
               }
-
-              // Set css order on rows.
-              $(this).attr('style',  'order:' + row_order);
 
             });
           }
