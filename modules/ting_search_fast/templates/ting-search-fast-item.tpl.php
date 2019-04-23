7ef5053cd04a3d4bbf9a2721ed15c1e0f9436d17
Kasper Garnaes <kasper.garnaes@gmail.com>
2019-04-23 15:03:24 +0200

    4214: Convert series information to theme function
    The separate theme function and template for series is removed to
    keep things simple and the Drupalish entity/field markup together.

--- modules/ting_search_fast/templates/ting-search-fast-item.tpl.php
+++ modules/ting_search_fast/templates/ting-search-fast-item.tpl.php
@@ -13,7 +13,14 @@
               <div class="field-item even"><?php print $abstract; ?></div>
             </div>
           </div>
-          <?php print $series; ?>
+          <?php if (!empty($series)) : ?>
+            <div class="field field-name-ting-series field-type-ting-series field-label-above">
+              <div class="field-label"><?php print t('Series') ?></div>
+              <div class="field-items">
+                <div class="field-item even"><?php print $series; ?></div>
+              </div>
+            </div>
+          <?php endif; ?>
           <div class="availability search-result--availability">
             <strong><?php print t('Borrowing options') ?></strong>
             <p class="js-online">
--- modules/ting_search_fast/templates/ting-search-fast-series.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php if (!empty($series)) : ?>
-<div class="field field-name-ting-series field-type-ting-series field-label-above">
-  <div class="field-label"><?php print t('Series') ?></div>
-  <div class="field-items">
-    <div class="field-item even"><?php print $series; ?></div>
-  </div>
-</div>
-<?php endif; ?>
