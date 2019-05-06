5212da1af3b8a43e3fc9c9d9048684c9aa6fae35
Ture Gjørup <ture@ignatz.dk>
2019-05-06 13:38:42 +0200

    3931: Removed antibot checkbot from LoginPage
    Antibot has been disabled for the login form in PR #1379

--- tests/behat/features/bootstrap/Page/Common/LoginPage.php
+++ tests/behat/features/bootstrap/Page/Common/LoginPage.php
@@ -7,9 +7,7 @@
 namespace Page\Common;
 
 use Behat\Mink\Element\NodeElement;
-use Behat\Mink\Exception\DriverException;
 use Behat\Mink\Exception\ElementNotFoundException;
-use Behat\Mink\Exception\UnsupportedDriverActionException;
 use SensioLabs\Behat\PageObjectExtension\PageObject\Page;
 
 /**
@@ -32,30 +30,18 @@ class LoginPage extends Page {
    *
    * @return NodeElement
    *
-   * @throws DriverException
    * @throws ElementNotFoundException
-   * @throws UnsupportedDriverActionException
    */
-  public function login(string $username, string $password) {
+  public function login(string $username, string $password): NodeElement
+  {
     $this->open();
-
-    // All forms are protected by the antibot module. The module uses
-    // javascript to set all form actions to '/antibot', then reset
-    // them to the original value when 'human' interaction is detected.
-    // Given that behat/mink interaction doesn't qualify as 'human' we
-    // need to wait for the antibot javascript module to load, then call
-    // its 'unlockForms()' before we submit the login form.
-    $this->waitFor(3, function (Page $page) {
-      return $page->getDriver()->evaluateScript('typeof(Drupal) !== "undefined" && typeof(Drupal.antibot) !== "undefined"');
-    });
-    $this->getDriver()->evaluateScript('Drupal.antibot.unlockForms()');
 
     $form = $this->getElement('Login Form');
     $form->fillField('edit-name', $username);
     $form->fillField('edit-pass', $password);
     $form->submit();
 
-    return $this->getPage('Login Page')->waitFor(3, function ($page) {
+    return $this->getPage('Login Page')->waitFor(3, static function ($page) {
       return $page->find('css', '.pane-current-user-name');
     });
   }
