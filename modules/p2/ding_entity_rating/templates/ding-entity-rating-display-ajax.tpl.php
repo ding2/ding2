a4d5834c4069913e4e8f31cd4fbe01dd74e16314
Jesper Kristensen <cableman@linuxdev.dk>
2019-05-08 12:30:52 +0200

    4296: Missing newline

--- modules/p2/ding_entity_rating/templates/ding-entity-rating-display-ajax.tpl.php
+++ modules/p2/ding_entity_rating/templates/ding-entity-rating-display-ajax.tpl.php
@@ -4,6 +4,7 @@
  * Default template for AJAX loading ratings.
  */
 ?>
+
 <div class="ding-entity-rating-display-ajax" data-ding-entity-display-ajax-rating-id="<?php print $variables['entity']->ding_entity_id; ?>">
  <span class="icon-spinner"></span>
 </div>
