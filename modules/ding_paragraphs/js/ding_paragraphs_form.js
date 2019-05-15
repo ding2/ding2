a6631266307c7024103d5194a2f6e323f0e9cde1
Martin Yde Granath <my@aarhus.dk>
2019-05-15 11:02:38 +0200

    3021: Cleaned up paragraphs form js

--- modules/ding_paragraphs/js/ding_paragraphs_form.js
+++ modules/ding_paragraphs/js/ding_paragraphs_form.js
@@ -3,35 +3,17 @@
  *
  * Disables dragging in paragraphs when any paragraphs are open.
  */
-
 (function ($) {
   'use strict';
   Drupal.behaviors.ding_paragraphs_form = {
     attach: function () {
       // Hide table drag on page load and ajax load.
-      if ($("input[name$='collapse_button']").length > 0) {
+      if ($(".field-type-paragraphs input[name$='collapse_button']").length > 0) {
         $(".field-type-paragraphs .field-multiple-drag").hide();
       }
       else {
         $(".field-type-paragraphs .field-multiple-drag").show();
       }
-
-      $('.field-type-paragraphs').once(function () {
-        // Limit the number of times to act on DOMSubtreeModified.
-        var active = false;
-        $(this).on('DOMSubtreeModified', function () {
-          if (!active) {
-            active = true;
-            // Hide dragging if an item is not collapsed.
-            if ($("input[name$='collapse_button']").length > 0) {
-              $(".field-type-paragraphs .field-multiple-drag").hide();
-            }
-            else {
-              $(".field-type-paragraphs .field-multiple-drag").show();
-            }
-          }
-        });
-      });
     }
   }
 })(jQuery);
