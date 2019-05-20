b3684c4946aaa0886e595e95784ee729b5a55060
Alex <bozeak@gmail.com>
2019-05-20 12:51:06 +0300

    4168 by Inlead: Add Eresource CT as supported Nodelist CT.

---
+++ modules/ding_nodelist/templates/ding_nodelist.ding_eresource.node_blocks.tpl.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @file
+ * Ding eresource node blocks template.
+ *
+ * @var array $item
+ * @var string $column
+ */
+?>
+<article data-row="<?php print $row; ?>" data-column="<?php print $column; ?>"
+         class="node node-ding-eresource node-promoted nb-item <?php print $item->image ? 'has-image' : ''; ?>">
+  <a href="<?php print '/node/' . $item->nid; ?>">
+    <div class="inner">
+      <div class="background">
+        <div class="button"><?php print t('Read more'); ?></div>
+      </div>
+      <div class="text news-text">
+        <div class="info-top">
+          <?php print drupal_render($item->category); ?>
+        </div>
+        <div class="title-and-lead">
+          <h3 class="title"><?php print $item->title; ?></h3>
+          <div
+            class="field field-name-field-ding-eresource-lead field-type-text-long field-label-hidden">
+            <div class="field-items">
+              <div class="field-item">
+                <?php print $item->teaser_lead; ?>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php if (!empty($item->image)): ?>
+      <div class="ding-eresource-list-image nb-image" style="background-image:url(<?php print $item->image; ?>);"></div>
+    <?php endif; ?>
+  </a>
+</article>
