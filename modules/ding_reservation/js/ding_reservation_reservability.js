d02af23d8323ca1076f27216a6d0b9c298fad9a7
holt83 <simoh@vejlebib.dk>
2019-06-13 11:57:27 +0200

    1460: Check reservabiblity for multiple entities in one AJAX-request

--- modules/ding_reservation/js/ding_reservation_reservability.js
+++ modules/ding_reservation/js/ding_reservation_reservability.js
@@ -8,19 +8,23 @@
 
   Drupal.behaviors.ding_reservation = {
     attach: function(context) {
-      $(".ting-object .reserve-button.check-reservability", context).once('check-reservability', function() {
-        var reserveButton = $(this);
-        var entityId = reserveButton.data("entity-id");
+      var entityIds = [];
+      var selector = '.ting-object .reserve-button.check-reservability';
+      $(selector, context).once('check-reservability', function() {
+        entityIds.push($(this).data("entity-id"));
+      });
+
+      if (entityIds.length) {
         $.ajax({
           dataType: "json",
-          url: "/ding_reservation/" + entityId + "/is_reservable",
+          url: "/ding_reservation/" + entityIds.join(',') + "/is_reservable",
           success: function(result) {
-            if (result.reservable) {
-              reserveButton.addClass('reservable');
-            }
+            $.each(result, function(entityId, reservability) {
+              $(selector + '[data-entity-id="' + entityId + '"]', context).addClass('reservable');
+            });
           }
         });
-      });
+      }
     }
   };
 
