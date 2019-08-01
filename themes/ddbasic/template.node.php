cdd71f46f7718eff7314722b1074ddbb0cb216b5
Nicolae Grigoriu <ng@inlead.dk>
2019-08-01 13:59:05 +0300

    EASYOPAC-976 - Remove show on mobile from core.

--- themes/ddbasic/template.node.php
+++ themes/ddbasic/template.node.php
@@ -359,15 +359,6 @@ function ddbasic_preprocess__node__ding_campaign(&$variables) {
   $variables['target'] = ding_base_get_value('node', $variables['node'], 'field_camp_new_window') ? '_blank' : '';
   $variables['panel_style'] = !empty($variables['elements']['#style']) ? drupal_html_class($variables['elements']['#style']) : '';
 
-  // @todo This should be moved to separate module.
-  // Display campaign if it is on the mobile browser.
-  if (!empty($variables['field_show_on_mobiles'])) {
-    $mobile_show = $variables['field_show_on_mobiles'][LANGUAGE_NONE][0]['value'];
-    if ($mobile_show) {
-      $variables['classes_array'][] = 'mobile-show';
-    }
-  }
-
   if (isset($type)) {
     switch ($type) {
       case 'image_and_text':
