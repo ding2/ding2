7031112c34486e65baeee636d35f1331d14288a6
Alex <bozeak@gmail.com>
2019-08-02 11:38:48 +0300

    DDBCMS-358 - Changing secondary result layout.

---
+++ templates/ting-subsearch-secondary--message-box--items-list.tpl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ *
+ * @var items
+ */
+?>
+<?php foreach ($items as $item) : ?>
+  <a href="<?php print $item['url']; ?>" target="_blank">
+    <div class="message-box--list-item">
+      <div class="message-box--content">
+        <?php print $item['markup']; ?>
+      </div>
+      <div class="message-box--see-more">
+        <?php print t('More details'); ?>
+      </div>
+    </div>
+  </a>
+<?php endforeach; ?>
---
+++ templates/ting-subsearch-secondary--message-box.tpl.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @file
+ *
+ * @var $content
+ * @var $message
+ * @var $bibdk_url
+ * @var $bibdk_url_text
+ */
+?>
+<div id="ting-subsearch-secondary"
+     class="message-box search-field-in-content--message"
+     data-href="<?php print $bibdk_url; ?>">
+  <div class="message-box--title">
+    <?php print $message; ?>
+  </div>
+  <div class="message-box--list-items">
+  <?php print $items; ?>
+  </div>
+  <div class="message-box--external-search">
+    <a href="<?php print $bibdk_url; ?>" target="_blank">
+      <?php print $bibdk_url_text; ?>
+    </a>
+  </div>
+</div>
