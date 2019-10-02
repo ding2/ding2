eb5d2a9dab1850716761f263aa61e3dfdaca676c
holt83 <simoh@vejlebib.dk>
2019-10-02 10:55:35 +0200

    3594: Update opt-out confirmation text

--- modules/ding_webtrekk/ding_webtrekk.opt-out.js
+++ modules/ding_webtrekk/ding_webtrekk.opt-out.js
@@ -23,7 +23,7 @@
         webtrekk.setCookie ('webtrekkOptOut', 1, 60 * 60 * 24 * 30 * 12 * 5);
 
         // Use ding_popup to show confirmation text.
-        var text = '<p>' + Drupal.t('You have successfully excluded yourself from tracking.') + '</p>';
+        var text = '<p>' + Drupal.t('You have successfully opted-out of Webtrekk cookie') + '</p>';
         var popup = ddbasic.popupbar.set('ding_webtrekk', text, false);
         $('.close-popupbar', popup).click(function(e) {
           e.preventDefault();
