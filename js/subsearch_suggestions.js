6b6366bdb20172f3a9f56db45442e497924d4270
Alex <bozeak@gmail.com>
2019-11-05 10:51:44 +0200

    4473 by Inlead: Subsearch performance optimization.

---
+++ js/subsearch_suggestions.js
@@ -0,0 +1,52 @@
+(function ($) {
+  'use strict';
+
+  let ssSettings = Drupal.settings;
+
+  let keys = ssSettings.subsearch_suggestions.keys;
+  let conditions = ssSettings.subsearch_suggestions.conditions;
+  let results = ssSettings.subsearch_suggestions.results;
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const originalSearch = urlParams.get('original-search');
+
+  let progressText = Drupal.t('Loading suggestions ...');
+  let wrapper = $('#subsearch-suggestions');
+  wrapper.text(progressText);
+
+  if (originalSearch !== null) {
+    let message = Drupal.t('See results for "' + keys + '", the search for "' + originalSearch + '" returned 0 hits.');
+    message = '<div id="subsearch-suggestions-first">' + message + '</div>';
+    wrapper.html(message);
+  }
+  else {
+    $.ajax({
+      type: 'POST',
+      url: '/subsearch_suggestions',
+      data: {
+        'keys': keys,
+        'conditions': conditions,
+        'results': results
+      },
+    })
+      .done(r => {
+        if (r !== '') {
+          wrapper.html(r);
+        }
+        else {
+          let messagePane = $('.pane-page-messages');
+          let messagesList = messagePane.find('li');
+          if (messagesList.length === 1) {
+            wrapper.parent('li').remove();
+            messagePane.find('.close-container a').click();
+          }
+          else {
+            wrapper.parent('li').remove();
+          }
+        }
+      })
+      .fail(e => {
+        console.log(e);
+      });
+  }
+})(jQuery, Drupal);
