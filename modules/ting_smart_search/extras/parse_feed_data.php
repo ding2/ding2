0e29937bbeee4a7bbffa8a378cc3b720757d748b
Kasper Skov-Nielsen <ks@randersbib.dk>
2019-11-13 10:24:22 +0100

    4362: Same procedure as previous commits

--- modules/ting_smart_search/extras/parse_feed_data.php
+++ modules/ting_smart_search/extras/parse_feed_data.php
@@ -1,9 +1,9 @@
 <?php
 
-/*
+/**
  * A script to parse the raw search files from Webtrekk via KPI index.
- * 
- * 
+ *
+ *
  */
 
 try {
@@ -16,7 +16,8 @@ try {
       if (ting_smart_search_is_from_period($line, 52)) {
         if (array_key_exists($search_key, $search_data) && is_numeric($number_of_searches)) {
           $search_data[$search_key]['long_period'] += $number_of_searches;
-        } else {
+        }
+        else {
           $search_data[$search_key] = array('long_period' => $number_of_searches, 'short_period' => 0);
         }
         if (ting_smart_search_is_from_period($line, 4)) {
@@ -26,7 +27,7 @@ try {
     }
   }
   uasort($search_data, 'ting_smart_search_sort_search_data');
-  $search_data = array_slice($search_data, 0, 5000); 
+  $search_data = array_slice($search_data, 0, 5000);
   $fp = fopen('searchdata.csv', 'w');
 
   foreach ($search_data as $search_key => $data) {
@@ -36,17 +37,22 @@ try {
 
   fclose($fp);
   ting_smart_search_write_to_feedlog("Feed data succesfully updated");
-} catch (Exception $e) {
+}
+catch (Exception $e) {
   ting_smart_search_write_to_feedlog("Parsing of data failed: " . $e->getMessage());
 }
 
 ting_smart_search_write_to_feedlog("Memory used: " . memory_get_peak_usage());
 
-
+/**
+ * Write to logfile
+ */
 function ting_smart_search_write_to_feedlog($entry) {
   file_put_contents("./feedlog.txt", print_r($entry . "\n", TRUE), FILE_APPEND);
 }
-
+/**
+ * Check if smart search record is within active year.
+ */
 function ting_smart_search_is_from_period($line, $lookback_period = 4) {
   $date = new DateTime();
   $year = $date->format("Y");
@@ -55,7 +61,8 @@ function ting_smart_search_is_from_period($line, $lookback_period = 4) {
   $search_week = $line[1];
   if ($search_year == $year && $week - $lookback_period <= $search_week) {
     return true;
-  } else if (($search_year == $year - 1) && $week <= $lookback_period) {
+  }
+  elseif (($search_year == $year - 1) && $week <= $lookback_period) {
     if ($search_week >= (52 - $lookback_period + $week)) {
       return true;
     }
@@ -63,6 +70,9 @@ function ting_smart_search_is_from_period($line, $lookback_period = 4) {
   return false;
 }
 
+/**
+ * Sort the search data.
+ */
 function ting_smart_search_sort_search_data($a, $b) {
   if ($a['long_period'] == $b['long_period']) {
     return 0;
