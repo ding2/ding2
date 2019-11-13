50b7ccc04aebe43afd0c15dca3417833d07bee58
Kasper Skov-Nielsen <ks@randersbib.dk>
2019-11-13 09:29:35 +0100

    4362: Newline after closing braces

--- modules/ting_smart_search/extras/parse_auto_data.php
+++ modules/ting_smart_search/extras/parse_auto_data.php
@@ -1,9 +1,9 @@
 <?php
 
-/*
+/**
  * A script to parse the raw autodata files from Webtrekk via KPI index.
- * 
- * 
+ *
+ *
  */
 
 $file_year = 'autodatayear.csv';
@@ -22,7 +22,8 @@ try {
       $object = null;
       if (strpos($clicked_page, 'ting/object/') !== false) {
         $object = explode('ting/object/', $clicked_page);
-      } else if (strpos($clicked_page, 'ting/collection/') !== false) {
+      }
+      elseif (strpos($clicked_page, 'ting/collection/') !== false) {
         $object = explode('ting/collection/', $clicked_page);
       }
       $faust = $object[1];
@@ -33,7 +34,8 @@ try {
 
         if (!array_key_exists($faust, $data[$search])) {
           $data[$search][$faust] = $hits;
-        } else {
+        }
+        else {
           $data[$search][$faust] += $hits;
         }
       }
@@ -50,13 +52,16 @@ try {
   file_put_contents($output_file, $serialized_output);
 
   ting_smart_search_write_to_log("Data succesfully updated");
-} catch (Exception $e) {
+}
+catch (Exception $e) {
   ting_smart_search_write_to_log("Parsing of data failed: " . $e->getMessage());
 }
 
 ting_smart_search_write_to_log("Memory used: " . memory_get_peak_usage());
 
-
+/**
+ * Write to log.
+ */
 function ting_smart_search_write_to_log($entry) {
   file_put_contents("./log.txt", print_r($entry . "\n", TRUE), FILE_APPEND);
 }
