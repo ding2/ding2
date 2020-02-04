7d85ed7cf2d1855117565ad2a718aa93611f84cd
Alex <bozeak@gmail.com>
2020-02-04 13:15:10 +0200

    DDBCMS-371 - Subsearch: Various fixes v2.

--- js/subsearch_suggestions.js
+++ js/subsearch_suggestions.js
@@ -10,12 +10,15 @@
   const urlParams = new URLSearchParams(window.location.search);
   const originalSearch = urlParams.get('original-search');
 
-  let progressText = Drupal.t('Loading suggestions ...');
-  let wrapper = $('#subsearch-suggestions');
-  wrapper.text(progressText);
+  let main = $('.pane-page-content');
+  main.prepend('<div id="subsearch-suggestions" style="background-color: #F1F2F2; padding-top: 30px;"><div style="max-width: 1124px;margin-left: auto;margin-right: auto;width: 90%;" class="suggestions inner-content"></div></div>');
+  let wrapper = $('#subsearch-suggestions .inner-content');
 
   if (originalSearch !== null) {
-    let message = Drupal.t('See results for "!keys", the search for "!originalSearch" returned 0 hits.', {'!keys': keys, '!originalSearch': originalSearch});
+    let message = Drupal.t('See results for "!keys", the search for "!originalSearch" returned 0 hits.', {
+      '!keys': keys,
+      '!originalSearch': originalSearch
+    });
     message = '<div id="subsearch-suggestions-first">' + message + '</div>';
     wrapper.html(message);
   }
@@ -29,23 +32,12 @@
         'results': results
       },
     })
-      .done(function(r) {
+      .done(function (r) {
         if (r !== '') {
           wrapper.html(r);
         }
-        else {
-          let messagePane = $('.pane-page-messages');
-          let messagesList = messagePane.find('li');
-          if (messagesList.length === 1) {
-            wrapper.parent('li').remove();
-            messagePane.find('.close-container a').click();
-          }
-          else {
-            wrapper.parent('li').remove();
-          }
-        }
       })
-      .fail(function(e) {
+      .fail(function (e) {
         console.log(e);
       });
   }
