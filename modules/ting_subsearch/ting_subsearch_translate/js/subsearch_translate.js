48a2d506d70f40ed5eec31b0a88f5f975ffddedb
Simon Holt <simonholt83@gmail.com>
2020-09-06 16:30:16 +0200

    4951: Switch to common ting_subsearch_message theme

--- modules/ting_subsearch/ting_subsearch_translate/js/subsearch_translate.js
+++ modules/ting_subsearch/ting_subsearch_translate/js/subsearch_translate.js
@@ -6,11 +6,10 @@
 (function ($) {
   'use strict';
 
-  let stSettings = Drupal.settings;
-
-  let keys = stSettings.subsearch_translate.keys;
-  let conditions = stSettings.subsearch_translate.conditions;
-  let results = stSettings.subsearch_translate.results;
+  var settings = Drupal.settings.subsearch_translate;
+  var keys = settings.keys;
+  var conditions = settings.conditions;
+  var results = settings.results;
 
   $.ajax({
     type: 'POST',
@@ -20,17 +19,12 @@
       'conditions': conditions,
       'results': results
     },
-  })
-      .done(r => {
-        if (r !== '') {
-          // @TODO: Rework wrapper attaching in more "Drupal" approach.
-          let main = $('.pane-page-content');
-          main.prepend('<div id="subsearch-translate" style="background-color: #F1F2F2; padding-top: 30px;"><div style="max-width: 1124px;margin-left: auto;margin-right: auto;width: 90%;" class="translate inner-content"></div></div>');
-          let wrapper = $('#subsearch-translate .inner-content');
-          wrapper.html(r);
-        }
-      })
-      .fail(e => {
-        console.log(e);
-      });
+  }).done(function(r) {
+    if (r !== '') {
+      $('#ting-subsearch-translate-placeholder').replaceWith(r);
+      Drupal.attachBehaviors(r);
+    }
+  }).fail(function(e) {
+    console.log(e);
+  });
 })(jQuery, Drupal);
