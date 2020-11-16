2e271b2592b58aecfeca169047a1264131799e63
Jesper Kristensen <cableman@linuxdev.dk>
2020-11-16 20:14:03 +0100

    DWAY-42: Updated to newest version of JS for wayfinding

--- modules/ding_wayfinding/js/ding_wayfinding.js
+++ modules/ding_wayfinding/js/ding_wayfinding.js
@@ -10,7 +10,7 @@
 
     // Check if a wayfindingId has any locations that can be shown on the map
     if (hasLocations) {
-      await window.libryWayfinding.createMap({
+      const map = await window.libryWayfinding.createMap({
         // Container ID of the html element holding the map
         container: "wayfinding-map",
         center: { lng: 10.2151585, lat: 56.1531825 },
@@ -22,19 +22,19 @@
         },
         // Shows a dot indicating the current device/user location
         showDevicePosition: true,
+        showAllPOIs: true,
       });
-      // Wait for the map to load with the ready event.
-      window.libryWayfinding.once("ready", function (map) {
-        // It is now safe to interact with the map.
-        map.showAllPOIS(true);
 
-        // Add the wayfinding marker.
-        map.addPoiMarkerByWayfindingId(wayfindingId, {
-          color: "red",
-          onClick: function (e, markerOpts) {
-            alert(`You clicked ${wayfindingId.faust}`);
-          },
-        });
+      map.updateDeviceLocation({
+        lngLat: { lng: 10.2151585, lat: 56.1531825 },
+      });
+
+      // Add the wayfinding marker.
+      map.addPoiMarkerByWayfindingId(wayfindingId, {
+        color: "red",
+        onClick: function (e, markerOpts) {
+          alert(`You clicked ${wayfindingId.faust}`);
+        },
       });
     } else {
       alert("There are no locations to show. Dont show map.");
