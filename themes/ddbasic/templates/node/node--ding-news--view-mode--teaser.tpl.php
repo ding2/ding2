e8a01bb1903a21cdd3eab53de9bbd2d92b9dbdc7
Jesper Pedersen <pejes@aarhus.dk>
2021-02-08 13:54:25 +0100

    4931: Add aria-labelledby to news list article elements

--- themes/ddbasic/templates/node/node--ding-news--view-mode--teaser.tpl.php
+++ themes/ddbasic/templates/node/node--ding-news--view-mode--teaser.tpl.php
@@ -86,12 +86,12 @@
  * @see template_process()
  */
 ?>
-<article class="<?php print $classes; ?>"<?php print $attributes; ?>>
+<article class="<?php print $classes; ?>" aria-labelledby="news-article-<?php print $node->nid; ?>" <?php print $attributes; ?>>
   <a href="<?php print $node_url; ?>">
     <div class="inner">
       <?php print $news_teaser_image; ?>
       <div class="news-text">
-        <h3 class="title"><?php print $title; ?></h3>
+        <h3 class="title" id="news-article-<?php print $node->nid; ?>"><?php print $title; ?></h3>
         <div class="category-and-submitted">
           <?php print render($content['field_ding_news_category']); ?>
           <div class="info-dash">-</div>
