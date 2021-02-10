e2d3ed0d282e58343c372bbbed533867a848cfe8
Alex <bozeak@gmail.com>
2021-02-10 14:05:30 +0200

    EASYOPAC-1272 - Implement video in Ding Nodelist widgets.

--- modules/ding_nodelist/templates/ding_nodelist.ding_eresource.rolltab.tpl.php
+++ modules/ding_nodelist/templates/ding_nodelist.ding_eresource.rolltab.tpl.php
@@ -7,9 +7,26 @@
  * @var array $item
  */
 
+$video_player = '';
+if (isset($item->video)) {
+  $video_player = '<div class="media-container">';
+  $video_player .= '<div class="media-content" data-url="' . $item->video . '" data-service="' . $item->video_service . '"></div>';
+  $video_player .= '<div class="pn-close-media"><i class="icon-cross"></i></div>';
+  $video_player .= '</div>';
+}
+
+$video_play_btn = '';
+if (isset($item->video)) {
+  $video_play_btn = "<div class='pn-media-play'><div class='pn-play pn-round'><i class='icon-play'></i></div></div>";
+}
+
+$classes = ['ding-tabroll'];
+$classes[] = (isset($item->video) ? 'has-video' : NULL);
+$classes = implode(' ', $classes);
 ?>
-<div class="ding-tabroll">
+<div class="<?php print $classes; ?>">
   <div class="ui-tabs-panel">
+    <?php print $video_player; ?>
     <div class="image">
       <?php if (!$item->image): ?>
         <span class="no-image"></span>
@@ -27,5 +44,7 @@
       <p> <?php print $item->teaser_lead; ?>
       </p>
     </div>
+
+    <?php print $video_play_btn; ?>
   </div>
 </div>
