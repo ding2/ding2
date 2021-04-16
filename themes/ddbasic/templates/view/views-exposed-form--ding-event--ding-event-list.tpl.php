fd27b613184986de6e83cba3977613d180bde11c
Martin Granath <martingranath@mac.local>
2021-04-16 08:59:52 +0200

    4085: Added spcing before if

--- themes/ddbasic/templates/view/views-exposed-form--ding-event--ding-event-list.tpl.php
+++ themes/ddbasic/templates/view/views-exposed-form--ding-event--ding-event-list.tpl.php
@@ -35,7 +35,7 @@
     <?php foreach ($widgets as $id => $widget): ?>
       <div id="<?php print $widget->id; ?>-wrapper" class="views-exposed-widget views-widget-<?php print $id; ?>">
         <?php if (!empty($widget->label)): ?>
-          <?php if($widget->id == 'edit-date') : ?>
+          <?php if ($widget->id == 'edit-date') : ?>
             <?php
             // Date fields use sub input fields for "from" and "to" so they can't use $widget->id as target.
             ?>
