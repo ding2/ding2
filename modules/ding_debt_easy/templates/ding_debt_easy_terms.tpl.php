328114b16bd02df1987d0f72ba9c02866ad98414
Jesper Kristensen <cableman@linuxdev.dk>
2021-12-15 17:37:33 +0100

    5117: Fixed terms rendering and absolute url

--- modules/ding_debt_easy/templates/ding_debt_easy_terms.tpl.php
+++ modules/ding_debt_easy/templates/ding_debt_easy_terms.tpl.php
@@ -11,11 +11,11 @@
 <div id="terms-of-sale">
   <h3><?php print t('Terms and conditions of sale', ['context' => 'ding_debt_easy'])?></h3>
   <p>
-    <?php if (!empty($terms)): ?>
+    <?php if (!empty($terms) && empty($url)): ?>
       <?php print $terms; ?>
     <?php endif; ?>
     <?php if (!empty($url)): ?>
-      <a href="<?php print $url; ?>" target="_blank"><?php print t('Read the Terms and conditions of sale at this page', ['context' => 'ding_debt_easy'])?></a>
+      <a href="<?php print url($url); ?>" target="_blank"><?php print t('Read the Terms and conditions of sale at this page', ['context' => 'ding_debt_easy'])?></a>
     <?php endif; ?>
   </p>
 </div>
