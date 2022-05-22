88a0313eabadb51e38212daa47a8dbff3335faf9
Simon Holt <simonholt83@gmail.com>
2022-05-22 21:37:27 +0200

    5150: Add comment explaining why we're not using behavoirs

--- modules/ding_seo/ding_seo.js
+++ modules/ding_seo/ding_seo.js
@@ -6,7 +6,8 @@
 (function($) {
   "use strict";
 
-  // TODO: Add comment explaining why we're not using Drupal behaviors.
+  // Bypass Drupal behaviors since we just need to insert a script in header
+  // once and have no need to interact with other elements on the page.
   $(function() {
     var tingObjectId = Drupal.settings.dingSeo.tingObjectId;
     $.get(`/ding_seo/jsonld/ting_object/${tingObjectId}`, function(jsonld) {
