d694269daf1d764497cb0c5a5fb43f73762ea3d5
Arni Loftsson <arniloftsson10.com>
2022-05-30 12:53:17 +0000

    5445: Code review fixes

--- modules/ting_fulltext/js/ting_fulltext.js
+++ modules/ting_fulltext/js/ting_fulltext.js
@@ -1,14 +1,20 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    var SearchExpansionResults = function (response) {
-        $("#ting-lektor-fulltext").html(response);
+  Drupal.behaviors.fullTextReview = {
+    attach: function (context) {
+      var selector = '#js-ting-lektor-fulltext';
+      $(selector, context).once('js-ting-lektor-fulltext', function () {
+         var url = $(this).attr("data-fulltext-url");
+         $.ajax({
+           type: 'POST',
+           url: url,
+           success: function (result) {
+             $("#js-ting-lektor-fulltext").html(result);
+           }
+         });
+      });
     }
-
-    Drupal.behaviors.fullTextReview = {
-        attach: function (context) {
-            $.get(Drupal.settings.tingFulltext.url, null, SearchExpansionResults);
-        }
-    };
+  };
 
 })(jQuery);
