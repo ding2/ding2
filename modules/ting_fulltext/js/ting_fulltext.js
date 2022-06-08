abf5d37c35c0ebdd732667d123c80e71219c3602
Arni Loftsson <arniloftsson10.com>
2022-06-08 09:38:01 +0000

    5445: Using class instead of id

--- modules/ting_fulltext/js/ting_fulltext.js
+++ modules/ting_fulltext/js/ting_fulltext.js
@@ -3,14 +3,14 @@
 
   Drupal.behaviors.fullTextReview = {
     attach: function (context) {
-      var selector = '#js-ting-lektor-fulltext';
+      var selector = '.js-ting-lektor-fulltext';
       $(selector, context).once('js-ting-lektor-fulltext', function () {
          var url = $(this).attr("data-fulltext-url");
          $.ajax({
            type: 'POST',
            url: url,
            success: function (result) {
-             $("#js-ting-lektor-fulltext").html(result);
+             $(".js-ting-lektor-fulltext").html(result);
            }
          });
       });
