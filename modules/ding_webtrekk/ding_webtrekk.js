cbb85a7ba97733acf2d0eac62bdd2fc02b37c852
Arni Loftsson <arniloftsson10.com>
2022-07-12 11:10:18 +0000

    5448: Adding no cookie tracking

--- modules/ding_webtrekk/ding_webtrekk.js
+++ modules/ding_webtrekk/ding_webtrekk.js
@@ -237,7 +237,13 @@
             sendCoverEvent(id);
           }
           $(this).unbind('DOMSubtreeModified');
-        })
+        });
+
+      // Cookieinformation compliance integration.
+      if (!CookieInformation.getConsentGivenFor('cookie_cat_statistic')) {
+        // If the user has not yet consented or opted out of statistic cookies we use nocookietracking.
+        noCookieTracking();
+      }
     }
   };
 
