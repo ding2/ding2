b28716692e4ca7db35c89a2835ea043ea422da4d
OIOIOOOI <anatolii.mn@gmail.com>
2023-02-21 18:53:26 +0200

    EASYOPAC-1440 - Missed files.

---
+++ themes/ddbasic/templates/a11y.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file
+ * A11y template file.
+ *
+ * @var $size
+ * @var $contrast
+ */
+?>
+<div id="a11y" class="a11y">
+  <?php print $size; ?>
+  <?php print $contrast; ?>
+</div>
