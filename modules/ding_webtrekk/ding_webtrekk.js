ff1e3619d2f2264cb8467e5ef917f3915f7c4628
Kasper Garnæs <kasperg@users.noreply.github.com>
2023-09-28 15:58:19 +0200

    Remove local nocookie handling
    According to Mapp this will not work together with their scripts. Our cookie deletion logic on needs to be removed.

--- modules/ding_webtrekk/ding_webtrekk.js
+++ modules/ding_webtrekk/ding_webtrekk.js
@@ -55,7 +55,6 @@
   // functions useful, so make them available on the global Drupal object.
   Drupal.dingWebtrekkAppendQueryParameter = appendQueryParameter;
   Drupal.dingWebtrekkPushEvent = pushEvent;
-  Drupal.dingWebtrekkNoCookieTracking= noCookieTracking;
 
   Drupal.dingWebtrekkCoverFound = false;
 
@@ -239,21 +238,6 @@
           $(this).unbind('DOMSubtreeModified');
         });
 
-      const noCookieTracking = function (name) {
-        var cookiesToDelete = Drupal.settings.dingWebtrekk.cookiesToRemove;
-        cookiesToDelete.forEach(cookie => {
-          var hostname = window.location.hostname;
-          while (hostname !== '') {
-          // The version of jquery.cookie which comes with this version of Drupal does not have
-          // removeCookie function so we use this method instead.
-            $.cookie(cookie, null, { path: '/', domain: hostname });
-
-            var index = hostname.indexOf('.');
-            // We can be on a sub-domain, so keep checking the main domain as well.
-            hostname = (index === -1) ? '' : hostname.substring(index + 1);
-          }
-        });
-      }
       // This code fires multiple times. But we need to make sure that the cookies are removed after they are
       // set and on any change in consent.
       if (typeof CookieInformation !== 'undefined' && CookieInformation !== null) {
